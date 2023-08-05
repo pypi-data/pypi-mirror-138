7d809d8ebd33548d3f33ef3acc5581ff7eb6a7b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:13 +0000

    Add package api-interactor/0.0.0/api-interactor-0.0.0.tar.gz

---
+++ packages/api-interactor/api-interactor-0.0.0.tar.gz/api-interactor-0.0.0/README.md
@@ -0,0 +1,2 @@
+D:\dev\api-interactor
+###This project was structure with [Choam](https://github.com/cowboycodr/choam)
---
+++ packages/api-interactor/api-interactor-0.0.0.tar.gz/api-interactor-0.0.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="api-interactor",
+    version="0.0.0",
+    description="asdasdasd",
+    packages=find_packages(),
+    keywords=[],
+    install_requires=['twine', 'wheel', 'flask', 'requests'],
+    project_urls={
+        'Source': 'https://github.com/cowboycodr/interactor'
+    },
+)
