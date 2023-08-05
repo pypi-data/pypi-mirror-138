82be905196eb6b71661d63286124bb19335689c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:19 +0000

    Add package xarray-cube/0.3.0/xarray_cube-0.3.0-py3-none-any.whl

---
+++ packages/xarray-cube/xarray_cube-0.3.0-py3-none-any.whl/xarray_cube/__init__.py
@@ -0,0 +1,13 @@
+# type: ignore
+__author__ = "xarray-cube developers"
+__version__ = "0.3.0"
+
+
+# submodules
+from . import accessor
+from . import converter
+from . import cube
+
+
+# aliases
+from .cube import *
