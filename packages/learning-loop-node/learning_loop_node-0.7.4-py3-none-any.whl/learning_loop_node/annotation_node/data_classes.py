ba330b0b9ab9ac7e88b01039df5108daf4a483ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:14:30 +0000

    Add package learning-loop-node/0.7.4/learning_loop_node-0.7.4-py3-none-any.whl

---
+++ packages/learning-loop-node/learning_loop_node-0.7.4-py3-none-any.whl/learning_loop_node/annotation_node/data_classes.py
@@ -0,0 +1,54 @@
+from typing import List, Optional
+from pydantic import BaseModel
+from enum import Enum
+from learning_loop_node.context import Context
+from learning_loop_node.data_classes.category import Category
+
+
+class Point(BaseModel):
+    x: int
+    y: int
+
+
+class Shape(BaseModel):
+    points: List[Point]
+
+
+class SegmentationAnnotation(BaseModel):
+    id: str
+    shape: Shape
+    image_id: str
+    category_id: str
+
+
+class EventType(str, Enum):
+    MouseDown = 'mouse_down',
+    MouseMove = 'mouse_move',
+    MouseUp = 'mouse_up',
+
+    # TODO Introduce Keyboard Event?
+    Enter_Pressed = 'enter_pressed'
+    ESC_Pressed = 'esc_pressed'
+
+
+class AnnotationData(BaseModel):
+    coordinate: Point
+    event_type: EventType
+    context: Context
+    image_uuid: str
+    category: Category
+    is_shift_key_pressed: Optional[bool]
+    is_alt_key_pressed: Optional[bool]
+    # keyboard_modifiers: Optional[List[str]]
+    # new_annotation_uuid: Optional[str]
+    # edit_annotation_uuid: Optional[str]
+
+
+class UserInput(BaseModel):
+    frontend_id: str
+    data: AnnotationData
+
+
+class ToolOutput(BaseModel):
+    svg: str
+    annotation: Optional[SegmentationAnnotation]
---
+++ packages/learning-loop-node/learning_loop_node-0.7.4-py3-none-any.whl/learning_loop_node/status.py
@@ -0,0 +1,70 @@
+from typing import List, Optional
+from pydantic import BaseModel
+from enum import Enum
+from dataclasses import dataclass, field
+
+from .trainer.model import PretrainedModel
+
+
+class State(str, Enum):
+    Idle = "idle"
+    Offline = "offline"
+    Online = "online"
+    Running = "running"
+    Preparing = "preparing"
+
+
+class Status(BaseModel):
+    id: str
+    name: str
+    state: Optional[State] = State.Offline
+    uptime: Optional[int] = 0
+    _errors: Optional[dict] = {}
+
+    def set_error(self, key: str, value: str):
+        self._errors[key] = value
+
+    def reset_error(self, key: str):
+        try:
+            del self._errors[key]
+        except AttributeError:
+            pass
+        except KeyError:
+            pass
+
+    def reset_all_errors(self):
+        for key in list(self._errors.keys()):
+            self.reset_error(key)
+
+
+class AnnotationNodeStatus(Status):
+    capabilities: List[str]
+
+
+@dataclass
+class TrainingStatus():
+    id: str
+    name: str
+    state: Optional[State]
+    errors: Optional[dict]
+    uptime: Optional[int]
+
+    latest_produced_model_id: Optional[str] = None
+    train_image_count: Optional[int] = None
+    test_image_count: Optional[int] = None
+    skipped_image_count: Optional[int] = None
+    pretrained_models: List[PretrainedModel] = field(default_factory=list)
+
+
+@dataclass
+class DetectionStatus():
+    id: str
+    name: str
+    state: Optional[State]
+    errors: Optional[dict]
+    uptime: Optional[int]
+
+    model_format: str
+    current_model: Optional[str]
+    target_model: Optional[str]
+    operation_mode: Optional[str]
