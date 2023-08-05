0b0f07e7eb69787cb1eaf8c21442e37670eb5144
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:21 +0000

    Add package yodo1-toolkit/0.3.2/yodo1-toolkit-0.3.2.tar.gz

---
+++ packages/yodo1-toolkit/yodo1-toolkit-0.3.2.tar.gz/yodo1-toolkit-0.3.2/setup.py
@@ -0,0 +1,28 @@
+import codecs
+
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+with codecs.open('requirements.txt', 'r', 'utf8') as reader:
+    install_requires = list(map(lambda x: x.strip(), reader.readlines()))
+
+setuptools.setup(
+    name="yodo1-toolkit",
+    version="0.3.2",
+    author="Eliyar Eziz",
+    author_email="eliyar@yodo1.com",
+    description="A Yodo1 Python Toolbox",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Yodo1Games/py-yodo1",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(exclude=('tests',)),
+    install_requires=install_requires,
+    python_requires=">=3.6",
+)
