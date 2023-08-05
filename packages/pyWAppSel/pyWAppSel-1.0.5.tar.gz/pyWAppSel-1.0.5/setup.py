bbf1d18659c7bae820e2b0dc5b8decd6ae1fc4d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:18:33 +0000

    Add package pyWAppSel/1.0.5/pyWAppSel-1.0.5.tar.gz

---
+++ packages/pyWAppSel/pyWAppSel-1.0.5.tar.gz/pyWAppSel-1.0.5/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+requirements = ["requests<=2.21.0"]
+
+setuptools.setup(
+    name="pyWAppSel",
+    version="1.0.5",
+    author="Dark White",
+    author_email="sasha.2000ibr@gmail.com",
+    description="pyWAppSel is a selenium-based Whatsapp wrapper",
+    long_description=long_description,
+    install_requires=[
+        'selenium',
+        'webdriver_manager',
+    ],
+    long_description_content_type="text/markdown",
+    url="https://github.com/pysashapy/pyWApp",
+    packages=setuptools.find_packages(),
+    license="Apache License 2.0",
+    classifiers=[
+        "Programming Language :: Python :: 3.10",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
