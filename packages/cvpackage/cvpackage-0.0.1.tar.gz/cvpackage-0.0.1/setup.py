9cacdb2dea90707355a2d50606ac05059d40ba84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:19:46 +0000

    Add package cvpackage/0.0.1/cvpackage-0.0.1.tar.gz

---
+++ packages/cvpackage/cvpackage-0.0.1.tar.gz/cvpackage-0.0.1/setup.py
@@ -0,0 +1,24 @@
+# importing setup from setup tools
+from setuptools import setup
+
+setup(
+   name='cvpackage',
+   packages=['cvpackage'],
+   version='0.0.1',
+   license='MIT',
+   description='An helper library for openCv developer.',
+   author='Kartik Panchal',
+   author_email='clickshare07@gmail.com',
+   url='https://github.com/MrBucks07/CvPack',
+   keywords=['MediaPipe', 'FaceMesh', 'FaceDetection', 'HandTracking', 'HandModule', 'FaceModule'],
+   install_requires=[
+      'opencv-python',
+      'mediapipe'
+   ],
+   classifiers=[
+      'License :: OSI Approved :: MIT License',
+      'Intended Audience :: Developers',
+      'Programming Language :: Python :: 3',
+      'Operating System :: OS Independent'
+    ],
+)
