a1499097efe59f26dd96694e94dfc31f55d44ce4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:17:53 +0000

    Add package assassin-api/0.0.1/assassin-api-0.0.1.tar.gz

---
+++ packages/assassin-api/assassin-api-0.0.1.tar.gz/assassin-api-0.0.1/src/assassin_api/decorators.py
@@ -0,0 +1,23 @@
+import json
+from .exceptions import NotFoundException, AuthenticationError, ServerError, Error, RequestError
+
+
+def api_exceptions_handler(api_func):
+    def wrapper(*args, **kwargs):
+        response = api_func(*args, **kwargs)
+        response_json = response.json()
+        response_str = str(response_json)
+
+        if response.status_code in (200, 201, 202, 204):
+            return response
+        if response.status_code == 404:
+            raise NotFoundException(response_str)
+        elif response.status_code == 400:
+            raise RequestError(response_str)
+        elif response.status_code == 403:
+            raise AuthenticationError(response_str)
+        elif response.status_code in (500, 501, 502):
+            raise ServerError(response_str)
+        else:
+            raise Error(response_str)
+    return wrapper
