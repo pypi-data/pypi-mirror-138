4f514788e8cb6b79304ce8de3cd8a49358907ae8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:13:19 +0000

    Add package openshift-python-wrapper/1.5.3.58/openshift-python-wrapper-1.5.3.58.tar.gz

---
+++ packages/openshift-python-wrapper/openshift-python-wrapper-1.5.3.58.tar.gz/openshift-python-wrapper-1.5.3.58/setup.py
@@ -0,0 +1,23 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+
+from setuptools import find_packages, setup
+
+
+setup(
+    name="openshift-python-wrapper",
+    license="apache-2.0",
+    keywords=["Openshift", "Kubevirt", "CNV"],
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "kubernetes",
+        "openshift",
+        "xmltodict",
+        "urllib3",
+        "netaddr",
+        "paramiko",
+        "pbr",
+    ],
+    python_requires=">=3.6",
+)
