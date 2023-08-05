790ef65c4f217565f1cf4409a68b5c0894b7fc09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:18:40 +0000

    Add package yyes/1.1.0/yyes-1.1.0.tar.gz

---
+++ packages/yyes/yyes-1.1.0.tar.gz/yyes-1.1.0/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+
+setup(name='yyes',
+      python_requires='>=3.5.0',
+      version='1.1.0',
+      description='http',
+      author='tjsh',
+      author_email='2438280753@qq.com',
+      maintainer='tjsh',
+      maintainer_email='2438280753@qq.com',
+      url='http://www.flexible-world.com',
+      packages=['yyes'],
+      long_description="none",
+      license="none",
+      platforms=["?"],
+      install_requires=[
+            "requests>=2.6",
+            "BeautifulSoup4>=2.2.1",
+            "lxml>=4.6.3",
+            "pywin32>=301"
+      ],
+      classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+      ],
+      )
