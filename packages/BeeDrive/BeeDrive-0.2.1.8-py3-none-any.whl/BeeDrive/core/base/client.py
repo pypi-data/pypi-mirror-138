4158c4293e470a0ea9e003a16a7c15d687d3be22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:36 +0000

    Add package BeeDrive/0.2.1.8/BeeDrive-0.2.1.8-py3-none-any.whl

---
+++ packages/BeeDrive/BeeDrive-0.2.1.8-py3-none-any.whl/BeeDrive/core/base/client.py
@@ -0,0 +1,117 @@
+import pickle
+import time
+
+from .idcard import IDCard
+from .worker import BaseWorker
+from ..utils import build_connect, disconnect
+from ..logger import callback_info, callback_flush
+from ..encrypt import SUPPORT_AES, AESCoder
+from ..constant import END_PATTERN, TCP_BUFF_SIZE, VERSION
+from ..constant import (STAGE_INIT, STAGE_PRE, STAGE_RUN,
+                        STAGE_DONE, STAGE_FAIL, STAGE_RETRY)
+
+
+class BaseClient(BaseWorker):
+    def __init__(self, user, passwd, target, task,
+                 retry=3, encrypt=True, proxy=None):
+        self.msg = self.stage = STAGE_INIT
+        BaseWorker.__init__(self, None, encrypt)
+        self.proxy = [] if proxy is None else proxy
+        self.user = user
+        self.passwd = passwd
+        self.target = target
+        self.task = task
+        self.max_retry = retry
+        self.peer = None
+        
+    def __enter__(self):
+        self.msg = "Connecting to cloud"
+        self.socket = self.build_connect()
+        if self.socket:
+            self.build_pipeline(self.passwd)
+            self.verify_connect()
+        if self.peer:
+            self.active()
+
+    def prepare(self):
+        raise NotImplemented()
+
+    def process(self, **kwrds):
+        raise NotImplemented()
+
+    def build_connect(self):
+        # connect the server
+        for ip, port in [self.target] + self.proxy:
+            # try to connect target
+            conn = build_connect(ip, port)
+            if isinstance(conn, str):
+                continue
+            
+            # whether connect to the proxy or not
+            if (ip, port) != self.target:
+                source = self.info.code #str(self.socket.getsockname())
+                # makesure the proxy node has target ip
+                conn.sendall(('Proxy:%s$%s$' % (str(self.target), source)).encode() + END_PATTERN)
+                rspn = conn.recv(TCP_BUFF_SIZE)
+                if rspn != b"TRUE":
+                    continue
+                callback_info('- using proxy %s:%s to connect target %s:%d' % (ip, port, self.target[0], self.target[1]))
+                self.use_proxy = True
+            return conn
+        
+        self.msg = u"Error: cannot find out target %s:%s" % self.target
+        callback_info(self.msg)
+        return False
+
+    def verify_connect(self):
+        # speak out who am I and what I need
+        header = ("%s %s BEE/%s\r\n" % (self.task.lower(), self.user, VERSION)).encode("utf8")
+        info = pickle.dumps(self.info.info)
+        if SUPPORT_AES:
+            info = AESCoder(self.passwd).encrypt(info)
+        header += pickle.dumps({"info": info, "text": not SUPPORT_AES})
+        if self.use_proxy:
+            target = str(self.target).encode("utf8")
+            source = self.info.code.encode("utf8")
+            header = b"%s$%s$%s%s" % (target, source, header, END_PATTERN)
+        self.socket.sendall(header)
+        
+        # makesure the request is confirmed
+        try:
+            rspn = self.socket.recv(1024)
+            if rspn.startswith(b"ERROR"):
+                callback_info(rspn.decode())
+                disconnect(self.socket)
+                return
+            rspn = rspn.replace(END_PATTERN, b"")
+            self.peer = pickle.loads(self.reciver(rspn))
+            if self.peer['code'] != IDCard(self.peer['uuid'], self.peer['mac'], self.peer['encrypt']).code:
+                self.peer = None
+        except Exception as e:
+            self.peer = None
+
+    def run(self, *args, **kwrds):
+        self.stage = STAGE_PRE
+        kwrds = self.prepare()
+        for retry in range(1, 1 + self.max_retry):
+            with self:
+                try:
+                    if self.peer:
+                        self.stage = STAGE_RUN
+                        result = self.process(**kwrds)
+                        callback_flush()
+                        if result is True:
+                            self.stage = STAGE_DONE
+                            return
+                except ConnectionResetError:
+                    pass
+                except ConnectionAbortedError:
+                    pass
+                except TimeoutError:
+                    pass
+            wait = 10 * retry # add 10 seconds per retry
+            self.stage = STAGE_RETRY
+            self.msg = "Retry connection in %d seconds" % wait
+            callback_info("Retry connection in %d seconds" % wait)
+            time.sleep(wait)
+        self.stage = STAGE_FAIL
---
+++ packages/BeeDrive/BeeDrive-0.2.1.8-py3-none-any.whl/BeeDrive/core/base/worker.py
@@ -0,0 +1,81 @@
+import re
+import threading
+import socket
+import traceback
+
+from .idcard import IDCard
+from ..encrypt import AESCoder, MD5Coder, SUPPORT_AES
+from ..utils import clean_coder, base_coder, disconnect
+from ..logger import callback_info, callback_flush
+from ..constant import END_PATTERN, TCP_BUFF_SIZE, STAGE_INIT, DISK_BUFF_SIZE
+
+
+END_PATTERN_COMPILE = re.compile(END_PATTERN)
+
+
+class BaseWorker(threading.Thread):
+    def __init__(self, sock, encrypt):
+        threading.Thread.__init__(self)
+        self.socket = sock             # socket instance
+        self.info = IDCard.create(encrypt)
+        self.use_proxy = False         # we try to connect the target directly
+        self.is_conn = False           # whether socket is connected
+        self.sender = clean_coder      # pipeline for sending data
+        self.reciver = clean_coder     # pipeline for reciving data
+        self.history = b"" 
+
+    def __enter__(self):
+        callback_error("NotImplementedError: Please rewrite BaseWorker.__enter__()", 6, self.info)
+        raise NotImplemented("NotImplementedError: Please rewrite BaseWorker.__enter__()")
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        disconnect(self.socket)
+        self.history = b""
+        self.is_conn = False
+        self.socket = None
+
+    def active(self):
+        assert self.socket is not None
+        assert self.sender and self.reciver
+        assert isinstance(self.info, IDCard)
+        self.is_conn = True
+
+    def build_socket(self):
+        if not self.socket:
+            self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.settimeout(None)
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, True)
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, True)
+
+    def build_pipeline(self, password=u""):
+        if SUPPORT_AES and self.info.encrypt:
+            ase, md5 = AESCoder(password), MD5Coder(password)
+            encrypt = lambda x: ase.encrypt(md5.encrypt(x))
+            decrypt = lambda x: md5.decrypt(ase.decrypt(x))
+            assert decrypt(encrypt(b"ABCD1234")) == b"ABCD1234"
+        else:
+            encrypt = decrypt = clean_coder
+            
+        if hasattr(self, 'target') and self.use_proxy:
+            head = str(self.target).encode("utf8") + b"$" + self.info.code.encode("utf8") + b"$"
+            forward = lambda text: head + text
+        else:
+            forward = clean_coder
+            
+        self.sender = lambda text: forward(encrypt(base_coder(text)))
+        self.reciver = decrypt
+
+    def send(self, text=''):
+        self.socket.sendall(self.sender(text) + END_PATTERN)
+
+    def recv(self):
+        msg = []
+        text = self.history + self.socket.recv(TCP_BUFF_SIZE)
+        while text:
+            texts = END_PATTERN_COMPILE.split(text)
+            msg.extend(self.reciver(_) for _ in texts[:-1] if _)
+            if not texts[-1] or sum(map(len, msg)) >= DISK_BUFF_SIZE:
+                self.history = texts[-1]
+                break
+            text = texts[-1] + self.socket.recv(TCP_BUFF_SIZE)
+        return b"".join(msg)
