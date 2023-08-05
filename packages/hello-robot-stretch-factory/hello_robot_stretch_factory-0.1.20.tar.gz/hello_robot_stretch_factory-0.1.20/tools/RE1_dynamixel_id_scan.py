df9e8dd9f5e70c120f52b4e4acb24fb9fb584e0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:55 +0000

    Add package hello-robot-stretch-factory/0.1.20/hello_robot_stretch_factory-0.1.20.tar.gz

---
+++ packages/hello-robot-stretch-factory/hello_robot_stretch_factory-0.1.20.tar.gz/hello_robot_stretch_factory-0.1.20/tools/RE1_dynamixel_id_scan.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+from stretch_body.dynamixel_XL430 import DynamixelXL430, DynamixelCommError
+import argparse
+import stretch_body.device
+d = stretch_body.device.Device(name='dummy_device') # to initialize logging config
+
+
+parser=argparse.ArgumentParser(description='Scan a dynamixel bus by ID for servos')
+parser.add_argument("usb", help="The dynamixel USB bus e.g.: /dev/hello-dynamixel-head")
+parser.add_argument("--baud", help="Baud rate (57600, 115200, or 1000000) [57600]", type=int,default=57600)
+args = parser.parse_args()
+
+print('Scanning bus %s at baud rate %d'%(args.usb,args.baud))
+print('----------------------------------------------------------')
+m=None
+try:
+    for id in range(25):
+        m = DynamixelXL430(id, args.usb,baud=args.baud)
+        try:
+            m.startup()
+        except DynamixelCommError:
+            print("ping failed for ID: " + str(id))
+            continue
+        m.do_ping()
+        m.stop()
+except (KeyboardInterrupt, SystemExit):
+    if m is not None:
+        m.stop()
---
+++ packages/hello-robot-stretch-factory/hello_robot_stretch_factory-0.1.20.tar.gz/hello_robot_stretch_factory-0.1.20/tools/RE1_dynamixel_reboot.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+from stretch_body.dynamixel_XL430 import *
+from stretch_body.hello_utils import *
+import argparse
+import stretch_body.device
+d = stretch_body.device.Device(name='dummy_device') # to initialize logging config
+
+
+parser=argparse.ArgumentParser(description='Reboot all of the Dynamixel servos on a bus')
+parser.add_argument("usb", help="The dynamixel USB bus e.g.: /dev/hello-dynamixel-head")
+parser.add_argument("--baud", help="Baud rate (57600, 115200, or 1000000) [57600]", type=int,default=57600)
+args = parser.parse_args()
+
+m=None
+try:
+    for id in range(25):
+        m = DynamixelXL430(id, args.usb,baud=args.baud)
+        #m.startup() #Don't startup as may be in error state
+        if (m.do_ping(verbose=False)):
+            m.do_reboot()
+        else:
+            m.stop()
+except (KeyboardInterrupt, SystemExit):
+    if m is not None:
+        m.stop()
