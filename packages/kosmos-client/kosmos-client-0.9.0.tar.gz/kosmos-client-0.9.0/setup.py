d50ae40881dae80cf263ebdadf5e6c3edd0ec266
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:26 +0000

    Add package kosmos-client/0.9.0/kosmos-client-0.9.0.tar.gz

---
+++ packages/kosmos-client/kosmos-client-0.9.0.tar.gz/kosmos-client-0.9.0/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+setuptools.setup(
+    name="kosmos-client",
+    version="0.9.0",
+    author="Jan Janssen",
+    author_email="Jan.Janssen@dfki.de",
+    description="Client to connect to the KosmoS Platform",
+    long_description="README",
+    # long_description_content_type="text/markdown",
+    url="https://kosmos-lab.de/python-client/",
+    install_requires=["websocket-client", "requests", "dataclasses-json","awaits"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    license="MIT",
+    license_file="LICENSE",
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
