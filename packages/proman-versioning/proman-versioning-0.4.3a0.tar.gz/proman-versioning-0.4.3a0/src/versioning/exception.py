806eaa5e5bd1f9f44c74a302ed1618b4250c15d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:54 +0000

    Add package proman-versioning/0.4.3a0/proman-versioning-0.4.3a0.tar.gz

---
+++ packages/proman-versioning/proman-versioning-0.4.3a0.tar.gz/proman-versioning-0.4.3a0/src/versioning/exception.py
@@ -0,0 +1,7 @@
+# copyright: (c) 2021 by Jesse Johnson.
+# license: MPL-2.0, see LICENSE for more details.
+"""Provide exceptions."""
+
+
+class PromanVersioningException(Exception):
+    """Provide base errors in PackageManager."""
