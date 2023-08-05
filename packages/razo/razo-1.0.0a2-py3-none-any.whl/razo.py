1c823b76c0602e31df89e4a3c11d313db99d1792
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:18:55 +0000

    Add package razo/1.0.0a2/razo-1.0.0a2-py3-none-any.whl

---
+++ packages/razo/razo-1.0.0a2-py3-none-any.whl/razo.py
@@ -0,0 +1,63 @@
+import time
+import setting
+import showinfo
+import sys
+
+rooting=False
+sudoroot=False
+nowsudo=False
+
+def setting():
+    a=input('Please set root password:')
+    with open('settings.py','w') as c:
+        c.write("rootpass='{0}'".format('a'))
+    
+def showinfo():
+    print('Razo 1.0.0a2.')
+    print('Type help for help.')
+    print('Still testing,please know.')
+
+
+h=(
+'''
+help:Show help.
+su:Ask for superuser license.
+shutdown:Shutdown razo.
+info:Show info.
+''')
+def wai(a):
+    global rooting
+    if a=='help':
+        print(h)
+    elif a=='su':
+        a=input('Please enter root password:')
+        import settings
+        if a==settings.rootpass:
+            rooting=True
+    elif a=='shutdown':
+        if rooting:
+            sys.exit(0)
+    elif a=='info':
+        showinfo()
+    else:
+        print('No this command.')
+
+
+
+try:
+    import settings
+    a=settings.rootpass
+except ImportError as e:
+    setting()
+
+
+do('showinfo')
+
+
+
+while True:
+    if rooting:
+        a=input('[root]>>>')
+    else:
+        a=input('[user]>>>')
+    wai(a)
