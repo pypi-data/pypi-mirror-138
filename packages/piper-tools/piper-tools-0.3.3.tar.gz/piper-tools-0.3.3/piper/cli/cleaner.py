d92d1df327b98135a67994d296f79cf683ba504b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:25 +0000

    Add package piper-tools/0.3.3/piper-tools-0.3.3.tar.gz

---
+++ packages/piper-tools/piper-tools-0.3.3.tar.gz/piper-tools-0.3.3/piper/cli/cleaner.py
@@ -0,0 +1,36 @@
+import logging
+import os
+import shutil
+
+from piper.cli.base import PiperCommand
+from piper.config.pipe import Pipe
+from piper.tools.venv import Virtualenv
+
+logger = logging.getLogger(__name__)
+
+
+class Cleaner(PiperCommand):
+
+    def _phase_name(self):
+        return "clean"
+
+    def _perform_tasks(self, pipe: Pipe, step: str):
+        if step == "post":
+            pass
+        else:
+            super()._perform_tasks(pipe, step)
+
+    def _manage(self, pipe: Pipe):
+
+        logger.info(f"Cleaning {pipe.name}")
+        to_remove = [
+            os.path.join(pipe.setup_py_folder, f"{pipe.package}.egg-info"),     # egg-info
+            Virtualenv(pipe).path,                                              # venv folder
+            pipe.build_folder                                                   # artifacts build folder
+        ]
+
+        # Clean
+        for path in to_remove:
+            if os.path.isdir(path):
+                shutil.rmtree(path)
+
---
+++ packages/piper-tools/piper-tools-0.3.3.tar.gz/piper-tools-0.3.3/piper/cli/setupper.py
@@ -0,0 +1,26 @@
+import os
+from pathlib import Path
+
+from piper.cli.base import PiperCommand
+from piper.config.pipe import Pipe
+from piper.tools.venv import Virtualenv
+
+
+class Setupper(PiperCommand):
+
+    def _phase_name(self) -> str:
+        return "setup"
+
+    def _manage(self, pipe: Pipe):
+
+        # Prepare the requirements file
+        if not os.path.isfile(pipe.requirements_file):
+            Path(pipe.requirements_file).touch()
+
+        # Create the virtualenv
+        venv = Virtualenv(pipe)
+        venv.create()
+        venv.update()
+
+        # Pip freeze
+        venv.freeze()
