d262392d62f97fcfdfe7dfe37b10e4d037a932cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:38 +0000

    Add package tententgclibrary/0.0.2/tententgclibrary-0.0.2.tar.gz

---
+++ packages/tententgclibrary/tententgclibrary-0.0.2.tar.gz/tententgclibrary-0.0.2/tententgclib/__init__.py
@@ -0,0 +1,23 @@
+def add_numbers(num1 , num2):
+    return num1 + num2
+
+
+def subtract_numbers(num1, num2):
+    return num1 - num2
+
+def multiply_numbers(num1, num2):
+    return num1 * num2
+
+def divide_numbers(num1, num2):
+    return num1 / num2
+
+def mean(a):
+    return sum(a) / len(a)
+
+def listfind_odd(a):
+    return [x for x in a if x % 2 == 1]
+
+def listfing_even(a):
+    return [x for x in a if x % 2 == 0]
+
+
