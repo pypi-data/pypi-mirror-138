d13ea9e5f4ba68b14549356bf1ac2c5a44514d9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:10 +0000

    Add package cachecore/0.1.2/cachecore-0.1.2.tar.gz

---
+++ packages/cachecore/cachecore-0.1.2.tar.gz/cachecore-0.1.2/src/cachecore/caches/dummy.py
@@ -0,0 +1,43 @@
+from cachecore.utils import MISSING_KEY
+
+
+class DummyCache:
+
+    def get(self, key):
+        return MISSING_KEY
+
+    def set(self, key, value, ttl=None):
+        pass
+
+    def add(self, key, value, ttl=None):
+        return False
+
+    def delete(self, key):
+        return False
+
+    def has_key(self, key):
+        return False
+
+    def get_many(self, *keys):
+        return [MISSING_KEY] * len(keys)
+
+    def set_many(self, mapping, ttl=None):
+        pass
+
+    def delete_many(self, *keys):
+        return [False] * len(keys)
+
+    def get_ttl(self, key):
+        return MISSING_KEY
+
+    def set_ttl(self, key, ttl=None):
+        pass
+
+    def incr(self, key, delta=1):
+        return delta
+
+    def decr(self, key, delta=1):
+        return -delta
+
+    def clear(self):
+        pass
---
+++ packages/cachecore/cachecore-0.1.2.tar.gz/cachecore-0.1.2/src/cachecore/caches/local.py
@@ -0,0 +1,86 @@
+import pickle
+from typing import Union
+
+from cachecore.utils import MissingKey, MISSING_KEY, Value
+
+
+class LocalCache:
+
+    def __init__(self):
+        self.serializer = pickle
+        self._data = {}
+
+    def _get_value(self, key: str) -> Union[Value, MissingKey]:
+        value = self._data.get(key)
+        if value is None:
+            return MISSING_KEY
+
+        value = self.serializer.loads(value)
+        
+        if value.is_expired():
+            self.delete(key)
+            return MISSING_KEY
+
+        return value
+
+    def _set_value(self, key: str, value: Value):
+        self._data[key] = self.serializer.dumps(value)        
+
+    def get(self, key):
+        value = self._get_value(key)
+        if value is MISSING_KEY:
+            return MISSING_KEY
+        return value.value
+
+    def set(self, key, value, ttl=None):
+        self._set_value(key, Value(value, ttl))
+
+    def add(self, key, value, ttl=None):
+        if self.has_key(key):
+            return False
+        self.set(key, value, ttl)
+        return True
+
+    def delete(self, key):
+        return self._data.pop(key, MISSING_KEY) is not MISSING_KEY
+
+    def has_key(self, key):
+        value = self._get_value(key)
+        return value is not MISSING_KEY
+
+    def get_many(self, *keys):
+        return [self.get(k) for k in keys]
+
+    def set_many(self, mapping, ttl=None):
+        for k, v in mapping:
+            self.set(k, v, ttl)
+
+    def delete_many(self, *keys):
+        return [self.delete(k) for k in keys]
+
+    def get_ttl(self, key):
+        value = self._get_value(key)
+        if value is MISSING_KEY:
+            return MISSING_KEY
+        return value.get_ttl()
+
+    def set_ttl(self, key, ttl=None):
+        value = self._get_value(key)
+        if value is not MISSING_KEY:
+            value.set_ttl(ttl)
+            self._set_value(key, value)
+
+    def incr(self, key, delta=1):
+        value = self._get_value(key)
+        if value is MISSING_KEY:
+            value = Value(0, None)
+
+        value.value += delta
+        self._set_value(key, value)
+        return value.value
+
+    def decr(self, key, delta=1):
+        return self.incr(key, -delta)
+
+    def clear(self):
+        self._data.clear()
