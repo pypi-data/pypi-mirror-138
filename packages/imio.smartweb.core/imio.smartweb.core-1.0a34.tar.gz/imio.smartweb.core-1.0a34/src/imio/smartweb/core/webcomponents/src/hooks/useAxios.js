8ede424fdf73019ce68e9b597c3ca602eaed0488
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:04 +0000

    Add package imio.smartweb.core/1.0a34/imio.smartweb.core-1.0a34.tar.gz

---
+++ packages/imio.smartweb.core/imio.smartweb.core-1.0a34.tar.gz/imio.smartweb.core-1.0a34/src/imio/smartweb/core/webcomponents/src/hooks/useAxios.js
@@ -0,0 +1,40 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const useAxios = (params) => {
+    const [response, setResponse] = useState(null);
+    const [error, setError] = useState("");
+    const [isLoading, setIsLoading] = useState(true);
+    const [isMore, setIsMore] = useState(false);
+
+    const fetchData = async (params) => {
+        setIsLoading(true);
+
+        if (params.load) {
+            setIsMore(true);
+        } else {
+            setIsMore(false);
+        }
+        if (Object.keys(params.params).length == 0) {
+            setResponse(null);
+            return;
+        } else {
+            try {
+                const res = await axios.request(params);
+                setResponse(res.data);
+                setError(null);
+            } catch (err) {
+                setError(err);
+            } finally {
+                setIsLoading(false);
+            }
+        }
+    };
+
+    useEffect(() => {
+        fetchData(params);
+    }, [params.params]);
+    return { response, error, isLoading, isMore };
+};
+
+export default useAxios;
