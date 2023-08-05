d4a652ce8b09502dacc2f0b45459c06240dbab05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:52 +0000

    Add package pyfyre/0.4.3/pyfyre-0.4.3-py3-none-any.whl

---
+++ packages/pyfyre/pyfyre-0.4.3-py3-none-any.whl/pyfyre/globals/__init__.py
@@ -0,0 +1,26 @@
+
+class Globals:
+    
+    DEBUG = False
+
+    # The Parent of all widgets object stored here
+    # from `runApp`'s first parameter.
+    __PARENT__ = None
+
+
+    # The current pathname location used for routing.
+    __LOC__ = None
+
+    # Dynamic routes
+    DYNAMIC_ROUTES = []
+    
+    # For checking if it's the first time initializing the Router
+    PATH_INITIALIZED = False
+
+
+    # All the event objects for firing.
+    __EVENTS__ = {}
+
+
+    # Path of Assets folder
+    __ASSETS__ = ""
---
+++ packages/pyfyre/pyfyre-0.4.3-py3-none-any.whl/pyfyre/widgets/states.py
@@ -0,0 +1,63 @@
+"""PyFyre States"""
+
+from pyfyre.globals import Globals
+from pyfyre.widgets.widget import Widget
+from pyfyre.core.exceptions import RenderError
+
+class UsesState:
+    """Create a component that is stateful.
+
+    If you think the component will update again and again,
+    you might want to use [UsesState] for that component.
+
+    This allows you to rerender the component when data
+    changes by calling `self.update()` method.
+
+    This also wraps the uncaught errors on `self.onerror` method
+    where you can also override and create your own UI error.
+
+    Your [UsesState] method must override the build method returning
+    an another component or a Widget.
+    """
+
+    def __init__(self):
+        self.domElement = None
+    
+    def build(self):
+        pass
+
+    def dom(self):
+        try:
+            self.domElement = self.build().dom()
+            return self.domElement
+        except Exception as e:
+            if Globals.DEBUG: raise e
+            
+            self.domElement = self.onerror(e).dom()
+            return self.domElement
+
+    def onerror(self, e):
+        """
+        Handle errors on every component separately. When an error
+        interrupts the `build` method on the component, this `onerror`
+        method will be fired and you can handle it professionally without
+        blowing the entire app.
+        
+        Override this method by declaring another `onerror` method on your
+        component and taking another parameter where the error exception will
+        pass. You must return a widget in which the UI you want your
+        users to see temporarily.
+        """
+        
+        return RenderError("Oh no! Something went wrong. We're working on fixing it.", e)
+
+    def update(self):
+        """
+        Updates the DOM element. Instead of painting the app again
+        for state change, only update the DOM where the change happens.
+        """
+        
+        parentNode = self.domElement.parentNode
+        self.domElement.remove()
+        self.domElement = self.dom()
+        parentNode.appendChild(self.domElement)
