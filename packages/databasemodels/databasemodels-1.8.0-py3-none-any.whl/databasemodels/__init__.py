c7b0c0773d2d94b002c5916d7d9cefb74c0001d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:08 +0000

    Add package databasemodels/1.8.0/databasemodels-1.8.0-py3-none-any.whl

---
+++ packages/databasemodels/databasemodels-1.8.0-py3-none-any.whl/databasemodels/__init__.py
@@ -0,0 +1,13 @@
+from .wrapper import *
+from . import datatypes
+from .connection import *
+from .columns import *
+from .protocols import *
+from .exceptions import *
+from .version import *
+
+# TODO: Implement Interval
+# TODO: Additional SQL parameter to createTable
+# TODO: Implement Fixed Point number type for numeric type
+
+version = Version(1, 8, 0)
