8f1d7f9f9519e8d54aaa9c6200c85425ec81315a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:16:18 +0000

    Add package envass/0.0.4/envass-0.0.4.tar.gz

---
+++ packages/envass/envass-0.0.4.tar.gz/envass-0.0.4/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    README = f.read()
+
+VERSION = '0.0.4'
+DESCRIPTION = 'ENVASS ENVironmental data quality ASSurance.'
+
+setup(
+    name="envass",
+    version=VERSION,
+    author="James Runnalls",
+    author_email="<james.runnalls@eawag.ch>",
+    description=DESCRIPTION,
+    long_description=README,
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    url='https://github.com/eawag-surface-waters-research/envass',
+    install_requires=[],
+    license="MIT",
+    keywords=['python', 'ENVASS', 'quality assurance', 'environmental data'],
+)
