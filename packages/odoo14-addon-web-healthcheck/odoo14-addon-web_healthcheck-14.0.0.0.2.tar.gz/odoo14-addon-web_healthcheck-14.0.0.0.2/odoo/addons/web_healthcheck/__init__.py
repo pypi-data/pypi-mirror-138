32875fd3b8b0f7344a4e5aead5996a204e585603
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:33 +0000

    Add package odoo14-addon-web-healthcheck/14.0.0.0.2/odoo14-addon-web_healthcheck-14.0.0.0.2.tar.gz

---
+++ packages/odoo14-addon-web-healthcheck/odoo14-addon-web_healthcheck-14.0.0.0.2.tar.gz/odoo14-addon-web_healthcheck-14.0.0.0.2/odoo/addons/web_healthcheck/__init__.py
@@ -0,0 +1,3 @@
+# Copyright (C) 2022 Jorge L. Juarez (https://github.com/JorgeJuarezM)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from . import controllers  # noqa: F401
---
+++ packages/odoo14-addon-web-healthcheck/odoo14-addon-web_healthcheck-14.0.0.0.2.tar.gz/odoo14-addon-web_healthcheck-14.0.0.0.2/odoo/addons/web_healthcheck/__manifest__.py
@@ -0,0 +1,6 @@
+{
+    "name": "Odoo Web Health Check",
+    "version": "14.0.0.0.2",
+    "author": "JorgeJuarezM",
+    "depends": ["web"],
+}
