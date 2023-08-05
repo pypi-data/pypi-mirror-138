9a5813bf63e39f3c1f53bf06f1c903e835d7f68d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:18:49 +0000

    Add package dzdsu/1.2.6/dzdsu-1.2.6.tar.gz

---
+++ packages/dzdsu/dzdsu-1.2.6.tar.gz/dzdsu-1.2.6/dzdsu/update.py
@@ -0,0 +1,57 @@
+"""Game and mod updates."""
+
+from itertools import chain
+from logging import getLogger
+from subprocess import CompletedProcess, run
+
+from dzdsu.constants import DAYZ_APP_ID, STEAMCMD
+from dzdsu.mods import Mod
+from dzdsu.server import Server
+
+
+__all__ = ['Updater', 'steamcmd']
+
+
+LOGGER = getLogger('Updater')
+
+
+class Updater:
+    """SteamCMD wrapper to update server and mods."""
+
+    def __init__(self, steam_user_name: str):
+        self.steam_user_name = steam_user_name
+
+    def update_server(self, server: Server) -> CompletedProcess:
+        """Updates the server."""
+        return steamcmd(
+            '+force_install_dir', str(server.base_dir),
+            '+login', self.steam_user_name,
+            '+app_update', str(server.app_id), 'validate'
+        )
+
+    def update_mods(self, server: Server) -> CompletedProcess:
+        """Updates the server's mods."""
+        return steamcmd(
+            '+force_install_dir', str(server.base_dir),
+            '+login', self.steam_user_name,
+            *chain.from_iterable(map(update_mod_command, server.mods))
+        )
+
+    def update(self, server: Server) -> None:
+        """Updates server and mods."""
+        self.update_server(server)
+        self.update_mods(server)
+
+
+def steamcmd(*commands: str) -> CompletedProcess:
+    """Invokes steamcmd and exits."""
+
+    return run([STEAMCMD, *commands, '+quit'], check=True)
+
+
+def update_mod_command(mod: Mod) -> list[str]:
+    """Returns a steamcmd command to update the given mod."""
+
+    return [
+        '+workshop_download_item', str(DAYZ_APP_ID), str(mod.id), 'validate'
+    ]
