8b2dadd8483d9bbaa5a355f9922b066b9fc48d0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:17:59 +0000

    Add package nanamilang/0.24.0/nanamilang-0.24.0-py3-none-any.whl

---
+++ packages/nanamilang/nanamilang-0.24.0-py3-none-any.whl/nanamilang/datatypes/undefined.py
@@ -0,0 +1,43 @@
+"""NanamiLang Undefined Data Type Class"""
+
+# This file is a part of NanamiLang Project
+# This project licensed under GNU GPL version 2
+# Initially made by @jedi2light (aka Stoian Minaiev)
+
+
+from .base import Base
+from .string import String
+from ._exports import export
+
+
+class Undefined(Base):
+    """NanamiLang Undefined Type"""
+
+    _hashed = hash('undefined')
+    name: str = 'Undefined'
+    _expected_type = str
+    _python_reference: str
+    purpose = 'To mark as an undefined at parse-time'
+
+    def format(self, **_) -> str:
+        """NanamiLang Undefined, format() method implementation"""
+
+        return 'undefined'
+
+    def origin(self) -> str:
+        """NanamiLang Undefined, origin() method implementation"""
+
+        return self._python_reference
+
+    @export()
+    def nominal(self) -> String:
+        """NanamiLang Undefined nominal() method implementation"""
+
+        return String(self.name)
+
+    def reference(self) -> None:
+        """NanamiLang Undefined, reference() method implementation"""
+
+        return None
+
+    # Due to architecture issues, self._python_reference is a string, but we need to return a None
