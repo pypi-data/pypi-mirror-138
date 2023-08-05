b2bf8360c1a0532e674e8bfadd7ff3d4850694a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:02 +0000

    Add package pyonenet/0.0.1/pyonenet-0.0.1.tar.gz

---
+++ packages/pyonenet/pyonenet-0.0.1.tar.gz/pyonenet-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pyonenet',
+    version='0.0.1',
+    description='SDK for onenet',
+    packages=['pyonenet'],
+    py_modules=['tool'],
+    author='Xiao-qiao',
+    long_description='test'
+    
+)
