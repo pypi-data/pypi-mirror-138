b64ecd8ba56096900b96284695adf8e76746f05e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:41 +0000

    Add package dg-commons/0.0.19/dg-commons-0.0.19.tar.gz

---
+++ packages/dg-commons/dg-commons-0.0.19.tar.gz/dg-commons-0.0.19/src/dg_commons/__init__.py
@@ -0,0 +1,18 @@
+__version__ = "0.0.19"
+
+from logging import INFO
+
+import contracts
+from zuper_commons.logs import ZLogger
+
+logger = ZLogger(__name__)
+
+logger.setLevel(level=INFO)
+
+contracts.disable_all()
+
+from .seq import *
+from .geo import *
+from .game_types import *
+from .utils_types import *
+from .utils_toolz import *
