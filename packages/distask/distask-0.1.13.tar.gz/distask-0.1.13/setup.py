0dea6be07b8bfcdd9b8c49dc3a0cd5e893d4c4f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:50 +0000

    Add package distask/0.1.13/distask-0.1.13.tar.gz

---
+++ packages/distask/distask-0.1.13.tar.gz/distask-0.1.13/setup.py
@@ -0,0 +1,44 @@
+from setuptools import setup, find_packages
+from os import path
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the relevant file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name="distask",
+    version="0.1.13",
+    keywords="distribute scheduling cron",
+    description="a distribute task scheduler ",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tickbh/distask",
+    license="MIT Licence",
+    author="tickbh",
+    author_email="tickdream125@hotmail.com",
+    packages=[
+        'distask', 
+        'distask/locks', 
+        'distask/datastores', 
+        'distask/schedulers', 
+        'distask/serializers', 
+        'distask/tiggers', 
+        'distask/tiggers/cron'
+    ],
+    # py_modules=['distask/*', 'distask/locks/*'],
+    platforms="any",
+    python_requires='>=3.6',
+    include_package_data=True,
+    install_requires=[
+        'tzlocal',
+        'pytz',
+        'kazoo',
+        'redlock',
+        'pymongo'
+    ],
+    extras_require={
+        'extra': ['']
+    }
+
+)
