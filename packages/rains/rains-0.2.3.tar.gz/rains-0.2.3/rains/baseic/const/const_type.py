4e243fe7770d42c4329341db6beb68280cebebaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:08 +0000

    Add package rains/0.2.3/rains-0.2.3.tar.gz

---
+++ packages/rains/rains-0.2.3.tar.gz/rains-0.2.3/rains/baseic/const/const_type.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# coding=UTF-8
+#
+# Copyright 2022. quinn.7@foxmail.com All rights reserved.
+# Author :: cat7
+# Email  :: quinn.7@foxmail.com
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, 
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# =============================================================================
+"""
+[ 类型常量 ]
+
+"""
+
+
+# ----------------------------------
+class ConstType(object):
+    """ [ 类型常量 ] """
+
+    PROJECT_TYPE_WEB: str = 'WEB'
+    """ [ WEB项目 ] """
+
+    PROJECT_TYPE_API: str = 'API'
+    """ [ API项目 ] """
+
+    TASK_TYPE_CODE: str = 'CODE'
+    """ [ 代码任务 ] """
+
+    TASK_TYPE_JSON: str = 'JSON'
+    """ [ JSON任务 ] """
