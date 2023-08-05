040597d0f3df4a20a0eeb8fe2117a30418f9e684
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:18:38 +0000

    Add package quickstruct/0.1.1/quickstruct-0.1.1.tar.gz

---
+++ packages/quickstruct/quickstruct-0.1.1.tar.gz/quickstruct-0.1.1/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['quickstruct']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'quickstruct',
+    'version': '0.1.1',
+    'description': 'A small library to ease the creation, usage, serialization and deserialization of C structs.',
+    'long_description': None,
+    'author': 'Binyamin Y Cohen',
+    'author_email': 'binyamincohen555@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': '',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
