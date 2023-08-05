7455ec6c334c331387492c9fbe50b7c666e80367
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:15:40 +0000

    Add package home-connect-async/0.4.1/home-connect-async-0.4.1.tar.gz

---
+++ packages/home-connect-async/home-connect-async-0.4.1.tar.gz/home-connect-async-0.4.1/home_connect_async/common.py
@@ -0,0 +1,22 @@
+""" Common classes shared across the code """
+
+class HomeConnectError(Exception):
+    """ Common exception class for the SDK """
+    def __init__(self, msg:str = None, code:int = None, response = None, inner_exception = None):
+        self.msg = msg
+        self.code = code
+        self.response = response
+        self.inner_exception = inner_exception
+        if response:
+            self.error_key = response.error_key
+            self.error_description = response.error_description
+            if not code: self.code = response.status
+        else:
+            self.error_key = None
+            self.error_description = None
+
+        super().__init__(msg, code, self.error_key, self.error_description, inner_exception)
+
+class DeviceOfflineError(HomeConnectError):
+    def __init__(self, msg: str = None, code: int = None, response=None):
+        super().__init__(msg, code, response)
