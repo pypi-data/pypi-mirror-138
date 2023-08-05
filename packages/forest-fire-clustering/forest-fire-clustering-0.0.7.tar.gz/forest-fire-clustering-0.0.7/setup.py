0cf4a37a02dcf92b43e1baaa2d5a0f4714c135d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:57 +0000

    Add package forest-fire-clustering/0.0.7/forest-fire-clustering-0.0.7.tar.gz

---
+++ packages/forest-fire-clustering/forest-fire-clustering-0.0.7.tar.gz/forest-fire-clustering-0.0.7/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="forest-fire-clustering", # Replace with your own username
+    version="0.0.7",
+    author="Flynn Chen",
+    author_email="zhanlinchen348@gmail.com",
+    description="Clustering Method Inspired by Forest Fire Dynamics",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/flynn-chen/forest-fire-clustering.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
