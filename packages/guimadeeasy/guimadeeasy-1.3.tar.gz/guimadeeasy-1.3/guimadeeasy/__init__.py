5486354f8788dfaf2ebf49a45a824b292e235444
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:15:45 +0000

    Add package guimadeeasy/1.3/guimadeeasy-1.3.tar.gz

---
+++ packages/guimadeeasy/guimadeeasy-1.3.tar.gz/guimadeeasy-1.3/guimadeeasy/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+from guimadeeasy.calculator import *
+from guimadeeasy.notepad import *
+from guimadeeasy.todo import *
+from guimadeeasy.wheather import *
+from guimadeeasy.snake import *
+from guimadeeasy.awesomeclock import *
---
+++ packages/guimadeeasy/guimadeeasy-1.3.tar.gz/guimadeeasy-1.3/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_packages
+from sys import exit, stderr
+import os
+
+try:
+    import tkinter
+except:
+    print("tkinter not found.", file=stderr)
+    if os.name == "posix":
+        print('use "apt install python3-tk" to install.')
+    exit(1)
+
+setup(
+    name="guimadeeasy",
+    version="1.3",
+    description="this is just a fun challenge for my friend",
+    packages=["guimadeeasy"],
+    long_description="""
+
+    This is my first package, I think these GUIs are super safe. Prove me wrong :->
+
+    Examples - 
+
+    >>> from guimadeeasy import *
+    >>> clock()
+    
+    >>> from guidmadeasy import *
+    >>> wheather()
+
+    >>> from guimadeeasy import *
+    >>> calculator()
+
+    >>> from guimadeeasy import *
+    >>> notepad()
+
+    >>> from guimadeeasy import *
+    >>> todo()
+
+    >>> from guimadeeasy import *
+    >>> snake()""",
+)
