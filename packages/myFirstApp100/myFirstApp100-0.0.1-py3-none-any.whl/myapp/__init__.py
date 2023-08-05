086af74f947814cb4a1a27a160d5c6c368d27419
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:01:21 +0000

    Add package myFirstApp100/0.0.1/myFirstApp100-0.0.1-py3-none-any.whl

---
+++ packages/myFirstApp100/myFirstApp100-0.0.1-py3-none-any.whl/myapp/__init__.py
@@ -0,0 +1,8 @@
+"""
+-*- encoding:utf-8 -*-
+==========================
+Author:carson.luan
+Email:carson.luan@meetsocial.com
+Time: 2022/2/9 14:13
+==========================
+"""
---
+++ packages/myFirstApp100/myFirstApp100-0.0.1-py3-none-any.whl/myapp/greet.py
@@ -0,0 +1,12 @@
+"""
+-*- encoding:utf-8 -*-
+==========================
+Author:carson.luan
+Email:carson.luan@meetsocial.com
+Time: 2022/2/9 14:21
+==========================
+"""
+
+
+def hello():
+    print('Hello, welcome to setuptools!')
