bf7d9f6b8b16afb7e7d71a7600038a1bbf04aa71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:02 +0000

    Add package TeamStrange/1.2/TeamStrange-1.2.tar.gz

---
+++ packages/TeamStrange/TeamStrange-1.2.tar.gz/TeamStrange-1.2/README.md
@@ -0,0 +1,8 @@
+# TeamStrange Package
+
+This is a small package for working with time series. The package is mainly build around the existing package pandas, 
+however, it focuses on editing specifically time series. The functionalities include import, export, 
+changing values in various ways, getting intervals and single data-points, adding, removing, finding and moving 
+data-points, and finally, creating peaks in the time series.\
+In order to use the TeamStrange Package, just typ "py -m pip install TeamStrange" and use "from TeamStrange import TimeSeries" 
+at the beginning of your code.
---
+++ packages/TeamStrange/TeamStrange-1.2.tar.gz/TeamStrange-1.2/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+setuptools.setup(
+    name="TeamStrange",
+    version="1.2",
+    author="Martin Clever",
+    author_email="martin.clever@rwth-aachen.de",
+    description="A small package for working with time series. Created by Team Strange.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://git.rwth-aachen.de/swc/teaching/winter-term-21-22/lab-timeseries-synthesizer/team-strange/-/tree/main",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    install_requires=['pandas'],
+    packages=setuptools.find_packages(where="src"),
+
+    python_requires=">=3.6",
+)
