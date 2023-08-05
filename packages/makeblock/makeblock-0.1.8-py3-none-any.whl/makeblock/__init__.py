c47e8ff79db82ffc197a172f30799fbe1adc90f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:42 +0000

    Add package makeblock/0.1.8/makeblock-0.1.8-py3-none-any.whl

---
+++ packages/makeblock/makeblock-0.1.8-py3-none-any.whl/makeblock/__init__.py
@@ -0,0 +1,36 @@
+#from .comm import SerialPort
+#from .comm import mlink
+
+
+import sys
+import signal
+
+_ports = []
+_threads = []
+
+def add_port(port):
+    global _ports
+    _ports.append(port)
+
+def add_thread(thread):
+    global _threads
+    thread.daemon = True
+    _threads.append(thread)
+
+def quit():
+    __exiting(0,0)
+    
+def __exiting(signal, frame):
+    global _ports
+    global _threads
+    print("exiting")
+    for port in _ports:
+        port.exit()
+    for thread in _threads:
+        thread.exit()
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, __exiting)
+
+from .boards import *
+
