552eae241e15d38481432ed23fc5857fa244dfa1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:14:45 +0000

    Add package eo-learn-coregistration/1.0.0/eo_learn_coregistration-1.0.0-py3-none-any.whl

---
+++ packages/eo-learn-coregistration/eo_learn_coregistration-1.0.0-py3-none-any.whl/eolearn/coregistration/__init__.py
@@ -0,0 +1,13 @@
+"""
+A collection of tools and EOTasks for image co-registration
+"""
+
+from .coregistration import (
+    RegistrationTask,
+    InterpolationType,
+    ECCRegistrationTask,
+    PointBasedRegistrationTask,
+    ThunderRegistrationTask,
+)
+
+__version__ = "1.0.0"
