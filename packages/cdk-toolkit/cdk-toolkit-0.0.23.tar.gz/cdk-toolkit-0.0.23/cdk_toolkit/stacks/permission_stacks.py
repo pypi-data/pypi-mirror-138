e4886888480f3e509c35855264a1d85f444f6e70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:39:50 +0000

    Add package cdk-toolkit/0.0.23/cdk-toolkit-0.0.23.tar.gz

---
+++ packages/cdk-toolkit/cdk-toolkit-0.0.23.tar.gz/cdk-toolkit-0.0.23/cdk_toolkit/stacks/permission_stacks.py
@@ -0,0 +1,23 @@
+from aws_cdk import Stack 
+from constructs import Construct
+
+from cdk_toolkit import permission  
+
+
+class IAMRolesStack(Stack): 
+    '''
+    This Stack Creates all the necessary IAM roles for other Toolkit Stacks
+    '''
+    def __init__(self, scope: Construct, construct_id: str, iam_roles_dir: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        ##
+        ## CodeBuild Service Role
+        codebuild_svc_role_dictionary = permission.readIAMRolePolicyStatements(iam_roles_dir, "codebuild_svc_role")
+        self.codebuild_svc_role = permission.createIAMServiceRole(self, codebuild_svc_role_dictionary)
+
+
+
+
+
+
