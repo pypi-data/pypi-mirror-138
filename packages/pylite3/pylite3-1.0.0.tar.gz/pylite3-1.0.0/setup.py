5dbb6427de4baa3a753c27368c6fcf63ca87e985
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:06:44 +0000

    Add package pylite3/1.0.0/pylite3-1.0.0.tar.gz

---
+++ packages/pylite3/pylite3-1.0.0.tar.gz/pylite3-1.0.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+PACKAGE_NAME = "pylite3"
+VERSION = "1.0.0"
+
+classifiers=[
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: Microsoft :: Windows",
+    "Operating System :: MacOS",
+    "Operating System :: POSIX :: Linux",
+    "Programming Language :: Python :: 3.8",
+]
+
+setup(
+    name=PACKAGE_NAME,
+    version=VERSION,
+    license="MIT",
+    classifiers=classifiers,
+    url="https://github.com/nguyenvantat1182002/pylite3",
+    author="nguyenvantat1182002",
+    author_email="nguyenvantat1182002@gmail.com",
+    packages=find_packages(),
+)
