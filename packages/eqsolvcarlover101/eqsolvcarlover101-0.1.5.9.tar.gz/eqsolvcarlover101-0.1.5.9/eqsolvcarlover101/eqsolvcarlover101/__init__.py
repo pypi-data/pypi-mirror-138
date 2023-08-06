a60443f967a5f9814b2820eea5a82483d8c36e62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:29:25 +0000

    Add package eqsolvcarlover101/0.1.5.9/eqsolvcarlover101-0.1.5.9.tar.gz

---
+++ packages/eqsolvcarlover101/eqsolvcarlover101-0.1.5.9.tar.gz/eqsolvcarlover101-0.1.5.9/eqsolvcarlover101/eqsolvcarlover101/__init__.py
@@ -0,0 +1,9 @@
+import quadsolve
+import slopeint
+import commands
+import help
+
+quadsolve = __import__("quadsolve",globals(),locals(),[],0)
+slopeint = __import__("slopeint",globals(),locals(),[],0)
+commands = __import__("commands",globals(),locals(),[],0)
+help = __import__("help",globals(),locals(),[],0)
