4e77da07542af89358795d37a1d4654662421b38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:30:34 +0000

    Add package openlabcluster/0.0.11/openlabcluster-0.0.11.tar.gz

---
+++ packages/openlabcluster/openlabcluster-0.0.11.tar.gz/openlabcluster-0.0.11/openlabcluster/gui/__init__.py
@@ -0,0 +1,16 @@
+"""
+DeepLabCut2.0 Toolbox (deeplabcut.org)
+© A. & M. Mathis Labs
+https://github.com/AlexEMG/DeepLabCut
+Please see AUTHORS for contributors.
+
+https://github.com/AlexEMG/DeepLabCut/blob/master/AUTHORS
+Licensed under GNU Lesser General Public License v3.0
+
+"""
+
+from openlabcluster.gui.create_new_project import *
+from openlabcluster.gui.load_project import *
+from openlabcluster.gui.media import *
+from openlabcluster.gui.train_network import *
+from openlabcluster.gui.welcome import *
---
+++ packages/openlabcluster/openlabcluster-0.0.11.tar.gz/openlabcluster-0.0.11/openlabcluster/training_utils/__init__.py
@@ -0,0 +1,36 @@
+"""
+DeepLabCut2.0 Toolbox (deeplabcut.org)
+© A. & M. Mathis Labs
+https://github.com/AlexEMG/DeepLabCut
+Please see AUTHORS for contributors.
+
+https://github.com/AlexEMG/DeepLabCut/blob/master/AUTHORS
+Licensed under GNU Lesser General Public License v3.0
+"""
+
+from openlabcluster.training_utils.config import *
+# from openlabcluster.training_utils.dataset import *
+from openlabcluster.training_utils.default_config import *
+# from openlabcluster.training_utils.evaluate import *
+# from openlabcluster.training_utils.evaluate_multianimal import (
+#     evaluate_multianimal_crossvalidate,
+# )
+# from openlabcluster.training_utils.export import export_model
+# from openlabcluster.training_utils.lib.nms_cython import *
+# from openlabcluster.training_utils.models import *
+# from openlabcluster.training_utils.nnet import *
+# from openlabcluster.training_utils.predict_videos import *
+#
+# # from openlabcluster.training_utils.predict_multianimal import convert_detections2tracklet
+# from openlabcluster.training_utils.test import *
+from openlabcluster.training_utils.train import *
+from openlabcluster.training_utils.training import *
+# from openlabcluster.training_utils.util import *
+# from openlabcluster.training_utils.visualizemaps import *
+
+"""
+import os
+if os.environ.get('DLClight', default=False) == 'True':
+    pass
+else:
+"""
