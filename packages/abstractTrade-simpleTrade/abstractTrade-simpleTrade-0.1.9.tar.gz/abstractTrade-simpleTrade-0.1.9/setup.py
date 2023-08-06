c6b7d1c031ea5d807a9ef08ea1266e36fb211a35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:56 +0000

    Add package abstractTrade-simpleTrade/0.1.9/abstractTrade-simpleTrade-0.1.9.tar.gz

---
+++ packages/abstractTrade-simpleTrade/abstractTrade-simpleTrade-0.1.9.tar.gz/abstractTrade-simpleTrade-0.1.9/setup.py
@@ -0,0 +1,15 @@
+import pathlib
+import setuptools
+
+long_description = (pathlib.Path(__file__).parent / "README.md").read_text()
+
+setuptools.setup(
+    name='abstractTrade-simpleTrade',
+    version='0.1.9',
+    license='MIT',
+    author="Joao Paulo Euko",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=setuptools.find_packages(),
+    install_requires=[],
+)
