780134d9adf47b9058a017562e007f14a2dafd7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:19:43 +0000

    Add package h5mapper/0.2.0/h5mapper-0.2.0-py3-none-any.whl

---
+++ packages/h5mapper/h5mapper-0.2.0-py3-none-any.whl/h5mapper/__init__.py
@@ -0,0 +1,9 @@
+__version__ = '0.2.0'
+
+from .core import *
+from .crud import *
+from .features import *
+from .file_walker import *
+from .serve import *
+from .typed_files import *
+from .utils import *
