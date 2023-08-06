5bbfe848205da4fa7c8e2ccf90f871d7f91cbaad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:31:29 +0000

    Add package openlineage-airflow/0.5.2/openlineage_airflow-0.5.2-py3-none-any.whl

---
+++ packages/openlineage-airflow/openlineage_airflow-0.5.2-py3-none-any.whl/openlineage/airflow/__init__.py
@@ -0,0 +1,22 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# -*- coding: utf-8 -*-
+from pkg_resources import parse_version
+from airflow.version import version as AIRFLOW_VERSION
+
+__author__ = """OpenLineage"""
+__version__ = "0.5.2"
+
+if parse_version(AIRFLOW_VERSION) < parse_version("2.0.0"):
+    from openlineage.airflow.dag import DAG
+    __all__ = ["DAG"]
