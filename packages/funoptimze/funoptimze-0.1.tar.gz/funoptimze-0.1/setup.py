738a335599adff1b983a74f3d469934a94509bf9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:07 +0000

    Add package funoptimze/0.1/funoptimze-0.1.tar.gz

---
+++ packages/funoptimze/funoptimze-0.1.tar.gz/funoptimze-0.1/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+     name='funoptimze',  
+     version='0.1',
+     author="Ahmed Mrabet",
+     author_email="ahmedmrabet.002@gmail.com",
+     description="this is a package with a lot of method of optimization",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
