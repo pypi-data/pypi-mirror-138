28898161df408cb0d60836feff99014b1035ab14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:25:38 +0000

    Add package joungwoo-test/1.0/joungwoo_test-1.0-py3-none-any.whl

---
+++ packages/joungwoo-test/joungwoo_test-1.0-py3-none-any.whl/test/__init__.py
@@ -0,0 +1,5 @@
+from test import joungwoo_test
+
+__version__ = '1.0'
+__author__ = 'joungwoo'
+
---
+++ packages/joungwoo-test/joungwoo_test-1.0-py3-none-any.whl/test/joungwoo_test.py
@@ -0,0 +1,6 @@
+def papering(string, number):
+    for i in range(number):
+        print(string)
+
+if __name__ == "__main__":
+    papering("test", 10)
