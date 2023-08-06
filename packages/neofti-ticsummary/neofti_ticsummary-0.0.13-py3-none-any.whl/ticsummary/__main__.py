53d455cdb7c0b42d3d5b45fa45cc482c558b2056
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:20:31 +0000

    Add package neofti-ticsummary/0.0.13/neofti_ticsummary-0.0.13-py3-none-any.whl

---
+++ packages/neofti-ticsummary/neofti_ticsummary-0.0.13-py3-none-any.whl/ticsummary/__main__.py
@@ -0,0 +1,20 @@
+from PyQt6 import QtCore, QtGui, QtWidgets   
+from ticsummary import MainWindow, ConnectionConfigurationDialog, DatabaseMYSQL, Plot
+
+
+class EntryPoint:
+    sqlParameters:DatabaseMYSQL.ParameterConnection
+    profilePlot1: Plot
+    
+    def run(self):
+        import sys
+        app = QtWidgets.QApplication(sys.argv)
+        self.mainWindow = MainWindow.MainWindow()
+        
+        self.mainWindow.show()
+        sys.exit(app.exec())
+    
+    
+if __name__ == '__main__':
+    entryPoint = EntryPoint()
+    entryPoint.run()
