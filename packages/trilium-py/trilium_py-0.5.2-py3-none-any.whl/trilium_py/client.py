8211bfd38ca2974d6398372d27db61e971d0a3e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:20:09 +0000

    Add package trilium-py/0.5.2/trilium_py-0.5.2-py3-none-any.whl

---
+++ packages/trilium-py/trilium_py-0.5.2-py3-none-any.whl/trilium_py/client.py
@@ -0,0 +1,248 @@
+import requests
+
+from .utils.param_util import format_query_string, clean_param
+
+
+class ETAPI:
+
+    def __init__(self, server_url: str, token: str = None):
+        self.server_url = server_url
+        self.token = token
+
+    def get_header(self) -> dict:
+        return {
+            'Authorization': self.token,
+        }
+
+    def login(self, password: str) -> str:
+        """
+        generate token with password
+        """
+        url = f'{self.server_url}/etapi/auth/login'
+
+        data = {'password': password}
+
+        res = requests.post(url, data=data)
+        if res.status_code == 201:
+            self.token = res.json()['authToken']
+            return self.token
+        else:
+            print(res.json()['message'])
+            return None
+
+    def logout(self, token_to_destroy: str = None) -> bool:
+        """
+        destroy token
+        """
+
+        if not token_to_destroy:
+            token_to_destroy = self.token
+
+        if not token_to_destroy:
+            return False
+
+        url = f'{self.server_url}/etapi/auth/logout'
+        headers = {
+            'Authorization': token_to_destroy,
+        }
+        res = requests.post(url, headers=headers)
+        if res.status_code == 204:
+            print('logout successfully')
+            return True
+        return False
+
+    def search_note(self, search: str, **params) -> dict:
+        """
+
+        :param search:
+        :param params:
+        :return:
+        """
+        result = []
+
+        url = f'{self.server_url}/etapi/notes'
+        params['search'] = search
+        res = requests.get(url, params=format_query_string(params), headers=self.get_header())
+        return res.json()
+
+    def get_note(self, noteId: str) -> dict:
+        """
+        get note by note id
+        root note's id is just "root"
+
+        :param noteId:
+        :return:
+        """
+        url = f'{self.server_url}/etapi/notes/{noteId}'
+        res = requests.get(url, headers=self.get_header())
+        return res.json()
+
+    def create_note(self, parentNoteId: str, title: str, type: str, mime: str = None, content=None,
+                    notePosition: int = None, prefix: str = None,
+                    isExpanded: str = None, noteId: str = None,
+                    branchId: str = None) -> dict:
+        """
+        Actually it's create or update, if noteId already exists, the corresponding note will be updated
+        :param parentNoteId:
+        :param title:
+        :param type:
+        :param mime:
+        :param content:
+        :param notePosition:
+        :param prefix:
+        :param isExpanded:
+        :param noteId:
+        :param branchId:
+        :return:
+        """
+        url = f'{self.server_url}/etapi/create-note'
+
+        params = {
+            "parentNoteId": parentNoteId,
+            "title": title,
+            "type": type,
+            "mime": mime,
+            "content": content,
+            "notePosition": notePosition,
+            "prefix": prefix,
+            "isExpanded": isExpanded,
+            "noteId": noteId,
+            "branchId": branchId
+        }
+        res = requests.post(url, json=clean_param(params), headers=self.get_header())
+
+        return res.json()
+
+    def patch_note(self, noteId: str, title: str = None, type: str = None, mime: str = None) -> dict:
+        url = f'{self.server_url}/etapi/notes/{noteId}'
+        params = {
+            "title": title,
+            "type": type,
+            "mime": mime,
+        }
+        res = requests.patch(url, json=clean_param(params), headers=self.get_header())
+        return res.json()
+
+    def delete_note(self, noteId: str) -> bool:
+        url = f'{self.server_url}/etapi/notes/{noteId}'
+        res = requests.delete(url, headers=self.get_header())
+        if res.status_code == 204:
+            return True
+        return False
+
+    def get_note_content(self, noteId: str) -> str:
+        url = f'{self.server_url}/etapi/notes/{noteId}/content'
+        res = requests.get(url, headers=self.get_header())
+        return res.content.decode('utf-8')
+
+    def update_note_content(self, noteId: str, content: str) -> bool:
+        """update note content"""
+        url = f'{self.server_url}/etapi/notes/{noteId}/content'
+        res = requests.put(url, data=content, headers={'content-type': 'text/plain', 'Authorization': self.token, })
+        if res.status_code == 204:
+            return True
+        return False
+
+    def get_branch(self, branchId: str) -> dict:
+        url = f'{self.server_url}/etapi/branches/{branchId}'
+        res = requests.get(url, headers=self.get_header())
+        return res.json()
+
+    def create_branch(self, branchId: str, noteId: str, parentNoteId: str, prefix: str, notePosition: int,
+                      isExpanded: bool, utcDateModified) -> dict:
+        # url = f'{self.server_url}/etapi/branches/{branchId}'
+        url = f'{self.server_url}/etapi/branches/'
+        params = {
+            "branchId": branchId,
+            "noteId": noteId,
+            "parentNoteId": parentNoteId,
+            "prefix": prefix,
+            "notePosition": notePosition,
+            "isExpanded": isExpanded,
+            "utcDateModified": utcDateModified
+        }
+        res = requests.post(url, json=clean_param(params), headers=self.get_header())
+        return res.json()
+
+    def patch_branch(self, branchId: str, notePosition: int, prefix: str, isExpanded: bool) -> dict:
+        url = f'{self.server_url}/etapi/branches/{branchId}'
+        params = {
+            "notePosition": notePosition,
+            "prefix": prefix,
+            "isExpanded": isExpanded,
+        }
+        res = requests.patch(url, json=clean_param(params), headers=self.get_header())
+        return res.json()
+
+    def delete_branch(self, branchId: str) -> bool:
+        url = f'{self.server_url}/etapi/branches/{branchId}'
+        res = requests.delete(url, headers=self.get_header())
+        if res.status_code == 204:
+            return True
+        return False
+
+    def get_attribute(self, attributeId: str) -> dict:
+        url = f'{self.server_url}/etapi/attributes/{attributeId}'
+        res = requests.get(url, headers=self.get_header())
+        return res.json()
+
+    def create_attribute(self, attributeId: str, noteId: str, type: str, name: str, value: str,
+                         isInheritable: bool) -> dict:
+        url = f'{self.server_url}/etapi/attributes/'
+        params = {
+            "attributeId": attributeId,
+            "noteId": noteId,
+            "type": type,
+            "name": name,
+            "value": value,
+            "isInheritable": isInheritable,
+        }
+        res = requests.post(url, json=clean_param(params), headers=self.get_header())
+        return res.json()
+
+    def patch_attribute(self, attributeId: str, value: str) -> dict:
+        url = f'{self.server_url}/etapi/attributes/{attributeId}'
+        params = {
+            "value": value,
+        }
+        res = requests.patch(url, json=clean_param(params), headers=self.get_header())
+        return res.json()
+
+    def delete_attribute(self, attributeId: str) -> bool:
+        url = f'{self.server_url}/etapi/attributes/{attributeId}'
+        res = requests.delete(url, headers=self.get_header())
+        if res.status_code == 204:
+            return True
+        return False
+
+    def refresh_note_ordering(self, parentNoteId: str) -> bool:
+        url = f'{self.server_url}/etapi/refresh-note-ordering/{parentNoteId}'
+        res = requests.post(url, headers=self.get_header())
+        if res.status_code == 204:
+            return True
+        return False
+
+    def inbox(self, date: str) -> dict:
+        url = f'{self.server_url}/etapi/inbox/{date}'
+        res = requests.get(url, headers=self.get_header())
+        return res.json()
+
+    def get_calendar_days(self, date: str) -> dict:
+        url = f'{self.server_url}/etapi/calendar/days/{date}'
+        res = requests.get(url, headers=self.get_header())
+        return res.json()
+
+    def get_calendar_weeks(self, date: str):
+        url = f'{self.server_url}/etapi/calendar/weeks/{date}'
+        res = requests.get(url, headers=self.get_header())
+        return res.json()
+
+    def get_calendar_months(self, month: str) -> dict:
+        url = f'{self.server_url}/etapi/calendar/months/{month}'
+        res = requests.get(url, headers=self.get_header())
+        return res.json()
+
+    def get_calendar_years(self, year: str) -> dict:
+        url = f'{self.server_url}/etapi/calendar/years/{year}'
+        res = requests.get(url, headers=self.get_header())
+        return res.json()
