0c47a3f4660d00f83564cd312293429d1aa50606
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:26:35 +0000

    Add package qplay-cli/0.4/qplay-cli-0.4.tar.gz

---
+++ packages/qplay-cli/qplay-cli-0.4.tar.gz/qplay-cli-0.4/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="qplay-cli",
+    version='0.4',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'Click',
+        'requests',
+        'retrying',
+    ],
+    entry_points='''
+        [console_scripts]
+        quantplay=qplay_cli.main:quantplay
+    ''',
+)
