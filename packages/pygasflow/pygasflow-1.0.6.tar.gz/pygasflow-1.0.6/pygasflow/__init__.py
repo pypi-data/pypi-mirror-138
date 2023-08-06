f310fb04cbf4a5841668168abf1b3a086cc70217
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:47 +0000

    Add package pygasflow/1.0.6/pygasflow-1.0.6.tar.gz

---
+++ packages/pygasflow/pygasflow-1.0.6.tar.gz/pygasflow-1.0.6/pygasflow/__init__.py
@@ -0,0 +1,16 @@
+from pygasflow._version import __version__
+from pygasflow.solvers import (
+    isentropic_solver,
+    fanno_solver,
+    rayleigh_solver,
+    shockwave_solver,
+    conical_shockwave_solver,
+    De_Laval_Solver
+)
+
+from pygasflow.nozzles import (
+    CD_Conical_Nozzle,
+    CD_Min_Length_Nozzle,
+    CD_TOP_Nozzle,
+    Rao_Parabola_Angles
+)
