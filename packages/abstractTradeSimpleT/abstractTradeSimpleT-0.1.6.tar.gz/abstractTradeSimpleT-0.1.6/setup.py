21419bf07655cd1d5e9c6d0f2e6ae7e33b128255
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:27 +0000

    Add package abstractTradeSimpleT/0.1.6/abstractTradeSimpleT-0.1.6.tar.gz

---
+++ packages/abstractTradeSimpleT/abstractTradeSimpleT-0.1.6.tar.gz/abstractTradeSimpleT-0.1.6/setup.py
@@ -0,0 +1,14 @@
+import pathlib
+import setuptools
+
+long_description = (pathlib.Path(__file__).parent / "README.md").read_text()
+
+setuptools.setup(
+    name='abstractTradeSimpleT',
+    version='0.1.6',
+    license='MIT',
+    author="Joao Paulo Euko",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=['lib'],
+)
