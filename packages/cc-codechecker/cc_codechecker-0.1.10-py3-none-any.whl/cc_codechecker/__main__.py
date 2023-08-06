40342977fa8b017f8079963cc9cce6bfb8fa31ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:40 +0000

    Add package cc-codechecker/0.1.10/cc_codechecker-0.1.10-py3-none-any.whl

---
+++ packages/cc-codechecker/cc_codechecker-0.1.10-py3-none-any.whl/cc_codechecker/__main__.py
@@ -0,0 +1,15 @@
+"""Coding Challenge Code Checker
+
+Use this tool to collect coding challenge projects, run and evaluate them.
+Configure using YAML notation and produce output in many formats.
+
+Rode map before release 0.1.0:
+1. Read YAML file codechecker.yml in the root of a repository
+2. Execute code inside a project
+3. Produce output in txt format
+"""
+
+from .codechecker import main
+
+if __name__ == "__main__":
+  main()
