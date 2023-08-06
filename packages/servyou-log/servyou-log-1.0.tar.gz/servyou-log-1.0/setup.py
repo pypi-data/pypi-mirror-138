fc8277e85a2c31f41be1cfc261cf9f12658ea113
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:19:42 +0000

    Add package servyou-log/1.0/servyou-log-1.0.tar.gz

---
+++ packages/servyou-log/servyou-log-1.0.tar.gz/servyou-log-1.0/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+requirements = open('requirements.txt').readlines()
+
+setuptools.setup(
+    name="servyou-log",
+    version="1.0",
+    author="PubDesktopLsw",
+    description='日志工具类',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    platforms=["all"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>2',
+    install_requires=requirements
+)
