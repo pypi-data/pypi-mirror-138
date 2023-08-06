a5eb3dd0f954faa9830c2fda74d8ec8c72a3cfa4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:10:55 +0000

    Add package ConSav/0.11/ConSav-0.11.tar.gz

---
+++ packages/ConSav/ConSav-0.11.tar.gz/ConSav-0.11/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name='ConSav',
+    url='https://github.com/NumEconCopenhagen/ConsumptionSaving',
+    download_url='https://github.com/NumEconCopenhagen/ConsumptionSaving/archive/refs/tags/v0.11.tar.gz',
+    version='0.11',
+    author='Jeppe Druedahl',
+    packages=setuptools.find_packages(),
+)
