3f9cc11daa1742526058b276600bd70fdb1adcda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:54 +0000

    Add package pyjjy/0.2/pyjjy-0.2-py3-none-any.whl

---
+++ packages/pyjjy/pyjjy-0.2-py3-none-any.whl/pyjjy/__init__.py
@@ -0,0 +1,10 @@
+from .pyjjy import JJYsignal, main
+
+__copyright__ = 'Copyright (C) 2022 Haruki EJIRI'
+__version__ = '0.2'
+__license__ = 'MIT'
+__author__ = 'Haruki EJIRI'
+__author_email__ = '0y35.ejiri.vmqewyhw@gmail.com'
+__url__ = 'https://github.com/ehki/pyjjy'
+
+__all__ = ['JJYsignal', 'main']
---
+++ packages/pyjjy/pyjjy-0.2-py3-none-any.whl/pyjjy/__main__.py
@@ -0,0 +1,7 @@
+import sys
+
+import pyjjy
+
+if __name__ == '__main__':
+    # execute with python -m pyjjy
+    sys.exit(pyjjy.main())
