0703808023587a6d49f39c8b18c61499c8980d6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:50 +0000

    Add package volkswagencarnet/4.4.47/volkswagencarnet-4.4.47.tar.gz

---
+++ packages/volkswagencarnet/volkswagencarnet-4.4.47.tar.gz/volkswagencarnet-4.4.47/tests/integration_test.py
@@ -0,0 +1,23 @@
+import pytest
+from aiohttp import ClientSession
+
+from volkswagencarnet import vw_connection
+
+try:
+    from credentials import username, password, spin, vin
+except ImportError:
+    username = password = spin = vin = None
+    pass
+
+
+@pytest.mark.skipif(
+    username is None or password is None, reason="Username or password is not set. Check credentials.py.sample"
+)
+@pytest.mark.asyncio
+async def test_successful_login():
+    async with ClientSession() as session:
+        connection = vw_connection.Connection(session, username, password)
+        await connection.doLogin()
+        if connection.logged_in:
+            return True
+    pytest.fail("Login failed")
