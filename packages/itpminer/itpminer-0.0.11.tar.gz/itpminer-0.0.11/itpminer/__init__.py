e3c4073ea6f6b9bf25caa9c44851a2701be985d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:19:00 +0000

    Add package itpminer/0.0.11/itpminer-0.0.11.tar.gz

---
+++ packages/itpminer/itpminer-0.0.11.tar.gz/itpminer-0.0.11/itpminer/__init__.py
@@ -0,0 +1,6 @@
+__version__ = '__version__ = '0.0.11''
+from itpminer.itp_miner import itp_miner
+# import sys
+# import os
+# root = os.path.dirname(os.path.realpath(__file__))
+# sys.path.append(root)
