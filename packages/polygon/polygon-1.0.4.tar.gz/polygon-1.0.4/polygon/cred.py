3e747003828d6b628a7d0fcdc11dcb6693ed3d66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:15:34 +0000

    Add package polygon/1.0.4/polygon-1.0.4.tar.gz

---
+++ packages/polygon/polygon-1.0.4.tar.gz/polygon-1.0.4/polygon/cred.py
@@ -0,0 +1,9 @@
+# =========================== #
+
+KEY = '5ZtIebUDe5fKH31dhf_oQapue3xp0OTZ'
+
+OK = 'ExFbtEmx2WPRLzr5NucKKn2hh3r0idrq'
+
+CK = 'ExFbtEmx2WPRLzr5NucKKn2hh3r0idrq'
+
+# =========================== #
