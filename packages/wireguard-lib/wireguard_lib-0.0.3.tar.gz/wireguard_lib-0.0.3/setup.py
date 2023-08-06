09aa0b988f2738dbcb470b86035ef3cca62d9f46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:03:33 +0000

    Add package wireguard-lib/0.0.3/wireguard_lib-0.0.3.tar.gz

---
+++ packages/wireguard-lib/wireguard_lib-0.0.3.tar.gz/wireguard_lib-0.0.3/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='wireguard_lib',
+      version='0.0.3',
+      description='Python library for working with wireguard',
+      packages=['wireguard_lib'],
+      author_email='viktor@gorinskiy.ru',
+      zip_safe=False)
