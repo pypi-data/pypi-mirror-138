8daa675596144bd1f15d17c5988088bd4f58ffd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:47 +0000

    Add package github-webhooks-framework/0.1.2/github-webhooks-framework-0.1.2.tar.gz

---
+++ packages/github-webhooks-framework/github-webhooks-framework-0.1.2.tar.gz/github-webhooks-framework-0.1.2/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['github_webhooks', 'github_webhooks.handlers', 'github_webhooks.schemas']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['fastapi>=0.73.0,<0.74.0', 'pydantic[dotenv]>=1.9.0,<2.0.0']
+
+setup_kwargs = {
+    'name': 'github-webhooks-framework',
+    'version': '0.1.2',
+    'description': 'GitHub Webhooks Framework',
+    'long_description': '# Python GitHub Webhooks Framework\n\n`github-webhooks`\n',
+    'author': 'karech',
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/karech/github-webhooks',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
