a5ed63871d592884c9c2395f72e098dac0dd4a72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:19 +0000

    Add package sha256/0.3/sha256-0.3.tar.gz

---
+++ packages/sha256/sha256-0.3.tar.gz/sha256-0.3/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+from setuptools.extension import Extension
+from setuptools.command.sdist import sdist as _sdist
+
+cmdclass = {}
+
+
+class sdist(_sdist):
+    def run(self):
+        from Cython.Build import cythonize
+        cythonize(['sha256.pyx'])
+        _sdist.run(self)
+cmdclass['sdist'] = sdist
+
+
+setup(
+    name='sha256',
+    version='0.3',
+    description='sha256 library with midstate',
+    author='Mark Peek',
+    author_email='mark@peek.org',
+    license="MIT",
+    cmdclass=cmdclass,
+    ext_modules=[Extension("sha256", ["sha256.c"])],
+    test_suite="tests",
+)
