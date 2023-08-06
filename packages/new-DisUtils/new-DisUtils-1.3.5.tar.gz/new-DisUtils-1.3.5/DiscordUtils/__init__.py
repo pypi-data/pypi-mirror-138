ed491234ae500c07aa223f1cca7a315cf5eb9154
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:38 +0000

    Add package new-DisUtils/1.3.5/new-DisUtils-1.3.5.tar.gz

---
+++ packages/new-DisUtils/new-DisUtils-1.3.5.tar.gz/new-DisUtils-1.3.5/DiscordUtils/__init__.py
@@ -0,0 +1,8 @@
+from DiscordUtils import Pagination
+from DiscordUtils.InviteTracker import InviteTracker
+from DiscordUtils.Music import Music
+
+__title__ = "DisUtils"
+__version__ = "1.3.5"
+__author__ = "DTS"
+__license__ = "MIT"
