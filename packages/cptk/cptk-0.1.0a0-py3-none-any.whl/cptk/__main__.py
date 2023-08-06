d0fffd3fbece38c907e28ea38cf7e992dd8c0684
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:43:05 +0000

    Add package cptk/0.1.0a0/cptk-0.1.0a0-py3-none-any.whl

---
+++ packages/cptk/cptk-0.1.0a0-py3-none-any.whl/cptk/__main__.py
@@ -0,0 +1,2 @@
+from cptk.main import main
+raise SystemExit(main())
---
+++ packages/cptk/cptk-0.1.0a0-py3-none-any.whl/cptk/constants.py
@@ -0,0 +1,9 @@
+RECIPE_FILE = 'recipes.cptk.yaml'
+PROJECT_FILE = '.cptk/project.cptk.yaml'
+LAST_FILE = '.cptk/stayaway/last.cptk.txt'
+
+MOVE_FILE = '.cptk/moves.cptk.txt'
+MOVE_FILE_SEPERATOR = '::'
+MOVE_SAFES = ['./', '**/.cptk/**']
+
+LAST_FILE_SEPERATOR = '::'
