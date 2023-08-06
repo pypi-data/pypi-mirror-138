256a35abc4a63a7a2b078000aadf33219935effc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:28 +0000

    Add package tarikED/1.0.7b0/tarikED-1.0.7b0.tar.gz

---
+++ packages/tarikED/tarikED-1.0.7b0.tar.gz/tarikED-1.0.7b0/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+#with open("latex/README.md", "r") as fh:
+#    long_description = fh.read()
+
+setuptools.setup(
+    name="tarikED",
+    version="1.0.7b",
+    author="Tarik Ronan Drevon",
+    author_email="ronandrevon@gmail.com",
+    description="Electron diffraction utilities",
+    long_description='', #long_description,
+    long_description_content_type="",
+    url="",
+    # packages=['multislice','scattering','wallpp'],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License ",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=['numpy','scipy','matplotlib','colorama','pandas',
+    'cbf','crystals','TarikDrevonUtils','easygui','tifffile','pickle5','bs4'],
+)
