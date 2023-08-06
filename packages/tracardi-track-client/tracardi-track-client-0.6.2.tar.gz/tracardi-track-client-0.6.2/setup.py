3e0a36e185cfdd4d959d4a719e532c6c9464fa91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:44:58 +0000

    Add package tracardi-track-client/0.6.2/tracardi-track-client-0.6.2.tar.gz

---
+++ packages/tracardi-track-client/tracardi-track-client-0.6.2.tar.gz/tracardi-track-client-0.6.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='tracardi-track-client',
+    version='0.6.2',
+    description='Tracardi Customer Data Platform',
+    author='Risto Kowaczewski',
+    author_email='risto.kowaczewski@gmail.com',
+    packages=['tracardi_track_client'],
+    install_requires=[
+        'requests'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    keywords=['tracardi'],
+    include_package_data=True,
+    python_requires=">=3.8",
+)
