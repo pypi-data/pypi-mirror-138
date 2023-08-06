d1425f579bf1b3caae1ac1b31938f1f5c35b834c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:08 +0000

    Add package qrunner/0.6.13/qrunner-0.6.13.tar.gz

---
+++ packages/qrunner/qrunner-0.6.13.tar.gz/qrunner-0.6.13/qrunner/__init__.py
@@ -0,0 +1,8 @@
+from qrunner.running.runner import main
+from qrunner.case import TestCase, Page, H5Page
+from qrunner.core.h5.driver import H5Driver
+
+
+__version__ = "0.6.13"
+__description__ = "跨平台UI自动化测试框架"
+
