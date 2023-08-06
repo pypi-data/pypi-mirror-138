c9f23920f13a1f0c162d838006f305e43bd2e7f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:45:11 +0000

    Add package pySQLBuilder/0.0.6/pySQLBuilder-0.0.6.tar.gz

---
+++ packages/pySQLBuilder/pySQLBuilder-0.0.6.tar.gz/pySQLBuilder-0.0.6/pySQLBuilder/Structure/Table.py
@@ -0,0 +1,38 @@
+from typing import Mapping
+
+class Table :
+    """Object for storing a table definition.
+    Object properties:
+    - Table name
+    - Table name alias
+    """
+
+    table = ''
+
+    def __init__(self, name: str, alias: str = '') :
+        self.__name = name
+        self.__alias = alias
+
+    def name(self) -> str :
+        """Get table name."""
+        return self.__name
+
+    def alias(self) -> str :
+        """Get table alias."""
+        return self.__alias
+
+    @classmethod
+    def create(cls, table) :
+        """Create table object from string input or ascossiative array with key as alias."""
+        name = ''
+        alias = ''
+        if isinstance(table, str) :
+            cls.table = table
+            name = table
+            return Table(table)
+        elif isinstance(table, Mapping) and len(table) == 1 :
+            key = next(iter(table.keys()))
+            name = str(table[key])
+            alias = str(key)
+            cls.table = alias
+        return Table(name, alias)
