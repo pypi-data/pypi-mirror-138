b6ca256b1ba130c2c828c67bd991beab990616b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:01 +0000

    Add package PIT-analysis1234/0.0.1/PIT_analysis1234-0.0.1.tar.gz

---
+++ packages/PIT-analysis1234/PIT_analysis1234-0.0.1.tar.gz/PIT_analysis1234-0.0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+	name = 'PIT_analysis1234',
+	version = '0.0.1',
+	description = 'Tools to data analysis.',
+	long_description = 'Package allows to analyze data about PIT taxes.',
+	author = 'Jurand Pradzynski',
+	author_email = 'j.pradzynski@student.uw.edu.pl',
+	License = '',
+	keywords = '',
+	packages = find_packages(),
+	install_requires = [''],
+	classifiers = [
+	'Development Status :: 5 - Production/Stable',
+	'Intended Audience :: Education',
+	'Operating System :: Unix',
+	'Programming Language :: Python :: 3'
+
+]
+)
