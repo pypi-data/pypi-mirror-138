89ae40242c550ca012d2554de64c8021a690a13e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:21 +0000

    Add package acconeer-exptool/4.0.0a5/acconeer-exptool-4.0.0a5.tar.gz

---
+++ packages/acconeer-exptool/acconeer-exptool-4.0.0a5.tar.gz/acconeer-exptool-4.0.0a5/portable/package/run_gui.bat
@@ -0,0 +1,11 @@
+@echo off
+
+if exist "acconeer-python-exploration" (
+    tools\python-3.7.4-embed-amd64\python.exe -m acconeer.exptool.app
+    pause
+) else (
+    echo Could not find acconeer-python-exploration
+    echo Did you forget to run update.bat?
+    echo.
+    pause
+)
---
+++ packages/acconeer-exptool/acconeer-exptool-4.0.0a5.tar.gz/acconeer-exptool-4.0.0a5/portable/package/tools/update.py
@@ -0,0 +1,40 @@
+import importlib.util
+import os
+from glob import glob
+from subprocess import check_call
+
+
+def main():
+    root_dir = os.getcwd()
+    repo_name = "acconeer-python-exploration"
+    repo_dir = os.path.join(root_dir, repo_name)
+    repo_addr = "https://github.com/acconeer/acconeer-python-exploration.git"
+    python_dir = os.path.abspath(glob("tools\\python-3.7.*")[0])
+    python = os.path.abspath(os.path.join(python_dir, "python.exe"))
+
+    # Check that pip is installed
+    check_call([python, "-m", "pip", "--version"])
+    pip_install = [python, "-m", "pip", "install", "--no-warn-script-location"]
+
+    # Install and import dulwich
+    if importlib.util.find_spec("dulwich") is None:
+        check_call(pip_install + ["urllib3", "certifi"])
+        check_call(pip_install + ["dulwich==0.20.28", "--global-option=--pure"])
+
+    from dulwich import porcelain
+
+    # Clone/clean/update repo
+    if os.path.isdir(repo_dir):
+        os.chdir(repo_dir)
+        porcelain.pull(".", repo_addr)
+    else:
+        os.chdir(root_dir)
+        porcelain.clone(repo_addr, target=repo_dir)
+
+    # Install package
+    os.chdir(repo_dir)
+    check_call(pip_install + [".[app]"])
+
+
+if __name__ == "__main__":
+    main()
