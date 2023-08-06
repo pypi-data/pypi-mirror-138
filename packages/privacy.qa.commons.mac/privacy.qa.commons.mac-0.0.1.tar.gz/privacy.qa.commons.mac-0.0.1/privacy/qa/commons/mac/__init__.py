55f6670920d1dc77ee19c721f40ba09bfb7a8da0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:28:11 +0000

    Add package privacy.qa.commons.mac/0.0.1/privacy.qa.commons.mac-0.0.1.tar.gz

---
+++ packages/privacy.qa.commons.mac/privacy.qa.commons.mac-0.0.1.tar.gz/privacy.qa.commons.mac-0.0.1/privacy/qa/commons/mac/__init__.py
@@ -0,0 +1,9 @@
+"""
+Test automation library
+
+Needs to be explicitly imported as:
+
+    import privacy.qa.commons.mac
+"""
+
+print("Placeholder for privacy.qa.commons.mac project to be opensourced soon")
---
+++ packages/privacy.qa.commons.mac/privacy.qa.commons.mac-0.0.1.tar.gz/privacy.qa.commons.mac-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='privacy.qa.commons.mac',
+    description='Testing library',
+    author='Teal team',
+    version='0.0.1',
+    packages=['privacy.qa.commons.mac'],
+    classifiers=['Programming Language :: Python :: 3'],
+)
