d2879d311f2e1c44d96ed5d9a8080553983159a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:50 +0000

    Add package zyz/0.1.6/zyz-0.1.6-py3-none-any.whl

---
+++ packages/zyz/zyz-0.1.6-py3-none-any.whl/zyz/__init__.py
@@ -0,0 +1,12 @@
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+
+def help():
+
+    print("****************************************\n")
+    print("Help:\n")
+    print("\n")
+    print("****************************************\n")
---
+++ packages/zyz/zyz-0.1.6-py3-none-any.whl/zyz/zlog/__init__.py
@@ -0,0 +1,5 @@
+import datetime
+
+def logdemo():
+
+    print("Demo: This is a demo log line ...")
