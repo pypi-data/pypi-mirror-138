a1145659627cd9f20170766f338065fb9224c695
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:39:15 +0000

    Add package procuret/0.0.25/procuret-0.0.25-py3-none-any.whl

---
+++ packages/procuret/procuret-0.0.25-py3-none-any.whl/procuret/ancillary/sale_nomenclature.py
@@ -0,0 +1,13 @@
+"""
+Procuret API Python
+Sale Nomenclature Module
+author: hugh@blinkybeach.com
+"""
+from enum import IntEnum
+
+
+class SaleNomenclature(IntEnum):
+
+    INVOICE = 1
+    ORDER = 2
+    SHOPIFY_ORDER = 3
