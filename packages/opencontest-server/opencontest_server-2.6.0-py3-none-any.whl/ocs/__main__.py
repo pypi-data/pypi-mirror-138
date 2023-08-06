f41029cc936c14f1255b9c018fc4b1b4677231e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:38 +0000

    Add package opencontest-server/2.6.0/opencontest_server-2.6.0-py3-none-any.whl

---
+++ packages/opencontest-server/opencontest_server-2.6.0-py3-none-any.whl/ocs/__main__.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+import sys
+import os
+import logging
+from http.server import ThreadingHTTPServer
+
+from ocs.args import args
+from ocs.server import Server
+
+
+def main():
+    """Run the server"""
+    
+    httpd = ThreadingHTTPServer(('', args.port), Server)
+    logging.info('Starting server')
+    httpd.serve_forever()
+
+
+if __name__ == '__main__':
+    sys.exit(main())
