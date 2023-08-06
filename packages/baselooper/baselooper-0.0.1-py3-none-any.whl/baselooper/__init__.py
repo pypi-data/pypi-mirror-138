22f623103676262e7be5e7d8ac98610a1d118d6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:16:27 +0000

    Add package baselooper/0.0.1/baselooper-0.0.1-py3-none-any.whl

---
+++ packages/baselooper/baselooper-0.0.1-py3-none-any.whl/baselooper/__init__.py
@@ -0,0 +1,6 @@
+VERSION = "0.0.1"
+
+from baselooper.state import State
+from baselooper.module import Module, ModuleConfig, SeededModule, SeededModuleConfig, NOP, NOPConfig, ModuleList, ModuleListConfig
+from baselooper.looper import Looper, LooperConfig, LooperState, LooperIterationStop, LooperIterationStopConfig
+from baselooper import logging
