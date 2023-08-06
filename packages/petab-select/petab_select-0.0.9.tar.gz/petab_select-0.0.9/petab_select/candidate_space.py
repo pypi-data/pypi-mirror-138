84c49b3355aa610f7e41c7787e917a1363b328d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:30 +0000

    Add package petab-select/0.0.9/petab_select-0.0.9.tar.gz

---
+++ packages/petab-select/petab_select-0.0.9.tar.gz/petab_select-0.0.9/petab_select/candidate_space.py
@@ -0,0 +1,540 @@
+"""Classes and methods related to candidate spaces."""
+import abc
+import warnings
+from typing import Any, Dict, List, Optional, Union
+
+import numpy as np
+from more_itertools import one
+
+from .constants import (ESTIMATE, VIRTUAL_INITIAL_MODEL,
+                        VIRTUAL_INITIAL_MODEL_METHODS, Method)
+from .handlers import TYPE_LIMIT, LimitHandler
+from .misc import snake_case_to_camel_case
+from .model import Model
+
+
+class CandidateSpace(abc.ABC):
+    """A base class for collecting candidate models.
+
+    The intended use of subclasses is to identify suitable models in a model
+    space, that will be provided to a model selection method for selection.
+
+    Attributes:
+        distances:
+            The distances of all candidate models from the initial model.
+            FIXME change list to int? Is storage of more than one value
+            useful?
+        predecessor_model:
+            The model used for comparison, e.g. for stepwise methods.
+        models:
+            The current set of candidate models.
+        exclusions:
+            A list of model hashes. Models that match a hash in `exclusions` will not
+            be accepted into the candidate space. The hashes of models that are accepted
+            are added to `exclusions`.
+        limit:
+            A handler to limit the number of accepted models.
+        #limited:
+        #    A descriptor that handles the limit on the number of accepted models.
+        #limit:
+        #    Models will fail `self.consider` if `len(self.models) >= limit`.
+    """
+
+    def __init__(
+        self,
+        # TODO add MODEL_TYPE = Union[str, Model], str for VIRTUAL_INITIAL_MODEL
+        predecessor_model: Optional[Model] = None,
+        exclusions: Optional[List[Any]] = None,
+        limit: TYPE_LIMIT = np.inf,
+    ):
+        self.limit = LimitHandler(
+            current=self.n_accepted,
+            limit=limit,
+        )
+        self.reset(predecessor_model=predecessor_model, exclusions=exclusions)
+
+    def is_plausible(self, model: Model) -> bool:
+        """Determine whether a candidate model is plausible.
+
+        A plausible model is one that could possibly be chosen by
+        model selection method, in the absence of information about other
+        models in the model space.
+
+        For example, given a forward selection method that starts with an
+        initial model `self.predecessor_model` that has no estimated
+        parameters, then only models with one or more estimated parameters are
+        plausible.
+
+        Args:
+            model:
+                The candidate model.
+
+        Returns:
+            `True` if `model` is plausible, else `False`.
+        """
+        return True
+
+    def distance(self, model: Model) -> Union[None, float, int]:
+        """Compute the distance between two models that are neighbors.
+
+        Args:
+            model:
+                The candidate model.
+            predecessor_model:
+                The initial model.
+
+        Returns:
+            The distance from `predecessor_model` to `model`, or `None` if the
+            distance should not be computed.
+        """
+        return None
+
+    def accept(
+        self,
+        model: Model,
+        distance: Union[None, float, int],
+        #keep_others: bool = True,
+    ) -> None:
+        """Add a candidate model to the candidate space.
+
+        Args:
+            model:
+                The model that will be added.
+            distance:
+                The distance of the model from the predecessor model.
+            #keep_others:
+            #    Whether to keep other models that were previously added to the
+            #    candidate space.
+        """
+        model.predecessor_model_hash = (
+            self.predecessor_model.get_hash()
+            if isinstance(self.predecessor_model, Model)
+            else self.predecessor_model
+        )
+        self.models.append(model)
+        self.distances.append(distance)
+        self.exclude(model)
+
+    def n_accepted(self) -> TYPE_LIMIT:
+        """Get the current number of accepted models."""
+        return len(self.models)
+
+    def exclude(
+        self,
+        model: Union[Model, List[Model]],
+    ) -> None:
+        """Exclude model(s) from future consideration.
+
+        Args:
+            model:
+                The model(s) that will be excluded.
+        """
+        if isinstance(model, list):
+            for _model in model:
+                self.exclusions.append(_model.get_hash())
+        else:
+            self.exclusions.append(model.get_hash())
+
+    def excluded(
+        self,
+        model: Model,
+    ) -> bool:
+        """Whether a model is excluded."""
+        return model.get_hash() in self.exclusions
+
+    @abc.abstractmethod
+    def _consider_method(self, model) -> bool:
+        """Consider whether a model should be accepted, according to a method.
+
+        Args:
+            model:
+                The candidate model.
+
+        Returns:
+            Whether a model should be accepted.
+        """
+        return True
+
+    def consider(self, model: Union[Model, None]) -> bool:
+        """Add a candidate model, if it should be added.
+
+        Args:
+            model:
+                The candidate model. This value may be `None` if the `ModelSubspace`
+                decided to exclude the model that would have been sent.
+
+        Returns:
+            Whether it is OK to send additional models to the candidate space. For
+            example, if the limit of the number of accepted models has been reached,
+            then no further models should be sent.
+            TODO change to return whether the model was accepted, and instead add
+                 `self.continue` to determine whether additional models should be sent.
+        """
+        # Model was excluded by the `ModelSubspace` that called this method, so can be
+        # skipped.
+        if model is None:
+            # TODO use a different code than `True`?
+            return True
+        if self.limit.reached():
+            return False
+        if self.excluded(model):
+            warnings.warn(f'Model has been previously excluded from the candidate space so is skipped here. Model subspace ID: {model.model_subspace_id}. Parameterization: {model.parameters}',  RuntimeWarning)
+            return True
+        if not self.is_plausible(model):
+            return True
+        if not self._consider_method(model):
+            return True
+
+        self.accept(model, distance=self.distance(model))
+        return not self.limit.reached()
+
+    def reset_accepted(self) -> None:
+        """Reset the accepted models."""
+        self.models = []
+        self.distances = []
+
+    def set_predecessor_model(self, predecessor_model: Union[Model, str, None]):
+        self.predecessor_model = predecessor_model
+        if (
+            self.predecessor_model == VIRTUAL_INITIAL_MODEL
+            and self.method not in VIRTUAL_INITIAL_MODEL_METHODS
+        ):
+            raise ValueError(f'A virtual initial model was requested for a method ({self.method}) that does not support them.')  # noqa: E501
+
+    def get_predecessor_model(self):
+        return self.predecessor_model
+
+    def set_exclusions(self, exclusions: Union[List[Any], None]):
+        # TODO change to List[str] for hashes?
+        self.exclusions = exclusions
+        if self.exclusions is None:
+            self.exclusions = []
+
+    def get_exclusions(self):
+        return self.exclusions
+
+    def set_limit(self, limit: TYPE_LIMIT = None):
+        if limit is not None:
+            self.limit.set_limit(limit)
+
+    def get_limit(self):
+        return self.limit.get_limit()
+
+    def reset(
+        self,
+        predecessor_model: Optional[Union[Model, str, None]] = None,
+        # FIXME change `Any` to some `TYPE_MODEL_HASH` (e.g. union of str/int/float)
+        exclusions: Optional[Union[List[Any], None]] = None,
+        limit: TYPE_LIMIT = None,
+    ) -> None:
+        """Reset the candidate models, optionally reinitialize with a model.
+
+        Args:
+            predecessor_model:
+                The initial model.
+            exclusions:
+                Whether to reset model exclusions.
+            limit:
+                The new upper limit of the number of models in this candidate space.
+        """
+        self.set_predecessor_model(predecessor_model)
+        self.reset_accepted()
+        self.set_exclusions(exclusions)
+        self.set_limit(limit)
+
+    def distances_in_estimated_parameters(
+        self,
+        model: Model,
+        predecessor_model: Optional[Model] = None,
+    ) -> Dict[str, Union[float, int]]:
+        """Distance between two models in model space, using different metrics.
+
+        All metrics are in terms of estimated parameters.
+
+        Metrics:
+            l1:
+                The L_1 distance between two models.
+            size:
+                The difference in the number of estimated parameters between two
+                models.
+
+        Args:
+            model:
+                The candidate model.
+
+        Returns:
+            The distances between the models, as a dictionary, where a key is the
+            name of the metric, and the value is the corresponding distance.
+        """
+        model0 = predecessor_model
+        if model0 is None:
+            model0 = self.predecessor_model
+        model1 = model
+
+        if (
+            model0 != VIRTUAL_INITIAL_MODEL
+            and not model1.petab_yaml.samefile(model0.petab_yaml)
+        ):
+            raise NotImplementedError(
+                'Computation of distances between different PEtab problems is '
+                'currently not supported. This error is also raised if the same '
+                'PEtab problem is read from YAML files in different locations.'
+            )
+
+        # All parameters from the PEtab problem are used in the computation.
+        if model0 == VIRTUAL_INITIAL_MODEL:
+            parameter_ids = list(model1.petab_parameters)
+            # FIXME need to take superset of all parameters amongst all PEtab problems
+            # in all model subspaces to get an accurate comparable distance. Currently
+            # only reasonable when working with a single PEtab problem for all models
+            # in all subspaces.
+            if self.method == Method.FORWARD:
+                parameters0 = np.array([0 for _ in parameter_ids])
+            elif self.method == Method.BACKWARD:
+                parameters0 = np.array([ESTIMATE for _ in parameter_ids])
+            else:
+                raise NotImplementedError(
+                    'Distances for the virtual initial model have not yet been '
+                    f'implemented for the method "{self.method}". Please notify the'
+                    'developers.'
+                )
+        else:
+            parameter_ids = list(model0.petab_parameters)
+            parameters0 = np.array(model0.get_parameter_values(parameter_ids=parameter_ids))
+
+        parameters1 = np.array(model1.get_parameter_values(parameter_ids=parameter_ids))
+
+        # TODO change to some numpy elementwise operation
+        estimated0 = np.array([p == ESTIMATE for p in parameters0]).astype(int)
+        estimated1 = np.array([p == ESTIMATE for p in parameters1]).astype(int)
+
+        difference = estimated1 - estimated0
+
+        # Changes to(/from) estimated from(/to) not estimated
+        l1 = np.abs(difference).sum()
+
+        # Change in the number of estimated parameters.
+        size = np.sum(difference)
+
+        # TODO constants?
+        distances = {
+            'l1': l1,
+            'size': size,
+           }
+
+        return distances
+
+
+
+class ForwardCandidateSpace(CandidateSpace):
+    """The forward method class.
+
+    Attributes:
+        direction:
+            `1` for the forward method, `-1` for the backward method.
+    """
+    method = Method.FORWARD
+    direction = 1
+
+    def __init__(
+        self,
+        *args,
+        predecessor_model: Optional[Union[Model, str]] = None,
+        **kwargs,
+    ):
+        # Although `VIRTUAL_INITIAL_MODEL` is `str` and can be used as a default
+        # argument, `None` may be passed by other packages, so the default value
+        # is handled here instead.
+        if predecessor_model is None:
+            predecessor_model = VIRTUAL_INITIAL_MODEL
+        super().__init__(*args, predecessor_model=predecessor_model, **kwargs)
+
+    def is_plausible(self, model: Model) -> bool:
+        distances = \
+            self.distances_in_estimated_parameters(model)
+        unsigned_size = self.direction * distances['size']
+        # A model is plausible if the number of estimated parameters strictly
+        # increases (or decreases, if `self.direction == -1`), and no
+        # previously estimated parameters become fixed.
+        if (
+            self.predecessor_model == VIRTUAL_INITIAL_MODEL
+            or (
+                unsigned_size > 0
+                and distances['l1'] == unsigned_size
+            )
+        ):
+            return True
+        return False
+
+    def distance(self, model: Model) -> int:
+        # TODO calculated here and `is_plausible`. Rewrite to only calculate
+        #      once?
+        distances = \
+            self.distances_in_estimated_parameters(model)
+        return distances['l1']
+
+    def _consider_method(self, model) -> bool:
+        """See `CandidateSpace._consider_method`."""
+        distance = self.distance(model)
+
+        # Get the distance of the current "best" plausible model(s)
+        distance0 = np.inf
+        if self.distances:
+            distance0 = one(set(self.distances))
+            # TODO store each or just one?
+            #distance0 = one(self.distances)
+        #breakpoint()
+
+        # Only keep the best model(s).
+        if distance > distance0:
+            return False
+        if distance < distance0:
+            self.reset_accepted()
+        return True
+
+
+class BackwardCandidateSpace(ForwardCandidateSpace):
+    """The backward method class."""
+    method = Method.BACKWARD
+    direction = -1
+
+
+class LateralCandidateSpace(ForwardCandidateSpace):
+    """Find models with the same number of estimated parameters."""
+    method = Method.LATERAL
+
+    def __init__(
+        self,
+        *args,
+        predecessor_model: Union[Model, str],
+        **kwargs,
+    ):
+        super().__init__(*args, predecessor_model=predecessor_model, **kwargs)
+
+    def is_plausible(self, model: Model) -> bool:
+        distances = self.distances_in_estimated_parameters(model)
+        # A model is plausible if the number of estimated parameters remains
+        # the same, but some estimated parameters have become fixed and vice
+        # versa.
+        if (
+            distances['size'] == 0 and
+            # distances['size'] == 0 implies L1 % 2 == 0.
+            # FIXME here and elsewhere, deal with models that are equal
+            #       except for the values of their fixed parameters.
+            distances['l1'] > 0
+        ):
+            return True
+        return False
+
+    def _consider_method(self, model):
+        return True
+
+
+class BruteForceCandidateSpace(CandidateSpace):
+    """The brute-force method class."""
+    method = Method.BRUTE_FORCE
+
+    def __init__(self, *args, **kwargs):
+        #if args or kwargs:
+        #    # FIXME remove?
+        #    # FIXME at least support limit
+        #    warnings.warn(
+        #        'Arguments were provided but will be ignored, because of the '
+        #        'brute force candidate space.'
+        #    )
+        super().__init__(*args, **kwargs)
+
+    def _consider_method(self, model):
+        return True
+
+
+candidate_space_classes = [
+    ForwardCandidateSpace,
+    BackwardCandidateSpace,
+    LateralCandidateSpace,
+    BruteForceCandidateSpace,
+]
+
+
+def method_to_candidate_space_class(method: Method) -> str:
+    """Instantiate a candidate space given its method name.
+
+    Args:
+        method:
+            The name of the method corresponding to one of the implemented candidate
+            spaces.
+
+    Returns:
+        The candidate space.
+    """
+    for candidate_space_class in candidate_space_classes:
+        if candidate_space_class.method == method:
+            return candidate_space_class
+    raise NotImplementedError(f'The provided method name {method} does not correspond to an implemented candidate space.')  # noqa: E501
+
+
+'''
+def distances_in_estimated_parameters(
+    model: Model,
+    predecessor_model: Model,
+    #method: str = None,
+) -> Dict[str, Union[float, int]]:
+    """Distance between two models in model space, using different metrics.
+
+    All metrics are in terms of estimated parameters.
+
+    Metrics:
+        l1:
+            The L_1 distance between two models.
+        size:
+            The difference in the number of estimated parameters between two
+            models.
+
+    Args:
+        model:
+            The candidate model.
+        predecessor_model:
+            The initial model.
+        method:
+            The search method in use. Necessary if the predecessor model is the
+            virtual initial model.
+
+    Returns:
+        The distances between the models, as a dictionary, where a key is the
+        name of the metric, and the value is the corresponding distance.
+    """
+    model0 = predecessor_model
+    model1 = model
+
+    if not model1.petab_yaml.samefile(model0.petab_yaml):
+        raise NotImplementedError(
+            'Computation of distances between different PEtab problems is '
+            'currently not supported. This error is also raised if the same '
+            'PEtab problem is read from YAML files in different locations.'
+        )
+
+    # All parameters from the PEtab problem are used in the computation.
+    parameter_ids = list(model0.petab_parameters)
+
+    parameters0 = np.array(model0.get_parameter_values(parameter_ids=parameter_ids))
+    parameters1 = np.array(model1.get_parameter_values(parameter_ids=parameter_ids))
+
+    estimated0 = (parameters0 == ESTIMATE).astype(int)
+    estimated1 = (parameters1 == ESTIMATE).astype(int)
+
+    difference = estimated1 - estimated0
+
+    # Changes to(/from) estimated from(/to) not estimated
+    l1 = np.abs(difference).sum()
+
+    # Change in the number of estimated parameters.
+    size = np.sum(difference)
+
+    # TODO constants?
+    distances = {
+        'l1': l1,
+        'size': size,
+    }
+
+    return distances
+'''
---
+++ packages/petab-select/petab_select-0.0.9.tar.gz/petab_select-0.0.9/petab_select/problem.py
@@ -0,0 +1,252 @@
+"""The model selection problem class."""
+import abc
+from functools import partial
+from itertools import chain
+from pathlib import Path
+from typing import Callable, Iterable, Optional, Union
+import yaml
+
+from .candidate_space import (
+    CandidateSpace,
+    method_to_candidate_space_class,
+)
+from .constants import (
+    CRITERION,
+    METHOD,
+    MODEL_SPACE_FILES,
+    VERSION,
+    Criterion,
+    Method,
+)
+from .model import (
+    Model,
+    default_compare,
+)
+from .model_space import ModelSpace
+
+
+class Problem(abc.ABC):
+    """Handle everything related to the model selection problem.
+
+    Attributes:
+        model_space:
+            The model space.
+        calibrated_models:
+            Calibrated models. Will be used to augment the model selection problem (e.g.
+            by excluding them from the model space).
+        compare:
+            A method that compares models by selection criterion.
+        criterion:
+            The criterion used to compare models.
+        method:
+            The method used to search the model space.
+        version:
+            The version of the PEtab Select format.
+        yaml_path:
+            The location of the selection problem YAML file. Used for relative
+            paths that exist in e.g. the model space files.
+            TODO should the relative paths be relative to the YAML or the
+            file that contains them?
+
+    Unsaved attributes:
+        candidate_space:
+            The candidate space that will be used.
+            Reason for not saving:
+                Essentially reproducible from `Problem.method` and
+                `Problem.calibrated_models`.
+    """
+    def __init__(
+        self,
+        model_space: ModelSpace,
+        compare: Callable[[Model, Model], bool] = None,
+        criterion: Criterion = None,
+        method: str = None,
+        version: str = None,
+        yaml_path: str = None,
+    ):
+        self.model_space = model_space
+        self.criterion = criterion
+        self.method = method
+        self.version = version
+        self.yaml_path = yaml_path
+
+        self.compare = compare
+        if self.compare is None:
+            self.compare = partial(default_compare, criterion=self.criterion)
+
+        self.calibrated_models = []
+
+    def get_path(self, relative_path: Union[str, Path]) -> Path:
+        """Get the path to a resource, from a relative path.
+
+        Args:
+            relative_path:
+                The path to the resource, that is relative to the PEtab Select
+                problem YAML file location.
+
+        Returns:
+            The path to the resource.
+
+        TODO:
+            Unused?
+        """
+        if self.yaml_path is None:
+            return Path(relative_path)
+        return self.yaml_path.parent / relative_path
+
+    def add_calibrated_models(
+        self,
+        models: Iterable[Model],
+        exclude: bool = True,
+    ) -> None:
+        """Add calibrated models to the history.
+
+        Args:
+            models:
+                The models to add to the history.
+            exclude:
+                Whether to exclude the hashes of the models from the model space.
+        """
+        self.calibrated_models = list(chain(self.calibrated_models, models))
+        self.model_space.exclude_models(models)
+
+    def set_state(
+        self,
+        state,
+    ) -> None:
+        """Set the state of the problem.
+
+        Currently, only the excluded models needs to be stored.
+
+        Args:
+            state:
+                The state to restore to.
+        """
+        self.add_calibrated_models(state)
+
+    def get_state(
+        self,
+    ):
+        """Get the state of the problem.
+
+        Currently, only the excluded models needs to be stored.
+
+        Returns:
+            The current state of the problem.
+        """
+        return self.calibrated_models
+
+    @staticmethod
+    def from_yaml(
+        yaml_path: Union[str, Path],
+    ) -> 'Problem':
+        """Generate a problem from a PEtab Select problem YAML file.
+
+        Args:
+            yaml_path:
+                The location of the PEtab Select problem YAML file.
+
+        Returns:
+            A `Problem` instance.
+        """
+        yaml_path = Path(yaml_path)
+        with open(yaml_path, 'r') as f:
+            problem_specification = yaml.safe_load(f)
+
+        if not problem_specification.get(MODEL_SPACE_FILES, []):
+            raise KeyError(
+                'The model selection problem specification file is missing '
+                'model space files.'
+            )
+
+        model_space = ModelSpace.from_files(
+            #problem_specification[MODEL_SPACE_FILES],
+            [
+                # `pathlib.Path` appears to handle absolute `model_space_file` paths
+                # correctly, even if used as a relative path.
+                # TODO test
+                # This is similar to the `Problem.get_path` method.
+                yaml_path.parent / model_space_file
+                for model_space_file in problem_specification[MODEL_SPACE_FILES]
+            ],
+            #source_path=yaml_path.parent,
+        )
+
+        criterion = problem_specification.get(CRITERION, None)
+        if criterion is not None:
+            criterion = Criterion(criterion)
+
+        return Problem(
+            model_space=model_space,
+            criterion=criterion,
+            # TODO refactor method to use enum
+            method=problem_specification.get(METHOD, None),
+            version=problem_specification.get(VERSION, None),
+            yaml_path=yaml_path,
+        )
+
+    def get_best(
+        self,
+        models: Optional[Iterable[Model]] = None,
+        criterion: Optional[Union[str, None]] = None,
+    ) -> Model:
+        """Get the best model from a collection of models.
+
+        The best model is selected based on the selection problem's criterion.
+
+        Args:
+            models:
+                The best model will be taken from these models. Defaults to
+                `self.calibrated_models`.
+            criterion:
+                The criterion by which models will be compared. Defaults to
+                `self.criterion` (e.g. as defined in the PEtab Select problem YAML
+                file).
+
+        Returns:
+            The best model.
+        """
+        if criterion is None:
+            criterion = self.criterion
+        # TODO check if commenting this out broke behavior somewhere
+        #if models is not None:
+        #    self.add_calibrated_models(models)
+        # TODO refactor s.t. `self.calibrated_models is None` when empty.
+        if models is None:
+            if self.calibrated_models:
+                models = self.calibrated_models
+            else:
+                raise ValueError('There are no calibrated models in the problem, and no models were supplied.')  # noqa: E501
+
+        best_model = None
+        for model in models:
+            if best_model is None:
+                if model.has_criterion(criterion):
+                    best_model = model
+                # TODO warn if criterion is not available?
+                continue
+            if self.compare(best_model, model):
+                best_model = model
+        if best_model is None:
+            raise KeyError(f'None of the supplied models have a value set for the criterion {criterion}.')  # noqa: E501
+        return best_model
+
+    def new_candidate_space(
+        self,
+        *args,
+        method: Method = None,
+        **kwargs,
+    ) -> None:
+        """Construct a new candidate space.
+
+        Args:
+            *args, **kwargs:
+                Arguments are passed to the candidate space constructor.
+            method:
+                The model selection method.
+        """
+        if method is None:
+            method = self.method
+        candidate_space_class = method_to_candidate_space_class(method)
+        candidate_space = candidate_space_class(*args, **kwargs)
+        return candidate_space
