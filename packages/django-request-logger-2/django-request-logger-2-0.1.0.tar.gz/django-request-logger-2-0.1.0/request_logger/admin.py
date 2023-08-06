42643c185a4a80535fd9527ee25a0fb6459f07f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:32:07 +0000

    Add package django-request-logger-2/0.1.0/django-request-logger-2-0.1.0.tar.gz

---
+++ packages/django-request-logger-2/django-request-logger-2-0.1.0.tar.gz/django-request-logger-2-0.1.0/request_logger/admin.py
@@ -0,0 +1,36 @@
+from django.contrib import admin
+
+from .models import RequestLog
+
+
+@admin.register(RequestLog)
+class RequestLogAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "user",
+        "reference",
+        "http_method",
+        "request_uri",
+        "http_status_code",
+        "duration",
+    )
+    readonly_fields = (
+        "user",
+        "timestamp",
+        "session_key",
+        "http_method",
+        "hostname",
+        "path",
+        "query",
+        "remote_addr",
+        "http_referer",
+        "http_user_agent",
+        "request_accepts",
+        "request_content_type",
+        "response_content_type",
+        "http_status_code",
+        "content_length",
+        "redirect_to",
+        "duration",
+    )
+    exclude = ("request_uri",)
