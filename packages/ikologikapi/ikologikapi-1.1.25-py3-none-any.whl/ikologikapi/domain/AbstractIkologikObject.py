43c05eb86b51a1391620c77906c56977999ec256
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:01 +0000

    Add package ikologikapi/1.1.25/ikologikapi-1.1.25-py3-none-any.whl

---
+++ packages/ikologikapi/ikologikapi-1.1.25-py3-none-any.whl/ikologikapi/domain/AbstractIkologikObject.py
@@ -0,0 +1,7 @@
+class AbstractIkologikObject(object):
+
+    def __init__(self):
+        super().__init__()
+        self.id = None
+        self.createdDate = None
+        self.modifiedDate = None
