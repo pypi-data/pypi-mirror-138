76fe337b6aab4361191d223a1d293221f9b80878
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:06 +0000

    Add package dotcfg/0.1.5/dotcfg-0.1.5-py3-none-any.whl

---
+++ packages/dotcfg/dotcfg-0.1.5-py3-none-any.whl/dotcfg/__init__.py
@@ -0,0 +1,9 @@
+__version__ = "0.1.5"
+import dotcfg.collections
+import dotcfg.configuration
+import dotcfg.engine
+import dotcfg.errors
+import dotcfg.types
+from dotcfg.collections import Config
+from dotcfg.configuration import load_configuration
+from dotcfg.utils import set_temporary_config
