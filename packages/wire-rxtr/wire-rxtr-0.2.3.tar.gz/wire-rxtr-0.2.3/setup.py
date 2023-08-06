589f09960d65dcb2b873860b82939d9e77afbb4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:53 +0000

    Add package wire-rxtr/0.2.3/wire-rxtr-0.2.3.tar.gz

---
+++ packages/wire-rxtr/wire-rxtr-0.2.3.tar.gz/wire-rxtr-0.2.3/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['wire_rxtr']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'wire-rxtr',
+    'version': '0.2.3',
+    'description': '',
+    'long_description': None,
+    'author': 'cheetahbyte',
+    'author_email': 'bernerdoodle@outlook.de',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
