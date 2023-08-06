6c660ff86c06299777f9cf7ac2f3f8fa1a3f87c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:39:54 +0000

    Add package document-utils/1.7.1/document-utils-1.7.1.tar.gz

---
+++ packages/document-utils/document-utils-1.7.1.tar.gz/document-utils-1.7.1/doc_utils/doc_utils.py
@@ -0,0 +1,9 @@
+from .chunk_doc_utils import ChunkDocUtils
+
+
+class DocUtils(ChunkDocUtils):
+    """Class for all document utilities.
+    Primarily should be used as a mixin for future functions
+    but can be a standalone.
+    # TODO: Extend to Chunk Doc Reading and Chunk Doc Writing
+    """
