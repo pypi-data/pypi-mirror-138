dbb0c8802b6f9edddcb922f0d64135bf714c2359
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:06:35 +0000

    Add package throttled/0.1.16a29/throttled-0.1.16a29-py3-none-any.whl

---
+++ packages/throttled/throttled-0.1.16a29-py3-none-any.whl/throttled/fastapi/utils.py
@@ -0,0 +1,26 @@
+from typing import List, Tuple, Union
+
+from fastapi import Depends
+from starlette.middleware.base import DispatchFunction
+
+from .base import FastAPILimiter, MiddlewareLimiter
+
+
+def split_dependencies_and_middlewares(
+    *limiters: Union[FastAPILimiter, MiddlewareLimiter]
+) -> Tuple[List[Depends], List[DispatchFunction]]:
+
+    dispatch_functions: List[DispatchFunction] = []
+    dependencies: List[Depends] = []
+
+    for limiter in limiters:
+        if isinstance(limiter, MiddlewareLimiter):
+            dispatch_functions.append(limiter.dispatch)
+        elif isinstance(limiter, FastAPILimiter):
+            if callable(limiter):
+                dependencies.append(Depends(limiter))
+            else:
+                raise TypeError(f"Object {limiter} is not Callable.")
+        else:
+            raise TypeError(f"Object {limiter} is not a Middleware or Limiter.")
+    return dependencies, dispatch_functions
