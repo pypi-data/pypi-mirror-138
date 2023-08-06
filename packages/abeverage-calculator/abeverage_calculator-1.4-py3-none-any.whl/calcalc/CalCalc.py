5cbca838a3f46131cfed1cbf6c270f798412f525
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:45:27 +0000

    Add package abeverage-calculator/1.4/abeverage_calculator-1.4-py3-none-any.whl

---
+++ packages/abeverage-calculator/abeverage_calculator-1.4-py3-none-any.whl/calcalc/CalCalc.py
@@ -0,0 +1,90 @@
+import numexpr as ne
+import urllib.request
+
+appID = 'JWKAQ4-U2LKHL8YEE'
+
+
+def eval_wolfram(s, return_float=True):
+    s = s.replace(' ','+')
+    f = urllib.request.urlopen(f'http://api.wolframalpha.com/v1/result?appid={appID}&i={s}%3F&units=metric')
+    result = str(f.peek(),"utf-8")
+    result = result.replace('about ','') # remove the `about`
+
+    # convert result to a float
+    if return_float: 
+        result = result.replace(' times 10 to the ','e') # put in scientific notation
+        # replace common words with numerical value
+        sub_words = {' thousand':'e3', ' million':'e6', ' billion':'e8', ' trillion':'e9'}
+        for key in sub_words.keys():
+            result = result.replace(key, sub_words[key])
+        return(float(result.split(' ')[0]))
+
+    # or return the string
+    else: return(result)
+
+def calculate(s, run_python=False, run_wolfram=False, return_float=True):
+    '''
+    use numexpr to evaluate string expression
+    return result in desired format
+    '''
+    if run_python:
+        try: 
+            return(ne.evaluate(s).item())
+        except Exception as e:
+            print("Oops!", e.__class__, "occurred.")
+            return("Perhaps try running with Wolfram (-w flag)")
+    
+    elif run_wolfram:
+        return eval_wolfram(s, return_float)
+    
+    # if for some reason both python and wolfram flags are set, try evaluating
+    # in python. If test fails, evaluate in wolfram
+    else: 
+        try: 
+            return(ne.evaluate(s).item())
+        except:
+            return eval_wolfram(s, return_float)
+        
+
+def test_1():
+    assert abs(4. - calculate('2**2')) < 0.001
+    
+def test_2():
+    assert isinstance(calculate('mass of the earth',return_float=True), float)
+    
+def test_3():
+    assert abs(1e6 - calculate('one million')) < 0.01
+    
+def test_4():
+    assert abs(4. - calculate('2+2')) < 0.001
+    
+def test_5():
+    print(type(calculate('2**2',return_float=False)))
+    assert isinstance(calculate('mass of the earth',return_float=False), str)
+
+    
+if __name__ == '__main__':
+    import argparse
+    
+    # parse command line arguments
+    parser = argparse.ArgumentParser(description='Evaluate a string.')
+    
+    # define input argument
+    parser.add_argument('string',
+                       help='String to be evaulated')
+    
+    # -s will try to evaluate using python
+    parser.add_argument('-s', action='store_true', default=False,
+                        dest='run_python',
+                        help='Run in python?')
+    
+    # -w will try to evaluate using wolfram
+    parser.add_argument('-w', action='store_true', default=False,
+                        dest='run_wolfram',
+                        help='Send to wolfram?')
+    # add versioning
+    parser.add_argument('--version', action='version', version='%(prog)s 1.1')
+    
+    results = parser.parse_args()
+    print(calculate(results.string, run_python=results.run_python, run_wolfram=results.run_wolfram))
+    
