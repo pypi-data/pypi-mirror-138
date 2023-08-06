2f8fabb2be28973f4bc89a89e0e8a894afe79528
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:24 +0000

    Add package pyrolab/0.2.1/PyroLab-0.2.1.tar.gz

---
+++ packages/pyrolab/PyroLab-0.2.1.tar.gz/PyroLab-0.2.1/pyrolab/drivers/scopes/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright © PyroLab Project Contributors
+# Licensed under the terms of the GNU GPLv3+ License
+# (see pyrolab/__init__.py for details)
+
+"""
+Oscilloscopes
+=============
+
+Submodule containing drivers for each supported scope type.
+"""
+
+from pyrolab.drivers import Instrument
+
+
+class Scope(Instrument):
+    pass
