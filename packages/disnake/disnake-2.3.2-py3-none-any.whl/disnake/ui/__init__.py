0852b55c9c2906aa1ec450f82a9d75447972894c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:17:47 +0000

    Add package disnake/2.3.2/disnake-2.3.2-py3-none-any.whl

---
+++ packages/disnake/disnake-2.3.2-py3-none-any.whl/disnake/ui/__init__.py
@@ -0,0 +1,15 @@
+"""
+disnake.ui
+~~~~~~~~~~~
+
+Bot UI Kit helper for the Discord API
+
+:copyright: (c) 2015-present Rapptz
+:license: MIT, see LICENSE for more details.
+
+"""
+
+from .button import *
+from .item import *
+from .select import *
+from .view import *
