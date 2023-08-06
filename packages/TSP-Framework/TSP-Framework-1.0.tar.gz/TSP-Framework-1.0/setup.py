cd16732316be4d22abad241c31288e3c00ab464c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:42:21 +0000

    Add package TSP-Framework/1.0/TSP-Framework-1.0.tar.gz

---
+++ packages/TSP-Framework/TSP-Framework-1.0.tar.gz/TSP-Framework-1.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+setup(
+    name='TSP-Framework',
+    version='1.00',
+    description='Framework para resolver el problema del vendedor viajero aplicando metaheristicas como Simulated Annealing y Genetic Algorithm',
+    long_description=open('README.md').read(),
+    author='Javier del Canto, Jorge Polanco',
+    author_email='javier.delcanto.m@mail.pucv.cl, jorge.polanco.sanmartin@gmail.com',
+    url='https://github.com/Javernaver/TSP-Framework',
+    scripts=['tspf.py'],
+    packages=['src.tspf','src.tspf.Algorithms', 'src.tspf.Tools'],
+    install_requires=['matplotlib', 'prettytable'], 
+    zip_safe=False,
+    classifiers=[
+        'License :: Freeware',
+        'Programming Language :: Python :: 3.9',
+    ],
+    include_package_data=True,
+    package_data={
+        '': ['instances/*.tsp']
+    },
+    entry_points={
+        'console_scripts':[
+            'tspf = tspf:main'
+        ]
+    },
+)
