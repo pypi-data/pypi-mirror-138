58b0a688e8c1ebd07025e563f8e4eb5e000ee425
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:37 +0000

    Add package dataflows-airtable/0.0.6/dataflows_airtable-0.0.6-py2.py3-none-any.whl

---
+++ packages/dataflows-airtable/dataflows_airtable-0.0.6-py2.py3-none-any.whl/dataflows_airtable/utilities.py
@@ -0,0 +1,27 @@
+import os
+import time
+import requests
+
+
+class RateLimiter():
+    def __init__(self, delay=0.2):
+        self.delay = delay
+        self.last = None
+
+    def execute(self, func):
+        curtime = time.time()
+        if self.last is not None and curtime - self.last < self.delay:
+            time.sleep(self.delay - (curtime - self.last))
+        self.last = time.time()
+        return func()
+
+rate_limiter = RateLimiter()
+
+
+def get_session(apikey):
+    if apikey.startswith('env://'):
+        apikey = apikey[6:]
+        apikey = os.environ[apikey]
+    session = requests.Session()
+    session.headers = dict(Authorization=f'Bearer {apikey}')
+    return session
