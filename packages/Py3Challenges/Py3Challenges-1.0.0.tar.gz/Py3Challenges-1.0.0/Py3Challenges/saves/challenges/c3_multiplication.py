e2a67f24d14062040b78140d8f9bd5eb7840fa20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:29:59 +0000

    Add package Py3Challenges/1.0.0/Py3Challenges-1.0.0.tar.gz

---
+++ packages/Py3Challenges/Py3Challenges-1.0.0.tar.gz/Py3Challenges-1.0.0/Py3Challenges/saves/challenges/c3_multiplication.py
@@ -0,0 +1,29 @@
+"""
+To master this you should consider using the ``*``-operator.
+"""
+from ...challenge import Challenge
+from random import randint
+
+
+x = randint(1, 100)
+y = randint(1, 100)
+
+intro = f"You have to print the product of {x} and {y}."
+
+
+def validate_function(stdin: str, stdout: str, stderr: str, exc: tuple) -> bool:
+    try:
+        z = int(stdout.removesuffix("\n"))
+    except ValueError:
+        return False
+    else:
+        return (x * y) == z
+
+
+challenge = Challenge(
+    intro=intro,
+    validate_function=validate_function,
+    help=__doc__,
+    values={"x": x, "y": y},
+    capture_stdout=True,
+)
---
+++ packages/Py3Challenges/Py3Challenges-1.0.0.tar.gz/Py3Challenges-1.0.0/README.md
@@ -0,0 +1,18 @@
+# Py3Challenges
+A collection of challenges for programming beginners.
+
+
+## Installation
+via pip:
+```shell
+pip3 install Py3Challenges -U
+```
+
+
+## Getting Started
+Try this script:
+```python
+import Py3Challenges as Py3C
+Py3C.getting_started()
+# read the instructions in the terminal
+```
