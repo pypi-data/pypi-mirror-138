27ebe0223d41110c903f60c6b53d97037bfceff9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:29:49 +0000

    Add package cplot/0.8.7/cplot-0.8.7-py3-none-any.whl

---
+++ packages/cplot/cplot-0.8.7-py3-none-any.whl/cplot/__init__.py
@@ -0,0 +1,28 @@
+from .__about__ import __version__
+from ._colors import get_srgb1
+from ._main import Plotter, plot, plot_abs, plot_arg, plot_contours, plot_phase
+from ._tri import tricontour_abs, tripcolor
+from .benchmark import show_kovesi_test_image, show_test_function
+from .create import create_colormap, find_max_srgb_radius, show_circular, show_linear
+
+__all__ = [
+    "show_test_function",
+    "show_kovesi_test_image",
+    "show_linear",
+    "show_circular",
+    "find_max_srgb_radius",
+    "create_colormap",
+    "get_srgb1",
+    "plot",
+    "plot_arg",
+    "plot_abs",
+    "plot_phase",
+    "plot_contours",
+    #
+    "tripcolor",
+    "tricontour_abs",
+    #
+    "Plotter",
+    #
+    "__version__",
+]
