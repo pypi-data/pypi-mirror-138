911f037c3439eeceb95034d35400f1089781e190
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:30:48 +0000

    Add package miko-tpl/1.3.2/miko_tpl-1.3.2-py3-none-any.whl

---
+++ packages/miko-tpl/miko_tpl-1.3.2-py3-none-any.whl/miko/__init__.py
@@ -0,0 +1,12 @@
+# miko by tasuren
+
+from .template import (
+    DEFAULT_BUILTINS, DEFAULT_ADJUSTORS, Adjustor,
+    Template, Block, CacheManager, caches
+)
+from .manager import Manager
+from . import builtins
+
+
+__version__ = "1.3.2"
+__author__ = "tasuren"
