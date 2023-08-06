68a3ac9c0b3700f4054642428ff86021bf9e33b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:43:27 +0000

    Add package pnav/0.2.2/pnav-0.2.2.tar.gz

---
+++ packages/pnav/pnav-0.2.2.tar.gz/pnav-0.2.2/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "readme.txt").read_text()
+
+setuptools.setup(
+    name='pnav',
+    version='0.2.2',
+    author='Zander Navratil',
+    description="Package with random functions, some are useful, some aren't so much. They are however used often, maintained, and good for learning.",
+    packages=['pnav'],
+    long_description=long_description,
+    long_description_content_type='text/markdown')
