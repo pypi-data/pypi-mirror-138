909b90ba949c2eb7daee6382e0c61d610cff1915
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:17:04 +0000

    Add package test-oglop-proj-b/0.1.0/test-oglop-proj-b-0.1.0.tar.gz

---
+++ packages/test-oglop-proj-b/test-oglop-proj-b-0.1.0.tar.gz/test-oglop-proj-b-0.1.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['test_oglop_proj_b']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['test-oglop-proj-a>=0.1.0,<0.2.0']
+
+setup_kwargs = {
+    'name': 'test-oglop-proj-b',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': None,
+    'author': 'Jan Gazda',
+    'author_email': '7480694+1oglop1@users.noreply.github.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
