b29b431611fe579ef37a368ff658f6cfe6b8e2ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:26 +0000

    Add package imio.smartweb.core/1.0a36/imio.smartweb.core-1.0a36.tar.gz

---
+++ packages/imio.smartweb.core/imio.smartweb.core-1.0a36.tar.gz/imio.smartweb.core-1.0a36/src/imio/smartweb/core/webcomponents/src/components/News/ContactList/ContactList.jsx
@@ -0,0 +1,36 @@
+import ContactCard from "../ContactCard/ContactCard";
+import { Link } from "react-router-dom";
+import React from "react";
+import removeAccents from 'remove-accents';
+const ContactList = ({ contactArray, onChange, parentCallback }) => {
+    function handleClick(event) {
+        onChange(event);
+    }
+    return (
+        <React.Fragment>
+            <ul className="r-result-list actu-result-list">
+                {contactArray.map((contactItem, i) =>
+                (<li
+                    key={i}
+                    className="r-list-item-group"
+                    onClick={() => handleClick(contactItem.UID)}
+                >
+                    <Link
+                        className="r-list-item-link"
+                        style={{ textDecoration: "none" }}
+                        to={{
+                            pathname: removeAccents(contactItem.title.replace(/\s/g, '-').toLowerCase()),
+                            search: `?u=${contactItem.UID}`,
+                            state: {
+                                idItem: contactItem.UID,
+                            },
+                        }}
+                    ></Link>
+                    <ContactCard contactItem={contactItem} key={contactItem.created} />
+                </li>)
+                )}
+            </ul>
+        </React.Fragment>
+    );
+};
+export default ContactList;
