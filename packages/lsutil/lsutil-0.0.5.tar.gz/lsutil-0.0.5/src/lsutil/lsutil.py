a270f8a48897489f10ab3cdc1cb7f2bbaa48754b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:26:57 +0000

    Add package lsutil/0.0.5/lsutil-0.0.5.tar.gz

---
+++ packages/lsutil/lsutil-0.0.5.tar.gz/lsutil-0.0.5/src/lsutil/lsutil.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+""" Unix ls command in Python """
+
+import stat
+import argparse
+import os
+
+
+class lsutil:  # pylint: disable = invalid-name
+    """Unix ls command in Python"""
+
+    def __init__(self):
+        self.args = argparse.Namespace()
+
+    def ls(
+        self,
+        pathnames: list,
+        one: bool = False,
+        long: bool = False,
+        classify: bool = False,
+        capture: list = None,
+        formats: str = None,
+    ):  # pylint: disable=invalid-name, too-many-arguments
+        """Unix ls command clone"""
+        if not pathnames:
+            pathnames = ["."]
+        for name in pathnames:
+            with os.scandir(name) as it:
+                for entry in it:
+                    classify = ""
+                    if entry.is_dir():
+                        classify = "/"
+                    if self.args.long:
+                        print(stat.filemode(entry.stat().st_mode), end=" ")
+                    print(f"{entry.name}{classify}")
+
+    def parse_cli(self):
+        """Parse Unix command line arguments"""
+        parser = argparse.ArgumentParser(description="Process some integers.")
+        parser.add_argument("files", metavar="file", nargs="*", help="Files or directories")
+        parser.add_argument(
+            "-l",
+            "--long",
+            action="store_true",
+            help="use a long listing format",
+        )
+
+        self.args = parser.parse_args()
+        print(self.args)
+
+    @staticmethod
+    def main_cli():
+        """Unix command line interface"""
+        self = lsutil()
+        self.parse_cli()
+        self.ls(self.args.files)
+
+
+if __name__ == "__main__":
+
+    lsutil.main_cli()
