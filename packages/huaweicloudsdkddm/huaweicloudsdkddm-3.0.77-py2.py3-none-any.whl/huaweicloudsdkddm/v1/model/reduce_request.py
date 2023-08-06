275a83ee2d46fa8f703c14e621353488824cef98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:42:43 +0000

    Add package huaweicloudsdkddm/3.0.77/huaweicloudsdkddm-3.0.77-py2.py3-none-any.whl

---
+++ packages/huaweicloudsdkddm/huaweicloudsdkddm-3.0.77-py2.py3-none-any.whl/huaweicloudsdkddm/v1/model/reduce_request.py
@@ -0,0 +1,113 @@
+# coding: utf-8
+
+import re
+import six
+
+
+
+from huaweicloudsdkcore.utils.http_utils import sanitize_for_serialization
+
+
+class ReduceRequest:
+
+
+    """
+    Attributes:
+      openapi_types (dict): The key is attribute name
+                            and the value is attribute type.
+      attribute_map (dict): The key is attribute name
+                            and the value is json key in definition.
+    """
+
+    sensitive_list = []
+
+    openapi_types = {
+        'node_number': 'int'
+    }
+
+    attribute_map = {
+        'node_number': 'node_number'
+    }
+
+    def __init__(self, node_number=None):
+        """ReduceRequest - a model defined in huaweicloud sdk"""
+        
+        
+
+        self._node_number = None
+        self.discriminator = None
+
+        self.node_number = node_number
+
+    @property
+    def node_number(self):
+        """Gets the node_number of this ReduceRequest.
+
+        需要缩容的节点数量，最大值为实例节点数减1。
+
+        :return: The node_number of this ReduceRequest.
+        :rtype: int
+        """
+        return self._node_number
+
+    @node_number.setter
+    def node_number(self, node_number):
+        """Sets the node_number of this ReduceRequest.
+
+        需要缩容的节点数量，最大值为实例节点数减1。
+
+        :param node_number: The node_number of this ReduceRequest.
+        :type: int
+        """
+        self._node_number = node_number
+
+    def to_dict(self):
+        """Returns the model properties as a dict"""
+        result = {}
+
+        for attr, _ in six.iteritems(self.openapi_types):
+            value = getattr(self, attr)
+            if isinstance(value, list):
+                result[attr] = list(map(
+                    lambda x: x.to_dict() if hasattr(x, "to_dict") else x,
+                    value
+                ))
+            elif hasattr(value, "to_dict"):
+                result[attr] = value.to_dict()
+            elif isinstance(value, dict):
+                result[attr] = dict(map(
+                    lambda item: (item[0], item[1].to_dict())
+                    if hasattr(item[1], "to_dict") else item,
+                    value.items()
+                ))
+            else:
+                if attr in self.sensitive_list:
+                    result[attr] = "****"
+                else:
+                    result[attr] = value
+
+        return result
+
+    def to_str(self):
+        """Returns the string representation of the model"""
+        import simplejson as json
+        if six.PY2:
+            import sys
+            reload(sys)
+            sys.setdefaultencoding("utf-8")
+        return json.dumps(sanitize_for_serialization(self), ensure_ascii=False)
+
+    def __repr__(self):
+        """For `print`"""
+        return self.to_str()
+
+    def __eq__(self, other):
+        """Returns true if both objects are equal"""
+        if not isinstance(other, ReduceRequest):
+            return False
+
+        return self.__dict__ == other.__dict__
+
+    def __ne__(self, other):
+        """Returns true if both objects are not equal"""
+        return not self == other
