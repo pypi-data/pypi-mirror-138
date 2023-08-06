4fff86ed85366205c4a14522f98f63d4621902de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:37 +0000

    Add package src12/0.1.0/src12-0.1.0.tar.gz

---
+++ packages/src12/src12-0.1.0.tar.gz/src12-0.1.0/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='src12',
+    version='0.1.0',
+    packages=['src12']
+)
---
+++ packages/src12/src12-0.1.0.tar.gz/src12-0.1.0/src12/configtrans1.py
@@ -0,0 +1,31 @@
+import numpy as np
+import pandas as pd
+
+from sklearn.base import BaseEstimator, TransformerMixin
+
+class cleandatatransformer(BaseEstimator, TransformerMixin):
+    
+    def __init__(self):
+        print('init cleandatatransformer')
+        
+    def fit(self, X, y = None):
+        print("fit cleandatatransformer!")
+        return self
+
+
+    def transform(self, X, y = None, df):
+        if(len(X.columns) == 7):
+            print("transform cleandatatransformer 7")
+            df = X
+            tbl1 = pd.pivot_table(df,index=['year','month','day','hour'],values='power',columns ='loggername',aggfunc=sum)
+            tbl2=tbl1.fillna(0)
+            tbl2[tbl2 < 0] = 0
+            tbl2.reset_index(inplace=True)
+            print(tbl2)
+            return tbl2
+        elif (len(X.columns) == 2):
+            print("transform cleandatatransformer 2")
+            anomalies_df = X
+            result = pd.concat([df, anomalies_df], axis=1)
+            print(result)
+            return result
