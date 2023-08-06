38653ce49bec1503d9aa3d8de3e15aabe1064475
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:28 +0000

    Add package metaflow-card-notebook/1.0.2/metaflow-card-notebook-1.0.2.tar.gz

---
+++ packages/metaflow-card-notebook/metaflow-card-notebook-1.0.2.tar.gz/metaflow-card-notebook-1.0.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_namespace_packages, setup
+
+def get_long_description() -> str:
+    with open("README.md") as fh:
+        return fh.read()
+
+setup(
+    name="metaflow-card-notebook",
+    version="1.0.2",
+    description="Render Jupyter Notebooks in Metaflow Cards",
+    long_description=get_long_description(),
+    long_description_content_type="text/markdown",
+    author="Hamel Husain",
+    author_email="hamel@outerbounds.com",
+    license="Apache Software License 2.0",
+    packages=find_namespace_packages(include=['metaflow_extensions.*']),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        "ipykernel==6.4.1",
+        "papermill==2.3.3",
+        "nbconvert==6.4.1",
+        "nbformat==5.1.3",
+        "metaflow",
+    ],
+)
