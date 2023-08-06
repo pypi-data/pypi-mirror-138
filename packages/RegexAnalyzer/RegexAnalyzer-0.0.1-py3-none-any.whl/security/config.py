ed6da5cdd762038b9f0c67f7b0fee470d245a25a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:33 +0000

    Add package RegexAnalyzer/0.0.1/RegexAnalyzer-0.0.1-py3-none-any.whl

---
+++ packages/RegexAnalyzer/RegexAnalyzer-0.0.1-py3-none-any.whl/security/config.py
@@ -0,0 +1,9 @@
+# Genetic algorithm constants
+
+max_len = 64                 # max length of any matching string 
+num_seeds = 100              # number of seeds initionally generated
+number_of_survivors = 20     # number of strings left in a generation after culling
+timeout = 1
+pump = 500
+print_iter = 100
+
