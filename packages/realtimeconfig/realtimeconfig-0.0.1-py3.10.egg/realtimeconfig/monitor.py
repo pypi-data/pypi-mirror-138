5c2ea7f484385d5c633b2f628483ad2e02273000
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:35 +0000

    Add package realtimeconfig/0.0.1/realtimeconfig-0.0.1-py3.10.egg

---
+++ packages/realtimeconfig/realtimeconfig-0.0.1-py3.10.egg/realtimeconfig/monitor.py
@@ -0,0 +1,27 @@
+import sys
+from realtimeconfig import get_queue, startup
+import argparse
+
+
+def main():
+    namespace = parser.parse_args(sys.argv[1:])
+    startup(namespace.key, namespace.url)
+    
+    queue = get_queue()
+
+    while True:
+        k, v = queue.get()
+        print(k, v)
+
+
+parser = argparse.ArgumentParser(description='realtimeconfig-monitor')
+parser.add_argument('--key',
+                    required=True,
+                    help='Your realtimeconfig API key')
+parser.add_argument('--url',
+                    default='wss://stream.realtimeconfig.com:1443/',
+                    help='The realtimeconfig source.  Change this if you are using a proxy')
+
+
+if __name__ == "__main__":
+    main()
