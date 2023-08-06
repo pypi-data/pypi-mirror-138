d90a6bdc0f73a0b39352cd797ba1904c829faf24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:41 +0000

    Add package bacon-net/0.1.0/bacon-net-0.1.0.tar.gz

---
+++ packages/bacon-net/bacon-net-0.1.0.tar.gz/bacon-net-0.1.0/setup.py
@@ -0,0 +1,47 @@
+from setuptools import setup
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.1.0'
+DESCRIPTION = 'A neural network architecture for building fully explainable neural network for arithmetic and gradient logic expression approximation.'
+
+# Setting up
+setup(
+    name="bacon-net",
+    version=VERSION,
+    url="https://github.com/Haishi2016/bacon-net",
+    author="haishibai (Haishi Bai)",
+    author_email="<haishi.bai@live.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    install_requires=[
+        "tensorflow >= 2.7.0",
+        "keras >= 2.7.0",
+        "numpy >= 1.22.0",
+        "simpy >= 4.0.1"
+    ],
+    keywords=['ai', 'explainable', 'gradient logic',
+              'approximation', 'formula', 'explainability', 'decision', 'decision making'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: Apache Software License",
+    ],
+    extras_require={
+        "dev": [
+            "pytest >= 3.7",
+        ],
+    },
+    py_modules=["bacon", "stack", "nets"],
+    package_dir={'': 'src'}
+)
