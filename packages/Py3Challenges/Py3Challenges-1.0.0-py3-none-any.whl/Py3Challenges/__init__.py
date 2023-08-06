e25be96d4c2076d241155b99fb91356ba4444961
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:29:59 +0000

    Add package Py3Challenges/1.0.0/Py3Challenges-1.0.0-py3-none-any.whl

---
+++ packages/Py3Challenges/Py3Challenges-1.0.0-py3-none-any.whl/Py3Challenges/__init__.py
@@ -0,0 +1,16 @@
+__author__ = "AlbertUnruh"
+__url__ = "https://github.com/AlbertUnruh/Py3Challenges"
+__license__ = "MIT"
+__copyright__ = f"(c) 2022 - present {__author__}"
+__version__ = "1.0.0"
+__description__ = "A collection of challenges for programming beginners."
+
+
+from . import helper
+from . import challenge
+
+from .helper import *
+from .challenge import *
+
+
+all_challenges: list[Challenge] = load_challenges()
