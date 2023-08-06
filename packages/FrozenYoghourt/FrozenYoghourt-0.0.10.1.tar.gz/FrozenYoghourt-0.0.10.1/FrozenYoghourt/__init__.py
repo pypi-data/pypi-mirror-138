a693a267ff184f7f2eab55e1e5c1779d3dcffdb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:45:31 +0000

    Add package FrozenYoghourt/0.0.10.1/FrozenYoghourt-0.0.10.1.tar.gz

---
+++ packages/FrozenYoghourt/FrozenYoghourt-0.0.10.1.tar.gz/FrozenYoghourt-0.0.10.1/FrozenYoghourt/__init__.py
@@ -0,0 +1,31 @@
+import numpy as np
+from sympy import *
+from sympy.physics.quantum import TensorProduct
+from functools import reduce
+from qiskit.quantum_info import *
+from scipy.optimize import minimize
+from numpy.testing import assert_array_almost_equal as aae
+from typing import *
+
+def default_import(items:list = ['a', 'r', 'g', 'm', 'q']):
+    
+    import_statements = {'a': 'from FrozenYoghourt import *', 
+                         'r': 'from FrozenYoghourt.mode import Mode as r\n', 
+                         'g': 'from FrozenYoghourt.gates import Gates as g\n', 
+                         'm': 'from FrozenYoghourt.maths import Maths as m\n', 
+                         'q': 'from FrozenYoghourt.quantum import Quantum as q'}
+    
+    
+    import_str = ''
+    for item in items:
+        import_str += import_statements[item]
+        
+    print(import_str)
+    
+def view(mat, rounding = 10):
+    if type(mat) == np.ndarray:
+        display(Matrix(np.round(mat, rounding)))
+    else:
+        display(mat.evalf(rounding))
+        
+   
