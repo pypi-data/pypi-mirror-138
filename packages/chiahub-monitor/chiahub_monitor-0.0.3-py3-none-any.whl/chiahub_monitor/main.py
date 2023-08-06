6a3106e15c2cdad20ecf05eebec38c5a1da20f87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:05 +0000

    Add package chiahub-monitor/0.0.3/chiahub_monitor-0.0.3-py3-none-any.whl

---
+++ packages/chiahub-monitor/chiahub_monitor-0.0.3-py3-none-any.whl/chiahub_monitor/main.py
@@ -0,0 +1,30 @@
+import asyncio
+
+from chiahub_monitor.client import ChiaClient
+
+
+async def run(client: ChiaClient):
+
+    while True:
+        try:
+            await client.upload()
+        except:
+            pass
+        print("sleeping")
+        await asyncio.sleep(60 * 60)
+
+
+if __name__ == '__main__':
+
+    client = ChiaClient(
+        wallet_adr="localhost", wallet_port=9256,
+        farmer_adr="localhost", farmer_port=8559,
+        )
+
+    try:
+        asyncio.get_event_loop().run_until_complete(run(client))
+    except KeyboardInterrupt:
+        pass
+
+    print("closing")
+    asyncio.get_event_loop().run_until_complete(client.close())
