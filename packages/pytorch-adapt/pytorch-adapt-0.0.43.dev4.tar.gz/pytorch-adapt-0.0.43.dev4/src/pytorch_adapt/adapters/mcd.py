01b1f949d83ce860399bf1cefb379de2fcf0d26a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:25:45 +0000

    Add package pytorch-adapt/0.0.43.dev4/pytorch-adapt-0.0.43.dev4.tar.gz

---
+++ packages/pytorch-adapt/pytorch-adapt-0.0.43.dev4.tar.gz/pytorch-adapt-0.0.43.dev4/src/pytorch_adapt/adapters/mcd.py
@@ -0,0 +1,40 @@
+from typing import Tuple
+
+import torch
+
+from ..hooks import MCDHook
+from .base_adapter import BaseGCAdapter
+from .utils import with_opt
+
+
+class MCD(BaseGCAdapter):
+    """
+    Extends [BaseGCAdapter][pytorch_adapt.adapters.base_adapter.BaseGCAdapter]
+    and wraps [MCDHook][pytorch_adapt.hooks.MCDHook].
+
+    |Container|Required keys|
+    |---|---|
+    |models|```["G", "C"]```|
+    |optimizers|```["G", "C"]```|
+
+    The C model must output a list of logits, where each list element
+    corresponds with a separate classifier. Usually the number of
+    classifiers is 2, so C should output ```[logits1, logits2]```.
+    """
+
+    hook_cls = MCDHook
+
+    def inference_default(self, x, domain=None) -> Tuple[torch.Tensor, torch.Tensor]:
+        """
+        Returns:
+            Features and logits, where ```logits = sum(C(features))```.
+        """
+        features = self.models["G"](x)
+        logits_list = self.models["C"](features)
+        logits = sum(logits_list)
+        return {"features": features, "logits": logits}
+
+    def init_hook(self, hook_kwargs):
+        self.hook = self.hook_cls(
+            g_opts=with_opt(["G"]), c_opts=with_opt(["C"]), **hook_kwargs
+        )
