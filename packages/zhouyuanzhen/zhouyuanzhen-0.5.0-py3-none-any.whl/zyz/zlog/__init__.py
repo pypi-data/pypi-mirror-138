d5d4273f84173b49b81419a2689635500c4d5698
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:34 +0000

    Add package zhouyuanzhen/0.5.0/zhouyuanzhen-0.5.0-py3-none-any.whl

---
+++ packages/zhouyuanzhen/zhouyuanzhen-0.5.0-py3-none-any.whl/zyz/zlog/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# Author: Yuanzhen Zhou <szrednick@gmail.com>
+
+import datetime
+
+
+def logdemo():
+
+    print("Demo: This is a demo log line ...")
