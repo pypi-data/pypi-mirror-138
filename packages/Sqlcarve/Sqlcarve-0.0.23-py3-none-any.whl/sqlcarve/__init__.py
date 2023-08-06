f2cc13ed9c3995276e348d0290dd57d0d21c35b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:20 +0000

    Add package Sqlcarve/0.0.23/Sqlcarve-0.0.23-py3-none-any.whl

---
+++ packages/Sqlcarve/Sqlcarve-0.0.23-py3-none-any.whl/sqlcarve/__init__.py
@@ -0,0 +1,12 @@
+"""
+
+SqlCarve Module
+---------------
+
+Ce module est la sclasse d'initialisation de la librairie
+"""
+
+
+from sqlcarve.validator.executeQuery import *
+from sqlcarve.validator.validator import *
+from sqlcarve.main import analyse
