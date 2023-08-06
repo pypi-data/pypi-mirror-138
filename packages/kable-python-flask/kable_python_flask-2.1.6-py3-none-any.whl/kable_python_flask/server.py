cc1380b1e41d3bd04a447911dc885fe9b2eefe06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:21 +0000

    Add package kable-python-flask/2.1.6/kable_python_flask-2.1.6-py3-none-any.whl

---
+++ packages/kable-python-flask/kable_python_flask-2.1.6-py3-none-any.whl/kable_python_flask/server.py
@@ -0,0 +1,36 @@
+# Importing flask module in the project is mandatory
+# An object of Flask class is our WSGI application.
+from flask import Flask, request
+from external import configure
+
+# Flask constructor takes the name of
+# current module (__name__) as argument.
+app = Flask(__name__)
+
+# The route() function of the Flask class is a decorator,
+# which tells the application which URL should call
+# the associated function.
+
+# kable = configure({
+#     "environment": "TEST",
+#     "clientId": "stripe",
+#     "clientSecret": "sk_test.SDfZPCLp.8SAR7H2asLE8uMXgBNf8AAf9UGiE8zAt",
+#     "baseUrl": "someUrl"
+# })
+
+kable = configure({
+    "client_id": 'kci_75bfe76dea214218be408fe6fa241f07',
+    "client_secret": 'sk_test.jjrhHXZL.XdSSvBt22dKQdmMvjpiQ9F9v1DGtw26h',
+    "environment": 'TEST',
+    "base_url": 'https://something.com'
+})
+
+
+@app.route('/')
+@kable.authenticate
+def hello_world():
+    return "hello world"
+
+
+if __name__ == '__main__':
+    app.run()
