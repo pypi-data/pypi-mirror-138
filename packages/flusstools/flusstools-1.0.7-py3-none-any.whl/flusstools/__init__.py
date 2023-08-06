b432f3d91486554fa07532894282f129cafc7f34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:17:37 +0000

    Add package flusstools/1.0.7/flusstools-1.0.7-py3-none-any.whl

---
+++ packages/flusstools/flusstools-1.0.7-py3-none-any.whl/flusstools/__init__.py
@@ -0,0 +1,16 @@
+import sys
+import os
+sys.path.insert(0, os.path.dirname(__file__))
+
+try:
+    from helpers import *
+    import lidartools
+    import fuzzycorr
+    import geotools
+except ModuleNotFoundError:
+    print("Failed to initialize FlussTools - consider re-installation")
+
+try:
+    logging.getLogger()
+except NameError:
+    pass
