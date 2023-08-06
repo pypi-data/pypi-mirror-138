17db47e588b0e436d9fd18c6d9ba9029e938c15b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:08 +0000

    Add package preserves/0.13.0/preserves-0.13.0.tar.gz

---
+++ packages/preserves/preserves-0.13.0.tar.gz/preserves-0.13.0/preserves/__init__.py
@@ -0,0 +1,12 @@
+from .values import Float, Symbol, Record, ImmutableDict, Embedded, preserve
+from .values import Annotated, is_annotated, strip_annotations, annotate
+
+from .error import DecodeError, EncodeError, ShortPacket
+
+from .binary import Decoder, Encoder, decode, decode_with_annotations, encode, canonicalize
+from .text import Parser, Formatter, parse, parse_with_annotations, stringify
+
+from . import fold
+
+loads = parse
+dumps = stringify
