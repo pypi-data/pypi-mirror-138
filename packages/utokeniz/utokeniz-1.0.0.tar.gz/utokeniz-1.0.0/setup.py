22ed7fc7760626dc45041117c2f989da4decb9f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:43:14 +0000

    Add package utokeniz/1.0.0/utokeniz-1.0.0.tar.gz

---
+++ packages/utokeniz/utokeniz-1.0.0.tar.gz/utokeniz-1.0.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='utokeniz',
+    author='Jaedson Silva',
+    author_email='imunknowuser@protonmail.com',
+    description='Crie tokens de autenticação íntegros e seguros com UToken.',
+    version='1.0.0',
+    packages=['utoken'],
+    project_urls={
+        'Código fonte': 'https://github.com/jaedsonpys/utoken'
+    },
+    license='GNU GPL v3.0'
+)
---
+++ packages/utokeniz/utokeniz-1.0.0.tar.gz/utokeniz-1.0.0/utoken/__init__.py
@@ -0,0 +1,4 @@
+from .main import decode, encode
+from .exceptions import *
+
+__version__ = '2.0.0dev'
