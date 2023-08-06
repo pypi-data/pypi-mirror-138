c76e0f135970159ec498f83e220d800edc73c1ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:21:18 +0000

    Add package mlprodict/0.8.1697/mlprodict-0.8.1697-cp36-cp36m-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/mlprodict/mlprodict-0.8.1697-cp36-cp36m-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/mlprodict/sklapi/__init__.py
@@ -0,0 +1,12 @@
+# -*- encoding: utf-8 -*-
+"""
+@file
+@brief Shortcut to *sklapi*.
+"""
+from .onnx_pipeline import OnnxPipeline
+from .onnx_transformer import OnnxTransformer
+from .onnx_speed_up import (
+    OnnxSpeedupClassifier,
+    OnnxSpeedupCluster,
+    OnnxSpeedupRegressor,
+    OnnxSpeedupTransformer)
