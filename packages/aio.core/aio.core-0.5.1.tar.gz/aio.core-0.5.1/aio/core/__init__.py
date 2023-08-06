74ef357c0ace076b3d4d2ce904edb0064693a3fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:15:36 +0000

    Add package aio.core/0.5.1/aio.core-0.5.1.tar.gz

---
+++ packages/aio.core/aio.core-0.5.1.tar.gz/aio.core-0.5.1/aio/core/__init__.py
@@ -0,0 +1,17 @@
+
+from . import (
+    directory,
+    functional,
+    output,
+    stream,
+    subprocess,
+    tasks)
+
+
+__all__ = (
+    "directory",
+    "functional",
+    "output",
+    "stream",
+    "subprocess",
+    "tasks")
