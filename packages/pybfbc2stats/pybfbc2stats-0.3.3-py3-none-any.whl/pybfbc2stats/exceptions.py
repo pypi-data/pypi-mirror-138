32fac7acfc9b4379448f87af3e962663dd433e40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:03:43 +0000

    Add package pybfbc2stats/0.3.3/pybfbc2stats-0.3.3-py3-none-any.whl

---
+++ packages/pybfbc2stats/pybfbc2stats-0.3.3-py3-none-any.whl/pybfbc2stats/exceptions.py
@@ -0,0 +1,26 @@
+class PyBfbc2StatsError(Exception):
+    pass
+
+
+class PyBfbc2StatsConnectionError(PyBfbc2StatsError):
+    pass
+
+
+class PyBfbc2StatsTimeoutError(PyBfbc2StatsError):
+    pass
+
+
+class PyBfbc2StatsAuthError(PyBfbc2StatsError):
+    pass
+
+
+class PyBfbc2StatsParameterError(PyBfbc2StatsError):
+    pass
+
+
+class PyBfbc2StatsNotFoundError(PyBfbc2StatsError):
+    pass
+
+
+class PyBfbc2StatsSearchError(PyBfbc2StatsError):
+    pass
