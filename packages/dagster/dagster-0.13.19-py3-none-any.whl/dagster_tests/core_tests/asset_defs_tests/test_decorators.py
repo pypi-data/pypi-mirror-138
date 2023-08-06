58281c26a40a5f314d34393f67cab28f639f9be7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:13 +0000

    Add package dagster/0.13.19/dagster-0.13.19-py3-none-any.whl

---
+++ packages/dagster/dagster-0.13.19-py3-none-any.whl/dagster_tests/core_tests/asset_defs_tests/test_decorators.py
@@ -0,0 +1,176 @@
+import pytest
+from dagster import AssetKey, DagsterInvalidDefinitionError, Out, Output, String, check
+from dagster.core.asset_defs import AssetIn, AssetsDefinition, asset, multi_asset
+
+
+def test_asset_no_decorator_args():
+    @asset
+    def my_asset():
+        return 1
+
+    assert isinstance(my_asset, AssetsDefinition)
+    assert len(my_asset.op.output_defs) == 1
+    assert len(my_asset.op.input_defs) == 0
+
+
+def test_asset_with_inputs():
+    @asset
+    def my_asset(arg1):
+        return arg1
+
+    assert isinstance(my_asset, AssetsDefinition)
+    assert len(my_asset.op.output_defs) == 1
+    assert len(my_asset.op.input_defs) == 1
+    assert my_asset.op.input_defs[0].get_asset_key(None) == AssetKey("arg1")
+
+
+def test_asset_with_compute_kind():
+    @asset(compute_kind="sql")
+    def my_asset(arg1):
+        return arg1
+
+    assert my_asset.op.tags == {"kind": "sql"}
+
+
+def test_multi_asset_with_compute_kind():
+    @multi_asset(outs={"o1": Out(asset_key=AssetKey("o1"))}, compute_kind="sql")
+    def my_asset(arg1):
+        return arg1
+
+    assert my_asset.op.tags == {"kind": "sql"}
+
+
+def test_multi_asset_out_name_diff_from_asset_key():
+    @multi_asset(
+        outs={
+            "my_out_name": Out(asset_key=AssetKey("my_asset_name")),
+            "my_other_out_name": Out(asset_key=AssetKey("my_other_asset")),
+        }
+    )
+    def my_asset():
+        yield Output(1, "my_out_name")
+        yield Output(2, "my_other_out_name")
+
+    assert my_asset.asset_keys == {AssetKey("my_asset_name"), AssetKey("my_other_asset")}
+
+
+def test_multi_asset_infer_from_empty_asset_key():
+    @multi_asset(outs={"my_out_name": Out(), "my_other_out_name": Out()})
+    def my_asset():
+        yield Output(1, "my_out_name")
+        yield Output(2, "my_other_out_name")
+
+    assert my_asset.asset_keys == {AssetKey("my_out_name"), AssetKey("my_other_out_name")}
+
+
+def test_asset_with_dagster_type():
+    @asset(dagster_type=String)
+    def my_asset(arg1):
+        return arg1
+
+    assert my_asset.op.output_defs[0].dagster_type.display_name == "String"
+
+
+def test_asset_with_inputs_and_namespace():
+    @asset(namespace="my_namespace")
+    def my_asset(arg1):
+        return arg1
+
+    assert isinstance(my_asset, AssetsDefinition)
+    assert len(my_asset.op.output_defs) == 1
+    assert len(my_asset.op.input_defs) == 1
+    assert my_asset.op.input_defs[0].get_asset_key(None) == AssetKey(["my_namespace", "arg1"])
+
+
+def test_asset_with_context_arg():
+    @asset
+    def my_asset(context):
+        context.log("hello")
+
+    assert isinstance(my_asset, AssetsDefinition)
+    assert len(my_asset.op.input_defs) == 0
+
+
+def test_asset_with_context_arg_and_dep():
+    @asset
+    def my_asset(context, arg1):
+        context.log("hello")
+        assert arg1
+
+    assert isinstance(my_asset, AssetsDefinition)
+    assert len(my_asset.op.input_defs) == 1
+    assert my_asset.op.input_defs[0].get_asset_key(None) == AssetKey("arg1")
+
+
+def test_input_asset_key():
+    @asset(ins={"arg1": AssetIn(asset_key=AssetKey("foo"))})
+    def my_asset(arg1):
+        assert arg1
+
+    assert my_asset.op.input_defs[0].get_asset_key(None) == AssetKey("foo")
+
+
+def test_input_asset_key_and_namespace():
+    with pytest.raises(check.CheckError, match="key and namespace cannot both be set"):
+
+        @asset(ins={"arg1": AssetIn(asset_key=AssetKey("foo"), namespace="bar")})
+        def _my_asset(arg1):
+            assert arg1
+
+
+def test_input_namespace_str():
+    @asset(ins={"arg1": AssetIn(namespace="abc")})
+    def my_asset(arg1):
+        assert arg1
+
+    assert my_asset.op.input_defs[0].get_asset_key(None) == AssetKey(["abc", "arg1"])
+
+
+def test_input_namespace_list():
+    @asset(ins={"arg1": AssetIn(namespace=["abc", "xyz"])})
+    def my_asset(arg1):
+        assert arg1
+
+    assert my_asset.op.input_defs[0].get_asset_key(None) == AssetKey(["abc", "xyz", "arg1"])
+
+
+def test_input_metadata():
+    @asset(ins={"arg1": AssetIn(metadata={"abc": 123})})
+    def my_asset(arg1):
+        assert arg1
+
+    assert my_asset.op.input_defs[0].metadata == {"abc": 123}
+
+
+def test_unknown_in():
+    with pytest.raises(DagsterInvalidDefinitionError):
+
+        @asset(ins={"arg1": AssetIn()})
+        def _my_asset():
+            pass
+
+
+def test_all_fields():
+    @asset(
+        required_resource_keys={"abc", "123"},
+        io_manager_key="my_io_key",
+        description="some description",
+        metadata={"metakey": "metaval"},
+    )
+    def my_asset():
+        pass
+
+    assert my_asset.op.required_resource_keys == {"abc", "123"}
+    assert my_asset.op.description == "some description"
+    assert len(my_asset.op.output_defs) == 1
+    output_def = my_asset.op.output_defs[0]
+    assert output_def.io_manager_key == "my_io_key"
+    assert output_def.metadata["metakey"] == "metaval"
+
+
+def test_infer_input_dagster_type():
+    @asset
+    def my_asset(_input1: str):
+        pass
+
+    assert my_asset.op.input_defs[0].dagster_type.display_name == "String"
---
+++ packages/dagster/dagster-0.13.19-py3-none-any.whl/dagster_tests/core_tests/host_representation_tests/test_external_data.py
@@ -0,0 +1,323 @@
+from typing import Dict
+
+import pytest
+from dagster import AssetKey, DagsterInvariantViolationError, In, Out, job, pipeline
+from dagster.core.asset_defs import ForeignAsset
+from dagster.core.decorator_utils import get_function_params
+from dagster.core.definitions.decorators.op import _Op
+from dagster.core.host_representation.external_data import (
+    ExternalAssetDependedBy,
+    ExternalAssetDependency,
+    ExternalAssetNode,
+    ExternalSensorData,
+    ExternalTargetData,
+    external_asset_graph_from_defs,
+)
+from dagster.serdes import deserialize_json_to_dagster_namedtuple
+
+
+def asset(fn):
+    asset_name = fn.__name__
+
+    ins: Dict[str, In] = {}
+    for input_param in get_function_params(fn):
+        input_param_name = input_param.name
+        asset_key = AssetKey(input_param_name)
+        ins[input_param_name] = In(asset_key=asset_key)
+
+    out = Out(asset_key=AssetKey(asset_name))
+    return _Op(
+        name=asset_name,
+        ins=ins,
+        out=out,
+    )(fn)
+
+
+def test_single_asset_pipeline():
+    @asset
+    def asset1():
+        return 1
+
+    @pipeline
+    def my_graph():
+        asset1()
+
+    external_asset_nodes = external_asset_graph_from_defs([my_graph], foreign_assets_by_key={})
+
+    assert external_asset_nodes == [
+        ExternalAssetNode(
+            asset_key=AssetKey("asset1"),
+            dependencies=[],
+            depended_by=[],
+            op_name="asset1",
+            op_description=None,
+            job_names=["my_graph"],
+        )
+    ]
+
+
+def test_two_asset_pipeline():
+    @asset
+    def asset1():
+        return 1
+
+    @asset
+    def asset2(asset1):
+        assert asset1 == 1
+
+    @pipeline
+    def my_graph():
+        asset2(asset1())
+
+    external_asset_nodes = external_asset_graph_from_defs([my_graph], foreign_assets_by_key={})
+
+    assert external_asset_nodes == [
+        ExternalAssetNode(
+            asset_key=AssetKey("asset1"),
+            dependencies=[],
+            depended_by=[
+                ExternalAssetDependedBy(
+                    downstream_asset_key=AssetKey("asset2"), input_name="asset1"
+                )
+            ],
+            op_name="asset1",
+            op_description=None,
+            job_names=["my_graph"],
+        ),
+        ExternalAssetNode(
+            asset_key=AssetKey("asset2"),
+            dependencies=[
+                ExternalAssetDependency(upstream_asset_key=AssetKey("asset1"), input_name="asset1")
+            ],
+            depended_by=[],
+            op_name="asset2",
+            op_description=None,
+            job_names=["my_graph"],
+        ),
+    ]
+
+
+def test_two_downstream_assets_pipeline():
+    @asset
+    def asset1():
+        return 1
+
+    @asset
+    def asset2_a(asset1):
+        assert asset1 == 1
+
+    @asset
+    def asset2_b(asset1):
+        assert asset1 == 1
+
+    @pipeline
+    def my_graph():
+        r = asset1()
+        asset2_a(r)
+        asset2_b(r)
+
+    external_asset_nodes = external_asset_graph_from_defs([my_graph], foreign_assets_by_key={})
+
+    assert external_asset_nodes == [
+        ExternalAssetNode(
+            asset_key=AssetKey("asset1"),
+            dependencies=[],
+            depended_by=[
+                ExternalAssetDependedBy(
+                    downstream_asset_key=AssetKey("asset2_a"), input_name="asset1"
+                ),
+                ExternalAssetDependedBy(
+                    downstream_asset_key=AssetKey("asset2_b"), input_name="asset1"
+                ),
+            ],
+            op_name="asset1",
+            op_description=None,
+            job_names=["my_graph"],
+        ),
+        ExternalAssetNode(
+            asset_key=AssetKey("asset2_a"),
+            dependencies=[
+                ExternalAssetDependency(upstream_asset_key=AssetKey("asset1"), input_name="asset1")
+            ],
+            depended_by=[],
+            op_name="asset2_a",
+            op_description=None,
+            job_names=["my_graph"],
+        ),
+        ExternalAssetNode(
+            asset_key=AssetKey("asset2_b"),
+            dependencies=[
+                ExternalAssetDependency(upstream_asset_key=AssetKey("asset1"), input_name="asset1")
+            ],
+            depended_by=[],
+            op_name="asset2_b",
+            op_description=None,
+            job_names=["my_graph"],
+        ),
+    ]
+
+
+def test_cross_pipeline_asset_dependency():
+    @asset
+    def asset1():
+        return 1
+
+    @asset
+    def asset2(asset1):
+        assert asset1 == 1
+
+    @pipeline
+    def asset1_graph():
+        asset1()
+
+    @pipeline
+    def asset2_graph():
+        asset2()  # pylint: disable=no-value-for-parameter
+
+    external_asset_nodes = external_asset_graph_from_defs(
+        [asset1_graph, asset2_graph], foreign_assets_by_key={}
+    )
+
+    assert external_asset_nodes == [
+        ExternalAssetNode(
+            asset_key=AssetKey("asset1"),
+            dependencies=[],
+            depended_by=[
+                ExternalAssetDependedBy(
+                    downstream_asset_key=AssetKey("asset2"), input_name="asset1"
+                )
+            ],
+            op_name="asset1",
+            op_description=None,
+            job_names=["asset1_graph"],
+        ),
+        ExternalAssetNode(
+            asset_key=AssetKey("asset2"),
+            dependencies=[
+                ExternalAssetDependency(upstream_asset_key=AssetKey("asset1"), input_name="asset1")
+            ],
+            depended_by=[],
+            op_name="asset2",
+            op_description=None,
+            job_names=["asset2_graph"],
+        ),
+    ]
+
+
+def test_same_asset_in_multiple_pipelines():
+    @asset
+    def asset1():
+        return 1
+
+    @pipeline
+    def graph1():
+        asset1()
+
+    @pipeline
+    def graph2():
+        asset1()
+
+    external_asset_nodes = external_asset_graph_from_defs(
+        [graph1, graph2], foreign_assets_by_key={}
+    )
+
+    assert external_asset_nodes == [
+        ExternalAssetNode(
+            asset_key=AssetKey("asset1"),
+            dependencies=[],
+            depended_by=[],
+            op_name="asset1",
+            op_description=None,
+            job_names=["graph1", "graph2"],
+        ),
+    ]
+
+
+def test_unused_foreign_asset():
+    foo = ForeignAsset(key=AssetKey("foo"), description="abc")
+    bar = ForeignAsset(key=AssetKey("bar"), description="def")
+
+    external_asset_nodes = external_asset_graph_from_defs(
+        [], foreign_assets_by_key={AssetKey("foo"): foo, AssetKey("bar"): bar}
+    )
+    assert external_asset_nodes == [
+        ExternalAssetNode(
+            asset_key=AssetKey("foo"),
+            op_description="abc",
+            dependencies=[],
+            depended_by=[],
+            job_names=[],
+        ),
+        ExternalAssetNode(
+            asset_key=AssetKey("bar"),
+            op_description="def",
+            dependencies=[],
+            depended_by=[],
+            job_names=[],
+        ),
+    ]
+
+
+def test_used_foreign_asset():
+    bar = ForeignAsset(key=AssetKey("bar"), description="def")
+
+    @asset
+    def foo(bar):
+        assert bar
+
+    @job
+    def job1():
+        foo()  # pylint: disable=no-value-for-parameter
+
+    external_asset_nodes = external_asset_graph_from_defs(
+        [job1], foreign_assets_by_key={AssetKey("bar"): bar}
+    )
+    assert external_asset_nodes == [
+        ExternalAssetNode(
+            asset_key=AssetKey("bar"),
+            op_description="def",
+            dependencies=[],
+            depended_by=[
+                ExternalAssetDependedBy(downstream_asset_key=AssetKey(["foo"]), input_name="bar")
+            ],
+            job_names=[],
+        ),
+        ExternalAssetNode(
+            asset_key=AssetKey("foo"),
+            op_name="foo",
+            op_description=None,
+            dependencies=[
+                ExternalAssetDependency(upstream_asset_key=AssetKey(["bar"]), input_name="bar")
+            ],
+            depended_by=[],
+            job_names=["job1"],
+        ),
+    ]
+
+
+def test_foreign_asset_conflicts_with_asset():
+    bar_foreign_asset = ForeignAsset(key=AssetKey("bar"), description="def")
+
+    @asset
+    def bar():
+        pass
+
+    @job
+    def job1():
+        bar()  # pylint: disable=no-value-for-parameter
+
+    with pytest.raises(DagsterInvariantViolationError):
+        external_asset_graph_from_defs(
+            [job1], foreign_assets_by_key={AssetKey("bar"): bar_foreign_asset}
+        )
+
+
+def test_back_compat_external_sensor():
+    SERIALIZED_0_12_10_SENSOR = '{"__class__": "ExternalSensorData", "description": null, "min_interval": null, "mode": "default", "name": "my_sensor", "pipeline_name": "my_pipeline", "solid_selection": null}'
+    external_sensor_data = deserialize_json_to_dagster_namedtuple(SERIALIZED_0_12_10_SENSOR)
+    assert isinstance(external_sensor_data, ExternalSensorData)
+    assert len(external_sensor_data.target_dict) == 1
+    assert "my_pipeline" in external_sensor_data.target_dict
+    target = external_sensor_data.target_dict["my_pipeline"]
+    assert isinstance(target, ExternalTargetData)
+    assert target.pipeline_name == "my_pipeline"
