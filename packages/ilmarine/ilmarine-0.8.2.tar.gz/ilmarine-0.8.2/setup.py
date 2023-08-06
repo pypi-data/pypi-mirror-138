f9e4c0a36fb470ef9f00ad2f8cea8ebbdb697ec4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:26:10 +0000

    Add package ilmarine/0.8.2/ilmarine-0.8.2.tar.gz

---
+++ packages/ilmarine/ilmarine-0.8.2.tar.gz/ilmarine-0.8.2/setup.py
@@ -0,0 +1,29 @@
+import pathlib
+import sys
+from distutils.core import setup
+from distutils.command.install import install as _install
+
+class install(_install):
+    def run(self):
+        print("Wrong repo from public pypi")
+        sys.exit(1)
+        _install.run(self)
+
+HERE = pathlib.Path(__file__).parent
+
+setup(
+    cmdclass={'install': install},
+    name="ilmarine",
+    version="0.8.2",
+    description="ilmarine package for the internal pypi",
+    long_description="ilmarine package for the internal pypi",
+    long_description_content_type="text/markdown",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+    ],
+    packages=["ilmarine"],
+    include_package_data=True,
+)
