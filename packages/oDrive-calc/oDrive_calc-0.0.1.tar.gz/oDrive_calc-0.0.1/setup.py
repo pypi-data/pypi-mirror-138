db146dba0b4d8d289c0b0c06cac53ea71624272d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:40 +0000

    Add package oDrive-calc/0.0.1/oDrive_calc-0.0.1.tar.gz

---
+++ packages/oDrive-calc/oDrive_calc-0.0.1.tar.gz/oDrive_calc-0.0.1/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name="oDrive_calc",
+    version="0.0.1",
+    author="Mario Yprus",
+    description="bldc motor calibration tool",
+    packages=["oDrive_calc"]
+)
