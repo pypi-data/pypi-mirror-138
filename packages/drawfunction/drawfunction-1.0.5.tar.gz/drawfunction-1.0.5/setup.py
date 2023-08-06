3b6455960df4cc4639689b57b93b9a433bb0d4cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:21:14 +0000

    Add package drawfunction/1.0.5/drawfunction-1.0.5.tar.gz

---
+++ packages/drawfunction/drawfunction-1.0.5.tar.gz/drawfunction-1.0.5/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="drawfunction",
+    version="1.0.5",
+    packages=find_packages(),
+    url="https://github.com/andreyshspb/deep-python/tree/main/hw01",
+    author='andreyshspb',
+    author_email='andreyshspb@gmail.com',
+    install_requires=[
+        "networkx==2.6.3",
+        "matplotlib==3.5.1",
+        "pydot==1.4.2",
+    ],
+)
