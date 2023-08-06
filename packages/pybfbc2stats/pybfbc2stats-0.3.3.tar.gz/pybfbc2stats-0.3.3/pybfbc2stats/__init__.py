61230a00de83aecb270b4009c4a06b3d1b8823e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:03:43 +0000

    Add package pybfbc2stats/0.3.3/pybfbc2stats-0.3.3.tar.gz

---
+++ packages/pybfbc2stats/pybfbc2stats-0.3.3.tar.gz/pybfbc2stats-0.3.3/pybfbc2stats/__init__.py
@@ -0,0 +1,21 @@
+from .asyncio_client import AsyncFeslClient, AsyncTheaterClient
+from .asyncio_connection import AsyncConnection, AsyncSecureConnection
+from .client import FeslClient, TheaterClient
+from .connection import Connection, SecureConnection
+from .constants import Platform, Namespace, DEFAULT_LEADERBOARD_KEYS, STATS_KEYS
+from .exceptions import PyBfbc2StatsError, PyBfbc2StatsTimeoutError, PyBfbc2StatsParameterError, \
+    PyBfbc2StatsNotFoundError, PyBfbc2StatsAuthError, PyBfbc2StatsConnectionError, PyBfbc2StatsSearchError
+
+"""
+pybfbc2stats.
+Python library for retrieving statistics of Battlefield: Bad Company 2 players.
+"""
+
+__version__ = '0.3.3'
+__author__ = 'cetteup'
+__credits__ = 'nemo, Luigi Auriemma'
+__all__ = ['Connection', 'SecureConnection', 'FeslClient', 'TheaterClient',
+           'AsyncConnection', 'AsyncSecureConnection', 'AsyncFeslClient', 'AsyncTheaterClient',
+           'Platform', 'Namespace', 'DEFAULT_LEADERBOARD_KEYS', 'STATS_KEYS',
+           'PyBfbc2StatsError', 'PyBfbc2StatsConnectionError', 'PyBfbc2StatsParameterError', 'PyBfbc2StatsTimeoutError',
+           'PyBfbc2StatsAuthError', 'PyBfbc2StatsNotFoundError', 'PyBfbc2StatsSearchError']
