c4639e71431ea9794e61b3ee0a7a11a23dead3c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:43 +0000

    Add package matpack/1.0.2/matpack-1.0.2.tar.gz

---
+++ packages/matpack/matpack-1.0.2.tar.gz/matpack-1.0.2/dismat/matpack.py
@@ -0,0 +1,33 @@
+from numpy import zeros, math
+from sympy import Symbol, expand
+
+def progressiva(array,choice):
+	order= len(array)-1
+	value = zeros((order + 1, order + 2))
+	for i in range(0, order + 1):
+		value[0][i] = array[i]
+	for i in range(0, order):
+		for j in range(0, order):
+			if choice == "arithmetic":
+				value[i + 1][j] = value[i][j + 1] - value[i][j]
+			elif choice == "geometric":
+				value[i + 1][j] = value[i][j + 1] / value[i][j]
+	x = Symbol("x")
+	if choice=="arithmetic":
+		g = 1
+		f = 0
+		for i in range(1, order + 1):
+			g = g * (x - i)
+			f = f + (g * value[i][0]) / math.factorial(i)
+		f = f + value[0][0]
+	if choice=="geometric":
+		g = 1
+		f = 1
+		for i in range(1, order + 1):
+			g = g * (x - i)
+			f = f * (value[i][0] ** (g / math.factorial(i)))
+		f = f * value[0][0]
+	h = expand(f)
+	return h
+if __name__=='__main__':
+	print("installed")
