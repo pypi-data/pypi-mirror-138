02349e03902f3cfb0d26d259ec4cdd02aaecad74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:12:34 +0000

    Add package giftmaster/0.0.41/giftmaster-0.0.41-py3-none-any.whl

---
+++ packages/giftmaster/giftmaster-0.0.41-py3-none-any.whl/giftmaster/timestamp.py
@@ -0,0 +1,16 @@
+import random
+
+
+class TimeStampURLManager:
+    urls = [
+        "http://timestamp.comodoca.com/rfc3161",
+        "http://timestamp.digicert.com",
+        "http://timestamp.globalsign.com/scripts/timestamp.dll",
+        "http://timestamp.sectigo.com",
+        "http://tsa.starfieldtech.com",
+    ]
+
+    @property
+    def url(self):
+        cls = type(self)
+        return random.choice(cls.urls)
