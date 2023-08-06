b339a93fd56f9afe4cde7d30d202f2595d28897b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:27:47 +0000

    Add package ahelper/0.3/ahelper-0.3.tar.gz

---
+++ packages/ahelper/ahelper-0.3.tar.gz/ahelper-0.3/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='ahelper',
+    version=0.3,
+    description='A helper.',
+    python_requires='>=3.6',
+    license="MIT LICENSE",
+    author='Jintang Li',
+    author_email='cnljt@outlook.com',
+    url='https://gitee.com/EdisonLeeeee/ahelper',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        'Topic :: Scientific/Engineering',
+        'Topic :: Software Development :: Libraries',
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: Unix",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+)
