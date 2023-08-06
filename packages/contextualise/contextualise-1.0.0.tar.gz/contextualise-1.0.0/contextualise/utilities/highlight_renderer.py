8605ddc259f305c8a3a170a70fb1e00c9f980b28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:32:10 +0000

    Add package contextualise/1.0.0/contextualise-1.0.0.tar.gz

---
+++ packages/contextualise/contextualise-1.0.0.tar.gz/contextualise-1.0.0/contextualise/utilities/highlight_renderer.py
@@ -0,0 +1,16 @@
+import mistune
+from pygments import highlight
+from pygments.lexers import get_lexer_by_name
+from pygments.formatters import html
+
+
+class HighlightRenderer(mistune.HTMLRenderer):
+    def __init__(self, escape=True, allow_harmful_protocols=None):
+        super().__init__(escape, allow_harmful_protocols)
+
+    def block_code(self, code, lang=None):
+        if lang:
+            lexer = get_lexer_by_name(lang, stripall=True)
+            formatter = html.HtmlFormatter()
+            return highlight(code, lexer, formatter)
+        return "<pre><code>" + mistune.escape(code) + "</code></pre>"
