a5ff577463ee2c952a0b4987389902c83d367dea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:22:50 +0000

    Add package parmesan/1.0.1/parmesan-1.0.1.tar.gz

---
+++ packages/parmesan/parmesan-1.0.1.tar.gz/parmesan-1.0.1/parmesan/__init__.py
@@ -0,0 +1,19 @@
+# system modules
+try:
+    from parmesan.version import __version__
+except ModuleNotFoundError:
+    __version__ = "0.0.0"
+
+# internal modules
+import parmesan.bounds
+import parmesan.units
+import parmesan.processing
+import parmesan.accessor
+import parmesan.analysis
+import parmesan.aggregate
+import parmesan.gas
+import parmesan.wind
+import parmesan.stats
+import parmesan.vector
+
+# external modules
