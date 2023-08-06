c812bf8a7da5034ccbfe8f33115e1d99108ce8c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:27:11 +0000

    Add package my-stocks/0.1.1/my-stocks-0.1.1.tar.gz

---
+++ packages/my-stocks/my-stocks-0.1.1.tar.gz/my-stocks-0.1.1/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['my_stocks']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['matplotlib>=3.5.1,<4.0.0', 'typer>=0.4.0,<0.5.0', 'yfinance>=0.1.70,<0.2.0']
+
+entry_points = \
+{'console_scripts': ['my-stocks = my_stocks.main:app']}
+
+setup_kwargs = {
+    'name': 'my-stocks',
+    'version': '0.1.1',
+    'description': 'Easy way to access financial data in .csv format and make historical data plot',
+    'long_description': '',
+    'author': 'markus',
+    'author_email': 'datamastery87@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<3.10',
+}
+
+
+setup(**setup_kwargs)
