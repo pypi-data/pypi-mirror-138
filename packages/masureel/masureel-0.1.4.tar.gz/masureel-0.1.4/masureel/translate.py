61b470847536173c361c6b01121f7009aa0b1882
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:25:13 +0000

    Add package masureel/0.1.4/masureel-0.1.4.tar.gz

---
+++ packages/masureel/masureel-0.1.4.tar.gz/masureel-0.1.4/masureel/translate.py
@@ -0,0 +1,18 @@
+import csv
+import codecs
+
+def translate_file(filename):
+  Dict = []
+  with open('./data/Dict.csv') as f:
+      aList = csv.reader(f, delimiter='|')
+      for row in aList:
+        Dict.append(row)
+
+  with codecs.open(filename, 'r', encoding='utf-8', errors='ignore') as fdata:
+    filedata = fdata.read()
+
+  for i in range(len(Dict)):
+    filedata = filedata.replace(Dict[i][0], Dict[i][1])
+
+  with open(f'translated_{filename}', 'w', encoding='utf-8') as file:
+    file.write(filedata)
