435ea86e00265c40a68edd8af2b4ee26e13f227d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:52 +0000

    Add package Sqlcarve/0.0.1/Sqlcarve-0.0.1.tar.gz

---
+++ packages/Sqlcarve/Sqlcarve-0.0.1.tar.gz/Sqlcarve-0.0.1/src/sqlcarve/__init__.py
@@ -0,0 +1,12 @@
+"""
+
+SqlCarve Module
+---------------
+
+Ce module est la sclasse d'initialisation de la librairie
+"""
+
+
+from src.sqlcarve.validator.executeQuery import *
+from src.sqlcarve.validator.validator import *
+from src.sqlcarve.main import analyse
