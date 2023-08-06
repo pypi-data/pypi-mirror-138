653ec5bd2f7320bc65d2ebe303af71a223721418
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:11 +0000

    Add package django-symkey/0.0.1/django-symkey-0.0.1.tar.gz

---
+++ packages/django-symkey/django-symkey-0.0.1.tar.gz/django-symkey-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='django-symkey',
+    version='0.0.1',
+    description='Symmetric encryption based keychain for Django projects',
+    long_description='',
+    url='https://www.example.com',
+    author='Somenath Sinha',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
