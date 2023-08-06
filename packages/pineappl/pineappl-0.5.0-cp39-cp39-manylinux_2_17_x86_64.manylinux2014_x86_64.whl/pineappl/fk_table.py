5ffee5f2c4b1bde5d6935245a243aab88ee201e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:07 +0000

    Add package pineappl/0.5.0/pineappl-0.5.0-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/pineappl/pineappl-0.5.0-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/pineappl/fk_table.py
@@ -0,0 +1,35 @@
+from .pineappl import PyFkTable
+from .utils import PyWrapper
+
+
+class FkTable(PyWrapper):
+    """
+    Python wrapper object to interface :class:`~pineappl.pineappl.PyFkTable`.
+
+    Parameters
+    ----------
+        pyfktable : PyFkTable
+            raw wrapper object
+    """
+
+    def __init__(self, pyfktable):
+        self._raw = pyfktable
+
+    @classmethod
+    def read(cls, path):
+        """
+        Load an existing grid from file.
+
+        Convenience wrapper for :meth:`pineappl.pineappl.PyFkTable.read()`.
+
+        Parameters
+        ----------
+            path : pathlike
+                file path
+
+        Returns
+        -------
+            FkTable
+                grid object
+        """
+        return cls(PyFkTable.read(path))
