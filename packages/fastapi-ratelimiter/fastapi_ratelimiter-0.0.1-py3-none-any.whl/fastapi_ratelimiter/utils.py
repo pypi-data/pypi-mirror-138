68a2bce56b8947b005cd4222483c2f3840b685c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:28:57 +0000

    Add package fastapi-ratelimiter/0.0.1/fastapi_ratelimiter-0.0.1-py3-none-any.whl

---
+++ packages/fastapi-ratelimiter/fastapi_ratelimiter-0.0.1-py3-none-any.whl/fastapi_ratelimiter/utils.py
@@ -0,0 +1,10 @@
+from starlette.requests import Request
+
+
+def extract_ip_from_request(request: Request) -> str:
+    forwarded_for = request.headers.get("X-Forwarded-For")
+    if forwarded_for:
+        ip = forwarded_for.split(",")[0]
+    else:
+        ip = request.client.host
+    return ip
