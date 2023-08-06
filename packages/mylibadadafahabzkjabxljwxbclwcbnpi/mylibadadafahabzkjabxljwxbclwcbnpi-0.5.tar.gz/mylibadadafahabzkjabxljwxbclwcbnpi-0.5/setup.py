794bf286d9d7114abfeb43b03c090c37ed3d019c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:50 +0000

    Add package mylibadadafahabzkjabxljwxbclwcbnpi/0.5/mylibadadafahabzkjabxljwxbclwcbnpi-0.5.tar.gz

---
+++ packages/mylibadadafahabzkjabxljwxbclwcbnpi/mylibadadafahabzkjabxljwxbclwcbnpi-0.5.tar.gz/mylibadadafahabzkjabxljwxbclwcbnpi-0.5/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='mylibadadafahabzkjabxljwxbclwcbnpi',
+    version='0.05',
+    description='hw_01',
+    packages=['mylibadadafahabzkjabxljwxbclwcbnpi'],
+    install_requires=[
+        'python_version >= "3.6"',
+    ],
+)
