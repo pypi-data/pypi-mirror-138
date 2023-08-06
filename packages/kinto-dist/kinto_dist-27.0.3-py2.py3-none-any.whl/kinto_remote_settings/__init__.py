dce4f1e96282bb49bce4ac11cfa5cc69b83a8128
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:35 +0000

    Add package kinto-dist/27.0.3/kinto_dist-27.0.3-py2.py3-none-any.whl

---
+++ packages/kinto-dist/kinto_dist-27.0.3-py2.py3-none-any.whl/kinto_remote_settings/__init__.py
@@ -0,0 +1,9 @@
+# TODO: This hardcoded version is a temporary fix to provide a version proprety
+# as server metadata. This value will eventually be replaced by a version
+# provided when we install `kinto_remote_settings` as a package.
+__version__ = "27.0.3"
+
+
+def includeme(config):
+    config.include("kinto_remote_settings.changes")
+    config.include("kinto_remote_settings.signer")
