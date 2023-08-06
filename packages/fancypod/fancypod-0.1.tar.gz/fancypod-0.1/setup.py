372208f921c6d5cf2067d4429e6126e9040444f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:31:40 +0000

    Add package fancypod/0.1/fancypod-0.1.tar.gz

---
+++ packages/fancypod/fancypod-0.1.tar.gz/fancypod-0.1/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+     name='fancypod',  
+     version='0.1',
+     scripts=['fancypod'] ,
+     author="fwang",
+     author_email="fwang@mowentech.com",
+     description="Place holder!",
+     long_description="Place Holder!",
+     url="https://fancypod.com",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Development Status :: 1 - Planning",
+     ],
+ )
