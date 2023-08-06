b85d693c0ffb73a8f9e184bf38c0f32c2cbb9dc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:36 +0000

    Add package Mixiis-Calculator/1.0.0/Mixiis%20Calculator-1.0.0.tar.gz

---
+++ packages/Mixiis-Calculator/Mixiis%20Calculator-1.0.0.tar.gz/Mixiis Calculator-1.0.0/Calculator/Calculator.py
@@ -0,0 +1,7 @@
+def calculator():
+    x = int(input('first number: '))
+    y = int(input('second number: '))
+    return x + y
+
+
+print(calculator())
---
+++ packages/Mixiis-Calculator/Mixiis%20Calculator-1.0.0.tar.gz/Mixiis Calculator-1.0.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Mixiis Calculator',
+    version='1.0.0',
+    description='Makes Standard Calculations',
+    author='Mikk Narusberg',
+    author_email='mikk.narusberg@gmail.com',
+    url='https://github.com/Mixiir/Calculator',
+    license='MIT',
+    platforms='Python',
+    packages=find_packages()  # Installs all the requirements
+)
