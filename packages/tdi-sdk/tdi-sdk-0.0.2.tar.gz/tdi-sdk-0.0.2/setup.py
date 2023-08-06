0aa4750c97545e704ba1340c436da7b6a957f225
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:29:52 +0000

    Add package tdi-sdk/0.0.2/tdi-sdk-0.0.2.tar.gz

---
+++ packages/tdi-sdk/tdi-sdk-0.0.2.tar.gz/tdi-sdk-0.0.2/setup.py
@@ -0,0 +1,26 @@
+﻿import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="tdi-sdk",
+    version="0.0.2",
+    author="Denis Germano",
+    author_email="dlcgermano@gmai.com",
+    description="Simple SDK to use TDI services",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/denisgermano/tdi_sdk",
+    project_urls={
+        "Bug Tracker": "https://github.com/denisgermano/tdi_sdk/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.7",
+)
