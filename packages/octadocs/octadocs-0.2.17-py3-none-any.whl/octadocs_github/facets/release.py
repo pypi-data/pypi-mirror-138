e362ce7fcd00fe1735d924cf33acd1683801cf5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:56 +0000

    Add package octadocs/0.2.17/octadocs-0.2.17-py3-none-any.whl

---
+++ packages/octadocs/octadocs-0.2.17-py3-none-any.whl/octadocs_github/facets/release.py
@@ -0,0 +1,43 @@
+from datetime import date, datetime
+
+from dominate.tags import code, a, span, br
+from more_itertools import first
+
+from iolanta.facet import Facet
+
+
+class Release(Facet):
+    """Render a software project release."""
+
+    sparql = '''
+    SELECT * WHERE {
+        $release
+            gh:published_at ?date ;
+            gh:name ?name ;
+            gh:prerelease ?prerelease .
+    }
+    '''
+
+    def render(self):
+        """Draw release description."""
+        rows = self.query(
+            self.sparql,
+            release=self.iri,
+        )
+
+        row = first(rows)
+
+        date_value = row['date'].value
+
+        if isinstance(date_value, datetime):
+            date_value = date_value.date()
+
+        return span(
+            code(str(date_value)),
+            br(),
+            a(
+                row['name'],
+                href=row['release'],
+            ),
+            cls='octadocs-github-release',
+        )
