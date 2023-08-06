5d54a7091fb7016d2bbab152fe924649aa7c34eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:17:08 +0000

    Add package packitos/0.46.0/packitos-0.46.0.tar.gz

---
+++ packages/packitos/packitos-0.46.0.tar.gz/packitos-0.46.0/packit/utils/source_script.py
@@ -0,0 +1,28 @@
+from typing import Optional
+
+from packit.config import JobConfig
+from packit.constants import COPR_SOURCE_SCRIPT
+from packit.schema import JobConfigSchema
+
+
+def create_source_script(
+    url: str,
+    ref: Optional[str] = None,
+    pr_id: Optional[str] = None,
+    merge_pr: Optional[bool] = True,
+    target_branch: Optional[str] = None,
+    job_config: Optional[JobConfig] = None,
+):
+    options = []
+    if ref:
+        options += ["--ref", ref]
+    if pr_id:
+        options += ["--pr-id", pr_id, f"--{'no-' if not merge_pr else ''}merge-pr"]
+        if merge_pr and target_branch:
+            options += ["--target-branch", target_branch]
+    if job_config:
+        job_config = JobConfigSchema().dumps(job_config)
+        options += ["--job-config", f"{job_config!r}"]
+
+    options += [url]
+    return COPR_SOURCE_SCRIPT.format(options=" ".join(options))
