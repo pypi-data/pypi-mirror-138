3f64dddf6117f7bced838a3e2eb1acad8827202d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:46:06 +0000

    Add package asttodotbuilder/1.1/asttodotbuilder-1.1.tar.gz

---
+++ packages/asttodotbuilder/asttodotbuilder-1.1.tar.gz/asttodotbuilder-1.1/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name="asttodotbuilder",
+    version="1.1",
+    author="KarinaAnisimova",
+    author_email="anisimova5005@gmail.com",
+    description="Advanced python homework",
+    packages=['asttodotbuilder'],
+    url="https://github.com/Karina5005/Python_HW2022/tree/master/hw01",
+    install_requires=["networkx", "pydot"]
+)
