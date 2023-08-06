43f148f9c9e77b1d8ed9c6d56db4dd9f5ec9b830
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:26:50 +0000

    Add package ods-tools/2.1.1/ods_tools-2.1.1.tar.gz

---
+++ packages/ods-tools/ods_tools-2.1.1.tar.gz/ods_tools-2.1.1/setup.py
@@ -0,0 +1,47 @@
+import io
+import os
+import re
+import setuptools
+
+
+SCRIPT_DIR = os.path.abspath(os.path.dirname(__file__))
+
+
+def get_readme():
+    with io.open(os.path.join(SCRIPT_DIR, 'src', 'README.md'), encoding='utf-8') as readme:
+        return readme.read()
+
+
+def get_version():
+    """
+    Return package version as listed in `__version__` in `init.py`.
+    """
+    with io.open(os.path.join(SCRIPT_DIR, 'src', '__init__.py'), encoding='utf-8') as init_py:
+        return re.search('__version__ = [\'"]([^\'"]+)[\'"]', init_py.read()).group(1)
+
+
+version = get_version()
+readme = get_readme()
+
+setuptools.setup(
+    name="ods_tools",
+    version=version,
+    include_package_data=True,
+    package_data={
+        'src': ['OpenExposureData_Spec.csv'],
+    },
+    entry_points={
+        'console_scripts': [
+            'ods_tools=ods_tools:main',
+        ]
+    },
+    author='Oasis LMF',
+    author_email="support@oasislmf.org",
+    packages=['ods_tools', 'OpenExposureData/Docs'],
+    package_dir={'ods_tools': 'src', 'OpenExposureData/Docs': 'OpenExposureData/Docs'},
+    python_requires='>=3.7',
+    description='Tools to manage ODS files',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    url='https://github.com/OasisLMF/OpenDataStandards',
+)
---
+++ packages/ods-tools/ods_tools-2.1.1.tar.gz/ods_tools-2.1.1/src/__init__.py
@@ -0,0 +1,9 @@
+__version__ = "2.1.1"
+from .oed import *
+
+import logging
+
+logger = logging.getLogger(__name__)
+logger.addHandler(logging.NullHandler())
+
+
