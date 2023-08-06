9bbd717e9776506fb7d284cb8ce4360bddac01c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:17:25 +0000

    Add package dzdsu/1.3.9/dzdsu-1.3.9-py3-none-any.whl

---
+++ packages/dzdsu/dzdsu-1.3.9-py3-none-any.whl/dzdsu/wrapper.py
@@ -0,0 +1,57 @@
+"""Server wrapper."""
+
+from argparse import ArgumentParser, Namespace
+from logging import INFO, WARNING, basicConfig, getLogger
+from pathlib import Path
+from subprocess import Popen
+from time import sleep
+
+from dzdsu.constants import JSON_FILE, XVFB
+from dzdsu.server import load_servers
+
+
+__all__ = ['main']
+
+
+LOGGER = getLogger('dzdsw')
+
+
+def get_args(description: str = __doc__) -> Namespace:
+    """Return the parsed command line arguments."""
+
+    parser = ArgumentParser(description=description)
+    parser.add_argument(
+        'server', default=Path.cwd().name,
+        help='the name of the server to start'
+    )
+    parser.add_argument(
+        '-f', '--servers-file', type=Path, default=JSON_FILE, metavar='file',
+        help='servers JSON file path'
+    )
+    parser.add_argument(
+        '-v', '--verbose', action='store_true', help='verbose logging'
+    )
+    return parser.parse_args()
+
+
+def main() -> int:
+    """Starts the DayZ server."""
+
+    args = get_args()
+    basicConfig(level=INFO if args.verbose else WARNING)
+    servers = load_servers(args.servers_file)
+    env = None
+
+    try:
+        server = servers[args.server]
+    except KeyError:
+        LOGGER.error('No such server: %s', args.server)
+        return 2
+
+    if server.wine:
+        Popen(XVFB, cwd=server.base_dir)
+        env = {'DISPLAY': ':0'}
+        sleep(3)    # Give Xvfb some time to start up.
+
+    Popen(server.command, cwd=server.base_dir, env=env)
+    return 0
