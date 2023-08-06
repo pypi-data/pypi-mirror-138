7ade57378be0d64aa54ece2e533241045f96060e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:21 +0000

    Add package lotto-game/0.1.2/lotto_game-0.1.2.tar.gz

---
+++ packages/lotto-game/lotto_game-0.1.2.tar.gz/lotto_game-0.1.2/lotto_game/lotto.py
@@ -0,0 +1,72 @@
+from random import randrange
+
+
+def get_numbers(threshold, range_to):
+    numbers = []
+    while threshold != 0:
+        get_number = randrange(1, range_to + 1)
+        if get_number not in numbers or len(numbers) == 0:
+            numbers.append(get_number)
+            threshold -= 1
+    return set(sorted(numbers))
+
+
+def try_match(random_choice, my_input_sorted):
+
+    if random_choice == my_input_sorted:
+        is_not_success = False
+    else:
+        is_not_success = True
+
+    return is_not_success
+
+
+def get_parially_results(partially_results, random_choice, my_input_sorted):
+
+    result = len(random_choice & my_input_sorted)
+
+    if result not in partially_results:
+        partially_results[result] = 1
+    else:
+        partially_results[result] = partially_results[result] + 1
+
+    return partially_results
+
+
+def try_lotto(input_value):
+    range_to = 49
+    my_input = input_value.split(",")
+    my_input = [
+        int(number)
+        for number in my_input
+        if int(number) > 0 and int(number) <= range_to
+    ]
+
+    is_not_success = True
+    attempt = 0
+    my_input_sorted = set(sorted(my_input))
+    len_my_input = len(my_input_sorted)
+    partially_results = dict()
+
+    while is_not_success:
+        random_choice = get_numbers(len_my_input, range_to)
+        is_not_success = try_match(random_choice, my_input_sorted)
+        partially_results = get_parially_results(
+            partially_results, random_choice, my_input_sorted
+        )
+
+        attempt += 1
+        if attempt % 1000000 == 0:
+            print(
+                f'--- number of attempts: {int(attempt/1000000)} {"millions" if attempt/1000000 >1 else "million "}  --- random choice: {random_choice}--- my_choice: {my_input_sorted} ---'
+            )
+    print(
+        f"--- number of attempts: {attempt}  --- random choice: {random_choice} --- my_choice: {sorted(my_input)} ---"
+    )
+    print(
+        f"number of attempts: {attempt}. Detailed information about partially matched before you win {partially_results}"
+    )
+
+
+if __name__ == "__main__":
+    try_lotto("1,2,3,4,5,6")
---
+++ packages/lotto-game/lotto_game-0.1.2.tar.gz/lotto_game-0.1.2/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['lotto_game']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'lotto-game',
+    'version': '0.1.2',
+    'description': '',
+    'long_description': None,
+    'author': 'czasoprzestrzenny',
+    'author_email': 'jaroslawold@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/czasoprzestrzenny/lotto_game',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
