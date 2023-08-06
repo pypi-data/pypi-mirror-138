5870d325f68c59ada50f51a0756f605326ba8281
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:25:02 +0000

    Add package fedhf/0.2.0/fedhf-0.2.0.tar.gz

---
+++ packages/fedhf/fedhf-0.2.0.tar.gz/fedhf-0.2.0/fedhf/component/__init__.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+""" 
+@File    :   fedhf\component\__init__.py 
+@Time    :   2021-11-15 18:25:24 
+@Author  :   Bingjie Yan 
+@Email   :   bj.yan.pa@qq.com 
+@License :   Apache License 2.0 
+"""
+
+from .trainer import build_trainer, Trainer, FedAsyncTrainer, trainer_factory
+
+from .evaluator import build_evalutor, Evaluator, evaluator_factory
+
+from .aggregator import build_aggregator, FedAsyncAggregator, FedAvgAggregator, SyncAggregator, AsyncAggregator, aggregator_factory
+
+from .selector import build_selector, RandomFedAsyncSelector, RandomSelector, selector_factory
+
+from .sampler import build_sampler, RandomSampler, NonIIDSampler, sampler_factory
