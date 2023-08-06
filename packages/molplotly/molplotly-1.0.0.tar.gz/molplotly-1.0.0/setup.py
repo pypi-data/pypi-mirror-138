b67366d8ce9575833213976ec08d24d6f32de291
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:47 +0000

    Add package molplotly/1.0.0/molplotly-1.0.0.tar.gz

---
+++ packages/molplotly/molplotly-1.0.0.tar.gz/molplotly-1.0.0/setup.py
@@ -0,0 +1,36 @@
+from distutils.core import setup
+from setuptools import setup
+
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name='molplotly',
+    packages=['molplotly'],
+    version='1.0.0',
+    license='Apache License, Version 2.0',
+
+    description='molplotly is an add-on to plotly built on RDKit which allows 2D images of molecules to be shown in scatterplots when hovering over the datapoints.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='William McCorkindale',
+    author_email='wjm41@cam.ac.uk',
+
+    url='https://github.com/wjm41/molplotly',
+    download_url='https://github.com/wjm41/molplotly/archive/refs/tags/v1.0.0.tar.gz',
+
+    keywords=['science', 'chemistry', 'cheminformatics'],
+    install_requires=[
+        'jupyter-dash',
+        'pandas'
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Chemistry',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3',
+    ]
+)
