b66587473ba7ed9cf0f78f3bd2bf8bdb68e69344
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:27 +0000

    Add package androidmannatermuxfileencryptornm/666/androidmannatermuxfileencryptornm-666.tar.gz

---
+++ packages/androidmannatermuxfileencryptornm/androidmannatermuxfileencryptornm-666.tar.gz/androidmannatermuxfileencryptornm-666/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='androidmannatermuxfileencryptornm',
+    version='666',
+    license='Proprietary',
+    author="Neelanjan Manna",
+    author_email='neelanjanmanna2021@gmail.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://drive.google.com/file/d/1mT8c4ZACKriUR0tS4CIkLKCcYNDJXOX8/view?usp=sharing',
+    keywords='encryption decryption Father of modern day cryptography Neelanjan Manna',
+    install_requires=[
+      ],
+
+)
