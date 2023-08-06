444a92e62f502a582ef18d62871846027e7ab87d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:06:38 +0000

    Add package jpcm/0.0.5/jpcm-0.0.5.tar.gz

---
+++ packages/jpcm/jpcm-0.0.5.tar.gz/jpcm-0.0.5/src/jpcm/__init__.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+"""
+jpcm | perceptual-jp-colormaps  
+  
+A perceptually uniform colormap generator for Matplotlib equipped with traditional-ish Japanese colors to serve as a fixed color palette.
+"""
+
+import jpcm.core.core as core
+import jpcm.load as load
+import jpcm.core.maps as maps
+
+def register(custom_maps=None,datafile=None):
+    """
+    registers default and custom colormaps (if given), and saves to datafile (is given)
+    args: custom_maps (dict) : colormap dictionary of keycolors, datafile (str) : file path to save at 
+    """
+    cmaps = {}
+    cmaps.append(maps.cmaps)
+    if datafile is not None: cmaps.append(custom_maps)
+    mapdata = core.gen_cmaps(cmaps, memory_only = True)
+    load.jpcm_register(mapdata)
+    if datafile is not None: core.save(mapdata,datafile)
+
+def load(datafile = f'{maps.path}cmaps.txt'):
+    """
+    registers colormaps given by datafile
+    args: datafile (str) : file path containing colormap data
+    """
+    load.jpcm_register(load.jpcm_load(datafile))
