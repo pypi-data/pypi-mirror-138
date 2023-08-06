a7a14947262552427fe60d6ab97d1957cdabe30c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:46 +0000

    Add package humlab-penelope/0.6.10/humlab-penelope-0.6.10.tar.gz

---
+++ packages/humlab-penelope/humlab-penelope-0.6.10.tar.gz/humlab-penelope-0.6.10/penelope/vendor/gensim_api/_gensim/__init__.py
@@ -0,0 +1,9 @@
+# type: ignore
+
+from .ext_text_corpus import ExtTextCorpus, SimpleExtTextCorpus
+from .utils import (
+    from_id2token_to_dictionary,
+    from_stream_of_tokens_to_dictionary,
+    from_stream_of_tokens_to_sparse2corpus,
+    from_token2id_to_dictionary,
+)
