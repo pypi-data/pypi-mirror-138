d16497e2c22a94bece5d2ad1f1a414813a3c10fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:25 +0000

    Add package mypack-Dotty/2.0.0/mypack_Dotty-2.0.0-py3-none-any.whl

---
+++ packages/mypack-Dotty/mypack_Dotty-2.0.0-py3-none-any.whl/mypack/__init__.py
@@ -0,0 +1,1 @@
+__all__ = ["funcs.clear", "funcs.depickler", "funcs.file_lister", "funcs.intput", "funcs.intput", "funcs.picklify", "funcs.strintput", "funcs.json_classreader", "funcs.json_classwriter"]
