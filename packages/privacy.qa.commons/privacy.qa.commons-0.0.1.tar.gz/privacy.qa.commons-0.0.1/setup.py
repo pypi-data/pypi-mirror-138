9288e102ae4e8afdd7a427985e71674512419b2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:30:20 +0000

    Add package privacy.qa.commons/0.0.1/privacy.qa.commons-0.0.1.tar.gz

---
+++ packages/privacy.qa.commons/privacy.qa.commons-0.0.1.tar.gz/privacy.qa.commons-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='privacy.qa.commons',
+    description='Testing library',
+    author='Teal team',
+    version='0.0.1',
+    packages=['privacy.qa.commons'],
+    classifiers=['Programming Language :: Python :: 3'],
+)
