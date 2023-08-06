2ec837ee4fbe9f0f00bdaf2c9a49d372144bf18d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:51 +0000

    Add package metaindexmanager/0.5.0/metaindexmanager-0.5.0.tar.gz

---
+++ packages/metaindexmanager/metaindexmanager-0.5.0.tar.gz/metaindexmanager-0.5.0/metaindexmanager/shared.py
@@ -0,0 +1,11 @@
+"""Shared definitions"""
+
+PROGRAMNAME = 'metaindexmanager'
+
+ANY_SCOPE = 'any'
+ALL_SCOPE = 'all'
+
+PREF_BORDER = 'border'
+DEFAULT_BORDER = 'full'
+BORDER_FULL = 'full'
+BORDER_MINIMAL = 'minimal'
