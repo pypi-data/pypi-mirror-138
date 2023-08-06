5410b8e8f7774c3831ee0d3db2b766d9b5c44364
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:25:38 +0000

    Add package bmkgdashboard/0.8/bmkgdashboard-0.8.tar.gz

---
+++ packages/bmkgdashboard/bmkgdashboard-0.8.tar.gz/bmkgdashboard-0.8/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="bmkgdashboard",
+    version="0.8",
+    author="Sencho Parameswara",
+    author_email="senchoparameswara@gmail.com",
+    description="this package is src for BMKG indonesian earthquake live and last articlee",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sencho-masteringpy/bmkg-dashboard",
+    project_urls={
+        "MyWeb": "https://senchooo.com",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
+        "Operating System :: OS Independent",
+        "Development Status :: 4 - Beta"
+    ],
+    packages=["bmkgdashboard"],
+    python_requires=">=3.6"
+)
