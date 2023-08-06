10024d9cd1f7ec2b8ef46b84189524c0f541b4fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:24 +0000

    Add package npyx/2.2.0/npyx-2.2.0.tar.gz

---
+++ packages/npyx/npyx-2.2.0.tar.gz/npyx-2.2.0/npyx/__init__.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+
+from . import utils, io, gl, spk_t, spk_wvf, corr, plot, behav, merger, stats
+
+from .utils import *
+from .io import *
+from .gl import *
+from .spk_t import *
+from .spk_wvf import *
+from .corr import *
+from .stats import *
+from .plot import *
+from .behav import *
+from .merger import *
+from .circuitProphyler import *
+
+__doc__="""
+
+npyx submodules:
+    npyx.utils
+
+    npyx.io
+
+    npyx.gl
+
+    npyx.spk_t
+
+    npyx.spk_wvf
+
+    npyx.corr
+
+    npyx.plot
+
+    npyx.ml
+
+    npyx.behav
+
+    npyx.stats
+"""
+
+__version__ = '2.2.0'
+
+print(f'npyx version {__version__} imported.')
