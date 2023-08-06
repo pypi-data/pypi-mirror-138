a2252e8ff310c116b07f00cb23bf544b20441996
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:41 +0000

    Add package selsearch/0.1.3/selsearch-0.1.3.tar.gz

---
+++ packages/selsearch/selsearch-0.1.3.tar.gz/selsearch-0.1.3/selsearch/selection.py
@@ -0,0 +1,32 @@
+import os
+import shutil
+import time
+
+import pyperclip
+from pynput.keyboard import Controller, Key
+
+keyboard = Controller()
+
+
+def get_selected_text_xsel():
+    return os.popen("xsel").read()
+
+
+def get_selected_text_alt():
+    clipboard = pyperclip.paste()
+
+    keyboard.press(Key.ctrl)
+    keyboard.press("c")
+    time.sleep(0.1)
+    keyboard.release("c")
+    keyboard.release(Key.ctrl)
+    text = pyperclip.paste()
+
+    pyperclip.copy(clipboard)
+    return text
+
+
+if shutil.which("xsel"):
+    get_selected_text = get_selected_text_xsel
+else:
+    get_selected_text = get_selected_text_alt
