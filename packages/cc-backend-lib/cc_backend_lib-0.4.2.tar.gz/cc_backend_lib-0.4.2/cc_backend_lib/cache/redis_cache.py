a7ccda903a0d734ec7f8295a203ac3075d0c2391
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:15:48 +0000

    Add package cc-backend-lib/0.4.2/cc_backend_lib-0.4.2.tar.gz

---
+++ packages/cc-backend-lib/cc_backend_lib-0.4.2.tar.gz/cc_backend_lib-0.4.2/cc_backend_lib/cache/redis_cache.py
@@ -0,0 +1,25 @@
+
+from typing import Optional
+import redis
+from . import base_cache
+
+class RedisCache(base_cache.BaseCache[str]):
+    def __init__(self,
+            name: str,
+            host: str,
+            expiry_time: Optional[int] = 10,
+            port: int = 6379,
+            db: int = 0):
+
+        self._redis = redis.Redis(host = host, port = port, db = db)
+        self._expiry_time = expiry_time
+        self._name = name
+
+    def get(self, key: str) -> str:
+        return self._redis.get(self._key(key)).decode()
+
+    def set(self, key: str, val: str) ->  None:
+        self._redis.set(self._key(key), val, ex = self._expiry_time)
+
+    def _key(self, key: int):
+        return self._name + "/" + str(key)
