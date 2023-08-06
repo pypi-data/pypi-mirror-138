45960d8784aa0b0dcbb114471ca3339b2755177a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:15 +0000

    Add package plum-tools/0.2.4/plum_tools-0.2.4.tar.gz

---
+++ packages/plum-tools/plum_tools-0.2.4.tar.gz/plum_tools-0.2.4/plum_tools/__init__.py
@@ -0,0 +1,13 @@
+"""
+#=============================================================================
+#  ProjectName: plum_tools
+#     FileName: __init__.py
+#       Author: seekplum
+#        Email: 1131909224m@sina.cn
+#     HomePage: seekplum.github.io
+#       Create: 2019-04-04 22:01
+#=============================================================================
+"""
+__import__("pkg_resources").declare_namespace(__name__)
+
+VERSION = "0.2.4"
