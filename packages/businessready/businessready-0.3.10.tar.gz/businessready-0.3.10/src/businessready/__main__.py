a47b25d866568e27f681252551804872d10e12eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:32 +0000

    Add package businessready/0.3.10/businessready-0.3.10.tar.gz

---
+++ packages/businessready/businessready-0.3.10.tar.gz/businessready-0.3.10/src/businessready/__main__.py
@@ -0,0 +1,10 @@
+# import sys
+# from .brd import IOS_learned_acl,IOS_learned_arp,IOS_learned_interface,IOS_learned_ospf,IOS_learned_routing,IOS_show_ip_interface_brief,DNAC_Sites
+# if __name__ == "__main__":
+#     print(IOS_learned_acl(sys.argv[4]))
+#     print(IOS_learned_arp(sys.argv[4]))
+#     print(IOS_learned_interface(sys.argv[4]))
+#     print(IOS_learned_ospf(sys.argv[4]))
+#     print(IOS_learned_routing(sys.argv[4]))
+#     print(IOS_show_ip_interface_brief(sys.argv[4]))
+#     print(DNAC_Sites(sys.argv[2]))
