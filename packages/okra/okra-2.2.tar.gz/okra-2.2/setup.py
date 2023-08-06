e7640317d6c15fa79015767878c120d1a7b02cd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:18:01 +0000

    Add package okra/2.2/okra-2.2.tar.gz

---
+++ packages/okra/okra-2.2.tar.gz/okra-2.2/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='okra',
+    version='2.2',
+    packages=["okra", "okra/proto"],
+    license='MIT',
+    author="Tyler Brown",
+    author_email="tylers.pile@gmail.com",
+    long_description_content_type="text/markdown",
+    long_description=open('README.md').read(),
+    python_requires="==3.7.*",
+    install_requires=[
+        'cython',
+        'click == 7.0',
+        'numpy==1.21',
+        'pandas == 1.0.1',
+        'protobuf == 3.19',
+        'sqlalchemy == 1.3.13',
+    ],
+    zip_safe=False,
+    url='https://okrahealth.github.io/',
+    project_urls={
+        "Bug Tracker": "https://github.com/okrahealth/okra/issues",
+        "Documentation": "https://okrahealth.github.io/okra/",
+        "Source Code": "https://github.com/okrahealth/okra",
+    },
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.7',
+    ],
+    entry_points={
+        'console_scripts': ['okra = okra.cli:cli'],
+    }
+)
