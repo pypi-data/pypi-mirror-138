257e4fda54aed6e7a70aa22f59a41b04faedc1d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:55 +0000

    Add package lindworm-language/0.1.0/lindworm-language-0.1.0.tar.gz

---
+++ packages/lindworm-language/lindworm-language-0.1.0.tar.gz/lindworm-language-0.1.0/README.md
@@ -0,0 +1,107 @@
+# Lindworm
+
+Lindworm is my personal extension of Python, inspired by Coconut and functional programming.  Like Coconut, all valid Python code is also Lindworm code. The package comes with a compiler, Sigurd, which transpiles it into human-readable Python code or `.pyc` code. Lindworm is suitable for writing extensions or packages which make heavy use of functional programming.
+
+## Installation
+
+Lindworm is on PyPi, and can be installed with pip from the command line with the following command:
+
+    pip install lindworm-language
+
+
+## Examples
+
+Lindworm comes with several new operators, as well as ways to combine them. , so it can be easily switched between. It has a high compilation speed and remains both short and relatively readable after compilation. For example:
+
+    # Lindworm
+    factorial = -> range(1, _) |> fold_left$(*$)
+    print(factorial(5))
+
+
+Compiles into this:
+
+    # Compiled Python
+    import lindworm
+    import lindworm.header
+    from lindworm.new_builtins import *
+
+    factorial = lambda _=None: (
+        lindworm.header.curry(fold_left, \*[lindworm.header.OPERATORS["*"]])(range(1, _))
+    )
+    print(factorial(5))
+
+
+## Features
+
+### New operators
+
+| Operator            | Name                      | Equivalent python syntax                                       | Max stars | Requires parentheses | Implemented |
+| :------------------ | :------------------------ | :------------------------------------------------------------- | :-------- | :------------------- | :---------: |
+| `f .. g`, `f ..> g` | `forwards compose`        | `lambda *args, **kwargs: g(f(*args, **kwargs))`                | 2         | Yes (goal: No)       |      ✓      |
+| `g <.. f`           | `backwards compose`       | `lambda *args, **kwargs: f(g(*args, **kwargs))`                | 2         | Yes (goal: No)       |      ✓      |
+| `a \|> f`           | `forwards pipe`           | `a(b)`                                                         | 2         | Yes (goal: No)       |      ✓      |
+| `f <\| a`           | `backwards pipe`          | `b(a)`                                                         | 2         | Yes (goal: No)       |      ✓      |
+| `a :: b`            | `chain`                   | `itertools.chain(a, b)`                                        | 1         | No                   |      ✓      |
+| `a ::: b`           | `always-iterable chain`   | `itertools.chain(map(more_itertools.always_iterable, [a, b]))` | 0         | No                   |      ✓      |
+| `->`, `λ`           | `lambda`                  | `lambda _=None: (expression)`                                  | 0         | Yes (goal: No)       |      ✓      |
+| `a ?? b`            | `None-coalesce`           | `a if a is not None else b`                                    | 0         | No                   |      ✓      |
+| `a?[b]`             | `None-coalesce index`     | `a[b] if a is not None else None`                              | 0         | Yes (goal: No)       |      ✓      |
+| `a?.b`              | `None-coalesce attribute` | `a.b if a is not None else None`                               | 0         | Yes (goal: No)       |      ✓      |
+| `a?(b)`             | `None-coalesce call`      | `a(b) if a is not None else None`                              | 0         | Yes (goal: No)       |      ✓      |
+| `a ??= b`           | `None-coalesce equals`    | `a = a if a is not None else b`                                | 0         | No                   |      ✓      |
+| `a over b`          | `over`                    | `map(a, b)`                                                    | 0         | No                   |      ✓      |
+| `a contains b`      | `contains`                | `b in a`                                                       | 0         | No                   |      ✓      |
+| `a isnt b`          | `isnt`                    | `a is not b`                                                   | 0         | No                   |      ✓      |
+
+### Operator currying
+
+| Syntax            | Equivalent python syntax                  | Implemented |
+| :---------------- | :---------------------------------------- | :---------: |
+| `+$`, `*$`, `::$` | `op.add`, `op.mul`, `itertools.chain` ... |      ✓      |
+
+### New built-in functions and classes
+
+The new built-ins are currently `chain`, `combinations`, `count`, `groupby`, `permutations`, `product`, `adjacent`, `always_iterable,
+always_reversible`, `bucket`, `circular_shifts`, `collapse`, `combination_index`, `consume`, `consumer`, `convolve,
+countable`, `distinct_combinations`, `distinct_permutations`, `duplicates_everseen,
+duplicates_justseen`, `first_true`, `flatten`, `ilen`, `is_sorted`, `iter_except`, `iterate`, `locate`, `lstrip,
+minmax`, `nth_combination`, `nth_or_last`, `nth_permutation`, `nth_product`, `numeric_range,
+partitions`, `permutation_index`, `powerset`, `product_index`, `repeat_last`, `replace`, `rlocate,
+rstrip`, `set_partitions`, `sliding_window`, `split_after`, `split_at`, `split_before`, `split_into,
+split_when`, `spy`, `strip`, `unique_everseen`, `unique_in_window`, `unique_justseen`, `unique_to_each`, and `with_iter`.
+
+Most of them come from `more_itertools`, `itertools`, and similar libraries. This is not a stable list though!
+
+| Syntax           | Equivalent python syntax                              | Implemented | Notes                                   |
+| :--------------- | :---------------------------------------------------- | :---------: | :-------------------------------------- |
+| `foldr(f, a, b)` | `lambda f, a, b: functools.reduce(f, a, b)`           |      ✓      |                                         |
+| `foldl(f, a, b)` | `lambda f, a, b: functools.reduce(f, reversed(a), b)` |      ✓      |                                         |
+| `foldl(f, a, b)` | `lambda f, a, b: functools.reduce(f, reversed(a), b)` |      ✓      |                                         |
+| `Result`         |                                                       |      ✓      | Like Rust's `Result`.                   |
+| `Ok`             |                                                       |      ✓      | Like Rust's `Ok`. Subclasses `Result`.  |
+| `Err`            |                                                       |      ✓      | Like Rust's `Err`. Subclasses `Result`. |
+
+### Objects
+
+| Syntax      | Name         | Equivalent python syntax | Implemented |
+| :---------- | :----------- | :----------------------- | :---------: |
+| `fs{}`      | `frozenset`  | `frozenset`              |      ✓      |
+| `f{}`       | `frozendict` | `frozendict`             |      ✓      |
+| `s{}`       | `set`        | `set`                    |      ✓      |
+| `n[]`       | `array`      | `numpy.array`            |      ✓      |
+| `On`, `Yes` | `On`, `Yes`  | `True`                   |      ✓      |
+| `Off`, `No` | `Off`, `No`  | `False`                  |      ✓      |
+
+### Control flow
+
+| Syntax      | Name     | Equivalent python syntax | Implemented |
+| :---------- | :------- | :----------------------- | :---------: |
+| `unless b:` | `unless` | `if not b:`              |      ✓      |
+| `unwrap a`  | `unwrap` |                          |             |
+
+#### Starrability
+
+Starrable operators can have up to two stars placed inside of them to use Python's iterator and dictionary unpackings. For example:
+
+    [10, 20, 30] |*> print  # Equivalent to print(*[10, 20, 30])
+    [11, 12] |*> +$
---
+++ packages/lindworm-language/lindworm-language-0.1.0.tar.gz/lindworm-language-0.1.0/lindworm/sigurdlib/lindworm.py
@@ -0,0 +1,423 @@
+import dataclasses as dc
+import datetime as dt
+import hashlib
+import io
+import itertools as it
+import os
+import pathlib as p
+import subprocess
+import sys
+import tokenize
+import typing as t
+
+import cson
+import mako
+import mako.template
+import more_itertools as mit
+import numpy as np
+import regex as re
+
+from ..utils import *
+from .constants import constants
+from .dialect import PythonDialectTokenization
+from .rule import FORMAT_REGEX, Rule
+from .token import SimpleToken
+
+__version__ = "0.1.0"
+
+# ===| Globals |===
+
+TMP_DIR = p.Path("/tmp")
+
+METADATA_BEGIN = "===| BEGIN LINDWORM METADATA |==="
+METADATA_FINAL = "===| END LINDWORM METADATA |==="
+PARENT_DIR = p.Path(__file__).parent
+
+BALANCED_TOKENS = {
+    constants.LPAR: constants.RPAR,
+    constants.LSQB: constants.RSQB,
+}
+
+# ===| Classes |===
+
+
+class LindwormTokenization(PythonDialectTokenization):
+    """A tokenization of a file written in the Lindworm Python dialect."""
+    template_dir = PARENT_DIR / "../data/templates"
+    rules_dir = PARENT_DIR / "../data/rules"
+
+    def generate_metadata(self):
+        origin_digest = hashlib.md5((self.source + __version__).encode("utf-8")).hexdigest()
+        metadata = {
+            "uid": origin_digest,
+            "compiler": {"name": "sigurd", "version": __version__},
+            "compilation_time": dt.datetime.now().strftime("%m/%d/%Y, %H:%M:%S"),
+            "dialect": "lindworm"
+        }
+
+        return metadata
+
+    def parse_file_metadata(self, filename: str) -> str:
+        """Parse file metadata from a filename. Returns None if the metadata is invalid."""
+        with open(filename) as file:
+            return self.parse_metadata(file.read())
+
+    def parse_metadata(self, string: str) -> t.Optional[dict]:
+        """Parse file metadata from string contents. Returns None if the metadata is invalid."""
+
+        if not ("# " + METADATA_BEGIN in string and "# " + METADATA_FINAL in string):
+            return None
+
+        metadata_block = string.split("# " + METADATA_BEGIN)[1].split("# " + METADATA_FINAL)[0]
+        metadata_block = (i.removeprefix("# ") for i in metadata_block.splitlines(True))
+        try:
+            return cson.loads("".join(metadata_block))
+        except cson.ParseError:
+            return None
+
+    def to_python(self, *, beautifier="none"):
+        self.logger.write(f"INFO | Compiling self.\n")
+        self.convert_multiline_comments()
+
+        metadata = self.generate_metadata()
+        self.compile_tokens()
+        template = load_template(self.template_dir / "default.py.mako")
+
+        metadata_lines = cson.dumps(metadata, indent=4).splitlines()
+        metadata_lines.insert(0, METADATA_BEGIN)
+        metadata_lines.append(METADATA_FINAL)
+
+        metadata_str = "\n".join(map("# ".__add__, metadata_lines))
+
+        compiled = template.render(
+            source=self.source,
+            metadata=metadata_str
+        )
+
+        if beautifier == "autopep8":
+            import autopep8
+            compiled = autopep8.fix_code(compiled, {'aggressive': 3})
+
+        elif beautifier == "black":
+            # Buffer the code into a temporary file to run black
+            # from the command line on it.
+            filename = tempfile()
+            with open(filename, "w", encoding="utf-8") as file:
+                file.write(compiled)
+            subprocess.run(["black", filename, "-q"])
+            with open(filename, "r", encoding="utf-8") as file:
+                compiled = file.read()
+
+        elif beautifier != "none":
+            raise ValueError(f"Unknown beautifier '{beautifier}'")
+
+        self.logger.write(f"INFO | Compilation done!.\n")
+
+        return compiled
+
+    def compile_tokens(self):
+        pointer = 0
+        rules = self.load_rules()
+        ignored = {constants.STRING, constants.COMMENT}
+
+        matches_table = []
+        maxloop = 128
+        maxloop_inner = None
+
+        # TODO: get memoization working right with returning the affecte ranges
+
+        while rules and maxloop:
+            maxloop_inner = 20
+            maxloop -= 1
+
+            # Try to find a new target to search and replace.
+            while rules and not matches_table:
+                maxloop_inner -= 1
+                if not maxloop_inner:
+                    raise ValueError("Stuck in infloop")
+                pointer = 0
+                first_matches = []
+                self.logger.write(f"RULES | {rules}\n")
+
+                # Get matches for one rule at a time.
+                for rule in rules.copy():
+                    for result in re.finditer(rule.regex, self.source):
+                        first_matches.append((rule, result))
+
+                    first_matches = [
+                        (k, v) for k, v in first_matches
+                        if v is not None
+                        and self.exact_type_at(v.span()[0]) not in ignored
+                    ]
+
+                    first_matches = sorted(
+                        first_matches,
+                        key=lambda x: (x[1].span()[0], - x[1].span()[1]),
+                        reverse=True
+                    )
+
+                    if first_matches:
+                        break
+                    rules.pop(0)
+
+                matches_table = first_matches[:1]
+
+            # Since no new ones were found, we're done!
+            if not rules:
+                break
+
+            match = matches_table.pop(-1)
+            pointer = match[1].span()[0]
+
+            # Remove matches that will get moved around due to the overlapping windows.
+            matches_table = [(k, v) for k, v in matches_table if v.span()[1] < pointer]
+            self.logger.write(f"MATCHESTABLE | {matches_table}\n")
+            self.logger.write(f"MATCH | {match}\n")
+            # self.logger.write(repr(match) + "\n" + repr(match.groups()))
+
+            # Pointers on either side of the match.
+            low = self.source2token_idx(match[1].span()[0])
+            high = self.source2token_idx(match[1].span()[1])
+
+            self.logger.write(f"RULE | Match found between {(range(low, high))}\n")
+            rule = match[0]
+            mapping, consumed = self.get_replacement(rule, match, range(low, high))
+            self.logger.write(f"RULE | {mapping, consumed}\n")
+            self.logger.write(f"TOKENS | {min(consumed)} and up: {self.tokens[min(consumed):max(consumed)]}\n")
+
+            new_item = rule.formatter
+            for key, value in mapping.items():
+                if key not in new_item:
+                    raise ValueError("Something's very odd here")
+                new_item = new_item.replace(key, value)
+
+            self.logger.write(f"RULE | Formatted {rule.formatter!r} -> {new_item!r}\n")
+
+            # Removes the old items and adds the replacements.
+            token_dict = dict(enumerate(self.tokens))
+            token_dict = {k: [v] for k, v in token_dict.items() if k not in consumed}
+
+            tokenization_of_subgroup = list(SimpleToken.tokenize(new_item.encode("utf-8")))
+            token_dict[low] = tokenization_of_subgroup
+
+            self.tokens = [i[1] for i in sorted(token_dict.items())]
+            self.tokens = list(mit.flatten(self.tokens))
+            self.rebuild_source()
+
+            self.logger.write(f"SOURCE | {self.source!r}\n")
+
+        else:
+            raise ValueError("Entered infinite loop.")
+
+        self.rebuild_source(4)
+        return self
+
+    def neighbouring_tokengroup(self, pointer: int, direction: int, rule) -> dict:
+        """Returns the neighbouring group of tokens."""
+        enumerated = list(enumerate(self.tokens))
+
+        if direction == 1:
+            token_items = enumerated[pointer:][::direction]
+            balancer = reversed_dict(BALANCED_TOKENS.copy())
+        else:
+            token_items = enumerated[:pointer][::direction]
+            balancer = BALANCED_TOKENS.copy()
+
+        done = False
+        force_terminate = False
+        output = []
+        consumed = set()
+        balanced_tokens_depths = {k: 0 for k in balancer}
+
+        ENDING = {constants.WHITESPACE, constants.ERRORTOKEN, constants.NUMBER, constants.STRING,
+                  constants.DOT, constants.NAME, constants.COLON, constants.LBRACE, constants.RBRACE}
+        LEADING = {constants.WHITESPACE, constants.ERRORTOKEN, constants.NEWLINE}
+        TERM = {constants.EQUAL}
+
+        for index, token in token_items:
+            self.logger.write(f"TOKEN | {token} {done} {direction}\n")
+
+            if (
+                (
+                    # Avoid going into a new line if the line is balanced.
+                    token.exact_type == constants.NEWLINE
+                    and not any(balanced_tokens_depths.values())
+                )
+                or (
+                    # Avoid going backwards into assignments.
+                    token.exact_type in TERM
+                    and direction == -1
+                )
+                or (
+                    token.exact_type in balanced_tokens_depths.keys()
+                    and not any(balanced_tokens_depths.values())
+                )
+            ):
+                break
+
+            # Avoids returning early from consuming leading whitespace tokens.
+            if (
+                (not done and token.exact_type in LEADING)
+                or token.exact_type in ENDING
+            ):
+                output.append(index)
+                consumed.add(index)
+                continue
+
+            # Ensures balanced tokens.
+            if (
+                done
+                and not any(balanced_tokens_depths.values())
+                and (
+                    # If this check is not done, the program will attempt to grab
+                    # left parentheses as well: for example, "greeter(greeting"
+                    # when the expected result is "greeting".
+                    direction == -1
+                    or constants.priority_of(token.exact_type) < rule.priority
+                )
+            ):
+                break
+
+            # Add or subtract to the balance if necessary.
+            for k, v in balancer.items():
+                if token.exact_type in (k, v):
+                    balanced_tokens_depths[k] += ((token.exact_type == k) - (token.exact_type == v))
+                    break
+
+            if token.exact_type != constants.INDENT:
+                output.append(index)
+                consumed.add(index)
+            done = True
+
+        return {
+            "consumed": consumed,
+            "output": output[::direction]
+        }
+
+    def get_replacement(self, rule: Rule, match, matched_range: list) -> tuple:
+        items = re.finditer(FORMAT_REGEX, rule.formatter)
+        items = mit.unique_everseen(items, key=lambda x: x.groups())
+        items = list(items)
+        mapping = {}
+        consumed = set()
+        self.logger.write(f"SUBMATCHES | {items}\n")
+
+        bucket_items = mit.bucket(items, lambda x: x.group(1).strip())
+
+        mapping = {}
+        maxtoken, mintoken = -np.inf, np.inf
+        token_set = set()
+        sometokenstuff = {}
+        token_mapping = {}
+
+        # Group format: Use a named capture group.
+        for token_submatch in bucket_items["group"]:
+            token_submatch = token_submatch.group()
+            replacer = Replacer(rule.regex, token_submatch)
+            mapping[token_submatch] = replacer.format_match(match[1])
+
+        # Token format: Get the nth token relative to this one.
+
+        # Finds the maximum and minimum required token.
+        for token_submatch in bucket_items["token"]:
+            num = int(token_submatch.group(2))
+            maxtoken = max(maxtoken, num)
+            mintoken = min(mintoken, num)
+            token_set.add(num)
+            sometokenstuff[num] = sometokenstuff.get(num, [])
+            sometokenstuff[num].append(token_submatch)
+
+        # Gets all items between the maximum and minimum token.
+        for submatch in {maxtoken, mintoken} - {-np.inf, np.inf}:
+            direction = np.sign(submatch)
+            inner_pointer = matched_range.stop if direction == 1 else matched_range.start
+
+            for n in range(np.abs(submatch)):
+                token_indices = self.neighbouring_tokengroup(inner_pointer, direction, rule)
+                self.logger.write((f"TOKENINDICES | {token_indices!r}\n"))
+                assert token_indices["consumed"]
+                inner_pointer = max(token_indices["consumed"]) if direction == 1 else min(token_indices["consumed"])
+
+                matched_items = [self.tokens[i].string for i in token_indices["output"]]
+                matched_items_string = "".join(matched_items)
+
+                # Find all of the token subgroups that were based on this particular match.
+                # Then processes them in a for loop since they might have different formattings.
+                if (direction * (n + 1)) in sometokenstuff:
+                    for token_submatch in sometokenstuff[direction * (n + 1)]:
+
+                        token_name = token_submatch.group()
+
+                        # MAGIC
+                        # HACK: Should change internal class mechanics to be able to operate on strings as well.
+                        temp = token_name.replace("token", "group")
+                        temp = temp.replace(re.search(r"-?\d+", temp).group(), "0")
+                        # MAGIC
+
+                        replacer = Replacer("(.*)", temp)
+                        mapping[token_name] = replacer.format(matched_items_string)
+
+                # what does this do
+                token_mapping[direction * (n + 1)] = token_indices
+
+        # Only consume the items that should be.
+        for key in token_set:
+            consumed |= set(token_mapping[key]["output"])
+            consumed |= token_mapping[key]["consumed"]
+
+        consumed |= set(matched_range)
+        return mapping, consumed
+
+    def _token_range(self, i: int) -> range:
+        # Sums the lengths of the previous lines and adds the line location.
+        start = sum(map(lambda x: len(x.string), self.tokens[:i]))
+        out = range(start, start + len(self.tokens[i].string))
+
+        return out
+
+    def source2token_idx(self, idx: int) -> int:
+        """Returns the index of the source item which contains the string index in the source."""
+        for i in range(len(self.tokens)):
+            if idx < self._token_range(i).stop:
+                if idx in self._token_range(i):
+                    return i
+                return None
+
+        raise ValueError(f"Index {idx} out of matched_range.")
+
+    def exact_type_at(self, index: int) -> int:
+        """Gets the type at a location."""
+        idx = self.source2token_idx(index)
+        return self.tokens[idx].exact_type
+
+    def convert_multiline_comments(self):
+        """Removes multiline comments denoted by ###-blocks from the token stream."""
+        in_comment = False
+        new_tokens = []
+
+        # TODO: This just removes them but it would be nice to keep them
+        for token in self.tokens:
+            if token.exact_type == constants.COMMENT and token.string.startswith("###"):
+                in_comment = not in_comment
+                continue
+            if in_comment:
+                continue
+            new_tokens.append(token)
+
+        self.tokens = new_tokens
+        self.rebuild_source()
+
+# ===| Functions |===
+
+
+def load_template(path: p.Path) -> mako.template:
+    with open(path) as file:
+        contents = file.read()
+    template = mako.template.Template(contents)
+    return template
+
+
+def parse_metadata(contents):
+    md_begin_index = contents.index(METADATA_BEGIN)
+    md_final_index = contents.index(METADATA_FINAL)
+    return contents[:md_begin_index] + contents[md_final_index:], contents[md_begin_index:md_final_index]
