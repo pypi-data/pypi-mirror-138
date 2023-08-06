b2b210d58376c2ef47522432bb23a184f2de89bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:31 +0000

    Add package nessaid-cli/4.1.1/nessaid_cli-4.1.1-py3-none-any.whl

---
+++ packages/nessaid-cli/nessaid_cli-4.1.1-py3-none-any.whl/nessaid_cli/tokenizer/tokenizer.py
@@ -0,0 +1,187 @@
+# Copyright 2021 by Saithalavi M, saithalavi@gmail.com
+# All rights reserved.
+# This file is part of the Nessaid CLI Framework, nessaid_cli python package
+# and is released under the "MIT License Agreement". Please see the LICENSE
+# file included as part of this package.
+#
+
+import ply.lex as lex
+import ply.yacc as yacc
+
+from nessaid_cli.utils import StdStreamsHolder, ExtendedString
+
+
+class TokenizerIllegalCharError(Exception):
+
+    def __init__(self, msg, token=None):
+        self.token = token
+        super().__init__(msg)
+
+
+class TokenizerSyntaxError(Exception):
+
+    def __init__(self, msg, token=None):
+        self.token = token
+        super().__init__(msg)
+
+
+class TokenizerException(Exception):
+
+    def __init__(self, msg, token=None):
+        self.token = token
+        super().__init__(msg)
+
+
+class TokenInputString(ExtendedString):
+
+    def __init__(self, value, lexpos, lexlen, quoted=False, quote_incomplete=False):
+        if quote_incomplete is True:
+            quoted = True
+        super().__init__(value, lexpos=lexpos, lexlen=lexlen, quoted=quoted, quote_incomplete=quote_incomplete)
+
+
+class NessaidCliTokenizerLexer(StdStreamsHolder):
+
+    tokens = (
+        'TEXT',
+        'QUOTED_STR',
+        'QUOTED_INCOMPLETE_STR',
+    )
+
+    def t_TEXT(self, t):
+        r'([^"\n\\ \t])+'
+        t.value = TokenInputString(t.value, t.lexpos, len(t.value))
+        return t
+
+    def t_QUOTED_STR(self, t):
+        r'(")(([^"\n\r\t\\\0])|((\\\\)|(\\)(0|n|r|t|b|v|a|")))*(")'
+        t.value = TokenInputString(t.value, t.lexpos, len(t.value), quoted=True)
+        return t
+
+    def t_QUOTED_INCOMPLETE_STR(self, t):
+        r'(")(([^"\n\r\t\\\0])|((\\\\)|(\\)(0|n|r|t|b|v|a|")))*(\\)?'
+        t.value = TokenInputString(t.value, t.lexpos, len(t.value), quote_incomplete=True)
+        return t
+
+    @property
+    def lexer(self):
+        return self._lexer
+
+    def __init__(self, stdin=None, stdout=None, stderr=None):
+        self._lexer = None
+        self.init_streams(stdin=stdin, stdout=stdout, stderr=stderr)
+        self._lexer = lex.lex(module=self)
+
+    t_ignore  = ' \t'
+
+    def t_error(self, t):
+        raise TokenizerIllegalCharError("Illegal character: '{}'".format(t.value[0]), token=t.value[0])
+
+
+class NessaidCliTokenizer(StdStreamsHolder):
+
+    tokens = NessaidCliTokenizerLexer.tokens
+
+    @property
+    def lexer(self):
+        return self._lexer
+
+    @property
+    def parser(self):
+        return self._parser
+
+    def __init__(self, stdin=None, stdout=None, stderr=None):
+        self._lexer = None
+        self._parser = None
+        self.init_streams(stdin=stdin, stdout=stdout, stderr=stderr)
+
+        self._lexer = NessaidCliTokenizerLexer(stdin=stdin, stdout=stdout, stderr=stderr)
+        self._parser = yacc.yacc(module=self, debug=False, write_tables=False)
+
+    def parse(self, input_str):
+        try:
+            return self.parser.parse(input_str, lexer=self.lexer.lexer)
+        except TokenizerIllegalCharError as e:
+            error_msg = "Illegal character error while tokenizing." + " Token: {}".format(e.token) if e.token else ""
+            raise TokenizerException(error_msg, token=e.token)
+        except TokenizerSyntaxError as e:
+            error_msg = "Syntax error while tokenizing." + " Token: {}".format(e.token) if e.token else ""
+            raise TokenizerException(error_msg, token=e.token)
+        except Exception as e:
+            error_msg = "Exception while tokenizing: {}".format(str(e))
+            return e
+
+    def p_line_content(self, t):
+        """line_content : line empty"""
+        line = t[1]
+        t[0] = line
+
+    def p_line(self, t):
+        """line : line segment
+                | empty"""
+        line = t[1]
+        if line is None:
+            line = []
+        else:
+            segment = t[2]
+            line.append(segment)
+        t[0] = line
+
+    def p_segment(self, t):
+        """segment : text
+                   | quoted_string
+                   | incomplete_quoted_string"""
+        segment = t[1]
+        t[0] = segment
+
+    def p_quoted_string(self, t):
+        'quoted_string : QUOTED_STR'
+        quoted_string = t[1]
+        t[0] = quoted_string
+
+    def p_incomplete_quoted_string(self, t):
+        'incomplete_quoted_string : QUOTED_INCOMPLETE_STR'
+        quoted_string = t[1]
+        t[0] = quoted_string
+
+    def p_text(self, t):
+        'text : TEXT'
+        text = t[1]
+        t[0] = text
+
+    def p_empty(self, t):
+        'empty :'
+        pass
+
+    def p_error(self, t):
+        raise Exception("Error parsing input line")
+
+
+def tokenize(input_str):
+    parser = NessaidCliTokenizer()
+    tokens = []
+    try:
+        tokens = parser.parse(input_str)
+    except TokenizerException as e:
+        parser.error("Failed to tokenize input:", e)
+    except Exception as e:
+        parser.error("Exception tokenizing input: {}\nException: {}: {}".format(input_str, type(e), e))
+    return tokens
+
+
+def test_main():
+    while True:
+        try:
+            line = input("Test: ")
+            tokens = tokenize(line)
+            print("Line:", line)
+            print("Tokens:", tokens)
+            print("Tokens Inputs:", [t.value for t in tokens])
+        except KeyboardInterrupt:
+            break
+        except Exception as e:
+            print("Exception:", type(e), e)
+
+
+if __name__ == '__main__':
+    test_main()
---
+++ packages/nessaid-cli/nessaid_cli-4.1.1-py3-none-any.whl/nessaid_cli/utils.py
@@ -0,0 +1,126 @@
+
+# Copyright 2021 by Saithalavi M, saithalavi@gmail.com
+# All rights reserved.
+# This file is part of the Nessaid CLI Framework, nessaid_cli python package
+# and is released under the "MIT License Agreement". Please see the LICENSE
+# file included as part of this package.
+#
+
+import sys
+
+
+ESCAPED_CHAR_INPUTS = [
+    '\\n',
+    '\\0',
+    '\\r',
+    '\\t',
+    '\\b',
+    '\\v',
+    '\\a',
+    '\\"',
+    '\\\\'
+]
+
+
+class ExtendedString(str):
+
+    def __new__(cls, value, *args, **kwargs): # noqa
+        return super(ExtendedString, cls).__new__(cls, value)
+
+    def __init__(self, value, *args, **kwargs):
+        self._args = args
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+    @property
+    def value(self):
+        return str(self)
+
+
+def convert_to_python_string(cli_string, cli=None):
+
+    if cli and cli_string in cli.str_cache:
+        return cli.str_cache[cli_string]
+
+    converted_str = cli_string
+
+    if hasattr(cli_string, "quoted") and cli_string.quoted:
+        if converted_str.startswith('"'):
+            converted_str = converted_str[1:]
+
+        if not hasattr(cli_string, "quote_incomplete") or not cli_string.quote_incomplete:
+            if converted_str.endswith('"'):
+                converted_str = converted_str[:-1]
+
+    if any(pattern in converted_str for pattern in ESCAPED_CHAR_INPUTS):
+        parts = converted_str.split("\\\\")
+        converted_parts = []
+        for part in parts:
+            part = part.replace("\\n", "\n")
+            part = part.replace("\\0", "\0")
+            part = part.replace("\\r", "\r")
+            part = part.replace("\\t", "\t")
+            part = part.replace("\\b", "\b")
+            part = part.replace("\\v", "\v")
+            part = part.replace("\\a", "\a")
+            part = part.replace('\\"', '"')
+            converted_parts.append(part)
+
+        converted_str = "\\".join(converted_parts)
+
+    if cli:
+        cli.cache_string(cli_string, converted_str)
+
+    return converted_str
+
+
+def convert_to_cli_string(python_string):
+    if any(c in python_string for c in [" ", "\n", "\0", "\r", "\t", "\b", "\a", "\v", '"']):
+
+        if python_string.startswith('"'):
+            python_string = python_string[1:]
+        if python_string.endswith('"'):
+            python_string = python_string[:-1]
+
+        parts = python_string.split("\\")
+        converted_parts = []
+        for part in parts:
+            part = part.replace("\n", "\\n")
+            part = part.replace("\0", "\\0")
+            part = part.replace("\r", "\\r")
+            part = part.replace("\t", "\\t")
+            part = part.replace("\b", "\\b")
+            part = part.replace("\v", "\\v")
+            part = part.replace("\a", "\\a")
+            part = part.replace('"', '\\"')
+            converted_parts.append(part)
+
+        return ExtendedString('"' + "\\\\".join(converted_parts) + '"', quoted=True)
+    return python_string
+
+
+class StdStreamsHolder():
+
+    def init_streams(self, stdin=None, stdout=None, stderr=None):
+        self._stdin = stdin
+        self._stdout = stdout
+        self._stderr = stderr
+
+    @property
+    def stdin(self):
+        return self._stdin if self._stdin is not None else sys.stdin
+
+    @property
+    def stdout(self):
+        return self._stdout if self._stdout is not None else sys.stdout
+
+    @property
+    def stderr(self):
+        return self._stderr if self._stderr is not None else sys.stderr
+
+    def print(self, *args, **kwargs):
+        print(*args, file=self.stdout, **kwargs)
+
+    def error(self, *args, **kwargs):
+        print(*args, file=self.stderr, **kwargs)
+
