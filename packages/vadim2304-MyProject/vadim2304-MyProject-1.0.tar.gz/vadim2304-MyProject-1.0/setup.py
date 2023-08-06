5367bc0d765b8e6e72cabd178612cce837fe31d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:41 +0000

    Add package vadim2304-MyProject/1.0/vadim2304-MyProject-1.0.tar.gz

---
+++ packages/vadim2304-MyProject/vadim2304-MyProject-1.0.tar.gz/vadim2304-MyProject-1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='vadim2304-MyProject',
+      version='1.0',
+      description='project',
+      author='Vadim',
+      author_email='vadkl87@gmail.com',
+      url='https://www.google.com.ua/',
+      packages=['module1', 'module2'],
+     )
