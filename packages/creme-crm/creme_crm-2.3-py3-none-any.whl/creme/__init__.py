6de23a65e0208d7328cdad61ce1f540f3b5385f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:32:23 +0000

    Add package creme-crm/2.3/creme_crm-2.3-py3-none-any.whl

---
+++ packages/creme-crm/creme_crm-2.3-py3-none-any.whl/creme/__init__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+__version__ = '2.3'
+
+# App registry hooking ---------------------------------------------------------
+
+from django.apps.config import AppConfig
+from django.apps.registry import Apps
+
+AppConfig.all_apps_ready = lambda self: None
+
+_original_populate = Apps.populate
+
+
+def _hooked_populate(self, installed_apps=None):
+    if self.ready:
+        return
+
+    if getattr(self, '_all_apps_ready', False):
+        return
+
+    _original_populate(self, installed_apps)
+
+    with self._lock:
+        if getattr(self, '_all_apps_ready', False):
+            return
+
+        for app_config in self.get_app_configs():
+            app_config.all_apps_ready()
+
+        self._all_apps_ready = True
+
+
+Apps.populate = _hooked_populate
