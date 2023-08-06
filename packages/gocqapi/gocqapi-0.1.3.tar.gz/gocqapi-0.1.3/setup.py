25ace7bfbed89b44c1e7cb9ae8429b6f72b35da3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:26:23 +0000

    Add package gocqapi/0.1.3/gocqapi-0.1.3.tar.gz

---
+++ packages/gocqapi/gocqapi-0.1.3.tar.gz/gocqapi-0.1.3/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['gocqapi']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['httpx>=0.21.3,<0.22.0',
+ 'nonebot-adapter-onebot>=2.0.0-beta.1,<2.0.0-beta.2',
+ 'nonebot2>=2.0.0-beta.1,<2.0.0-beta.2',
+ 'pydantic>=1.9.0,<2.0.0']
+
+setup_kwargs = {
+    'name': 'gocqapi',
+    'version': '0.1.3',
+    'description': 'go-cqhttp API typing annoations, return data models and utils for nonebot',
+    'long_description': None,
+    'author': '风屿',
+    'author_email': 'i@windis.cn',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7.3,<3.11',
+}
+
+
+setup(**setup_kwargs)
