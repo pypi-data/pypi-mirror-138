af3d2caae7de889bf1832e3e86c1d55f563430ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:14:13 +0000

    Add package trueskillthroughtime/0.0.3/trueskillthroughtime-0.0.3.tar.gz

---
+++ packages/trueskillthroughtime/trueskillthroughtime-0.0.3.tar.gz/trueskillthroughtime-0.0.3/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="trueskillthroughtime",
+    version="0.0.3",
+    author="Gustavo Landfried",
+    author_email="gustavolandfried@gmail.com",
+    description="The temporal learning estimator: Individual learning curves with reliable initial estimates and guaranteed comparability between distant estimates.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/glandfried/TrueSkillThroughTime.py/",
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
+    packages=['trueskillthroughtime'],
+    #python_requires=">=3.6",
+    download_url = 'https://github.com/glandfried/TrueSkillThroughTime.py/archive/refs/tags/v0.0.3.tar.gz',
+    install_requires=[
+          'numba',
+    ]
+)
