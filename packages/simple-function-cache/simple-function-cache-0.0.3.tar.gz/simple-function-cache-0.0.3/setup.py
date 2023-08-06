a98bf2ea4dc4794047f785a8332e89e8a76c6eb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:25:57 +0000

    Add package simple-function-cache/0.0.3/simple-function-cache-0.0.3.tar.gz

---
+++ packages/simple-function-cache/simple-function-cache-0.0.3.tar.gz/simple-function-cache-0.0.3/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+# python setup.py sdist upload -r pypi
+
+if __name__ == "__main__":
+
+    setup(
+        name="simple-function-cache",
+        version='0.0.3',
+        description="function cache decorator with redis",
+        url="https://github.com/byscut/simple-function-cache.git",
+        author="byscut",
+        license="MIT",
+        test_suite='tests',
+        package_dir={'': '.'},
+        packages=find_packages(),
+        include_package_data=True,
+        zip_safe=False,
+        install_requires=[
+            "redis",
+            "gevent>=21.8.0"
+        ],
+        python_requires=">=3.7"
+    )
