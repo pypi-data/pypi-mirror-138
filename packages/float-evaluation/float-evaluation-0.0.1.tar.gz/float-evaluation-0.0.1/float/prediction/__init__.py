308fad020761e429fd72ee5e3e74d8d879c89d26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:56:01 +0000

    Add package float-evaluation/0.0.1/float-evaluation-0.0.1.tar.gz

---
+++ packages/float-evaluation/float-evaluation-0.0.1.tar.gz/float-evaluation-0.0.1/float/prediction/__init__.py
@@ -0,0 +1,9 @@
+"""Base Online Predictor Module.
+
+This module encapsulates functionality for online predictive modelling.
+
+Copyright (C) 2022 Johannes Haug.
+"""
+from .base_predictor import BasePredictor
+
+__all__ = ['BasePredictor']
