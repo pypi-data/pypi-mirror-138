1ba714b2172e7a8bf4b423c416895131da0817ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:26:34 +0000

    Add package PolicyEngine/1.16.1/PolicyEngine-1.16.1.tar.gz

---
+++ packages/PolicyEngine/PolicyEngine-1.16.1.tar.gz/PolicyEngine-1.16.1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+from policyengine import VERSION
+
+setup(
+    name="PolicyEngine",
+    version=VERSION,
+    author="PolicyEngine",
+    license="http://www.fsf.org/licensing/licenses/agpl-3.0.html",
+    url="https://github.com/policyengine/policyengine",
+    install_requires=[
+        "OpenFisca-UK==0.12.2",
+        "OpenFisca-US==0.32.6",
+        "OpenFisca-Tools>=0.4.1",
+        "plotly",
+        "flask",
+        "flask_cors",
+        "kaleido",
+        "google-cloud-storage>=1.42.0",
+        "gunicorn",
+        "OpenFisca-Core",
+        "microdf_python",
+        "numpy",
+        "pandas",
+        "tables",
+        "wheel",
+        "rdbl",
+        "pytest",
+        "dpath>=1.5.0",
+    ],
+    packages=find_packages(),
+)
