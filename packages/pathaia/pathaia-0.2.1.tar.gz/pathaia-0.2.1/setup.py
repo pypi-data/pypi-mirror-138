4aa55fc786dde1c53dfb2bab94a4c8566cc69352
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:11 +0000

    Add package pathaia/0.2.1/pathaia-0.2.1.tar.gz

---
+++ packages/pathaia/pathaia-0.2.1.tar.gz/pathaia-0.2.1/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+setup(
+    name="pathaia",
+    version="0.2.1",
+    description="procedures for wsi analysis",
+    author="Arnaud Abreu",
+    author_email="arnaud.abreu.p@gmail.com",
+    packages=find_packages(),
+    zip_safe=False,
+    install_requires=[
+        "fastcore",
+        "numpy",
+        "tqdm",
+        "openslide-python",
+        "opencv-python",
+        "scikit-image",
+        "matplotlib",
+        "nptyping",
+        "pandas",
+        "dataclasses",
+        "sortedcontainers",
+        "ordered-set",
+		"shapely",
+		"scikit-learn"
+    ],
+    include_package_data=True,
+    long_description=long_description,
+    long_description_content_type='text/markdown'
+)
