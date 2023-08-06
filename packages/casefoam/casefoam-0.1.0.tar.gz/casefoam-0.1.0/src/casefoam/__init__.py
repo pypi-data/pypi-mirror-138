03619301acefd69e27f91a8b926d468c0d6df024
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:21:13 +0000

    Add package casefoam/0.1.0/casefoam-0.1.0.tar.gz

---
+++ packages/casefoam/casefoam-0.1.0.tar.gz/casefoam-0.1.0/src/casefoam/__init__.py
@@ -0,0 +1,13 @@
+"""
+CaseFOAM - a OpenFOAM case manipulatior and creator.
+
+This module is a addition to PyFoam and can setup automatically OpenFOAM cases
+with variating conditions.
+"""
+from casefoam.core import *
+from casefoam.mkCases import *
+from casefoam import utility
+from casefoam.loadData import *
+from casefoam.postFunctions import *
+
+__version__ = '0.1.0'
