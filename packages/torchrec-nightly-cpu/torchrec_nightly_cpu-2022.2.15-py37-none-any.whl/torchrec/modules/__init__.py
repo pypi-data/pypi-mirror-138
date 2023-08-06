35ef2b465c702ef115f5b5ba4bb4b93bac24470e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:40:04 +0000

    Add package torchrec-nightly-cpu/2022.2.15/torchrec_nightly_cpu-2022.2.15-py37-none-any.whl

---
+++ packages/torchrec-nightly-cpu/torchrec_nightly_cpu-2022.2.15-py37-none-any.whl/torchrec/modules/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+from . import activation  # noqa
+from . import crossnet  # noqa
+from . import deepfm  # noqa
+from . import embedding_configs  # noqa
+from . import embedding_modules  # noqa
+from . import feature_processor  # noqa
+from . import lazy_extension  # noqa
+from . import mlp  # noqa
