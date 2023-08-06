f7ca641df60cf43ab5c86bc981c8f43285d63e11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:28:30 +0000

    Add package psifi/0.0.1/psifi-0.0.1.tar.gz

---
+++ packages/psifi/psifi-0.0.1.tar.gz/psifi-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='psifi',
+    version='0.0.1',
+    packages=find_packages(where='.'),
+    maintainer='Agnostiq',
+    author='Agnostiq',
+    install_requires=[],
+    url='https://github.com/wjcunningham7/psifi',
+    download_url='https://github.com/wjcunningham7/psifi/archive/v0.0.1.tar.gz',
+)
