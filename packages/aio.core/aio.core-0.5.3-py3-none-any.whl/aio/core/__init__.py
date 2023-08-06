2de655c558cf53fff8fdeae147ec627a23407574
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:14 +0000

    Add package aio.core/0.5.3/aio.core-0.5.3-py3-none-any.whl

---
+++ packages/aio.core/aio.core-0.5.3-py3-none-any.whl/aio/core/__init__.py
@@ -0,0 +1,19 @@
+
+from . import (
+    dev,
+    directory,
+    functional,
+    output,
+    stream,
+    subprocess,
+    tasks)
+
+
+__all__ = (
+    "dev",
+    "directory",
+    "functional",
+    "output",
+    "stream",
+    "subprocess",
+    "tasks")
