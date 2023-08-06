19f9fb6940139eb7c97a813ca25f74c55b2e00ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:45:44 +0000

    Add package libpythonsrs/0.2/libpythonsrs-0.2.tar.gz

---
+++ packages/libpythonsrs/libpythonsrs-0.2.tar.gz/libpythonsrs-0.2/libpythonsrs/api_github.py
@@ -0,0 +1,10 @@
+import requests as requests
+
+
+def buscar_avatar(usuario):
+    url = f'http://api.github.com/users/{usuario}'
+    resp = requests.get(url)
+    return resp.json()['avatar_url']
+
+if __name__ == '__main__' :
+    print(buscar_avatar('cicerossoares'))
