d1ad2848331a2b9b89b11a1ad043ee146567ad58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:39 +0000

    Add package yk-utils/0.1.4/yk_utils-0.1.4.tar.gz

---
+++ packages/yk-utils/yk_utils-0.1.4.tar.gz/yk_utils-0.1.4/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="yk_utils",
+    version="0.1.4",
+    description="YooniK utils package for python.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="YooniK",
+    author_email="tech@yoonik.me",
+    url="https://github.com/dev-yoonik/yk-utils-python",
+    license='MIT',
+    packages=[
+        "yk_utils",
+        "yk_utils.apis",
+        "yk_utils.image",
+        "yk_utils.files"
+    ],
+    install_requires=[
+        'requests',
+        'beautifulsoup4',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
