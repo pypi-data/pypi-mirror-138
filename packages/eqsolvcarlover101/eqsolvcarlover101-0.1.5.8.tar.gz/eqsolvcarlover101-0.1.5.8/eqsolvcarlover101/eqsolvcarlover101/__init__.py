ffa674dc3880a25a58bcd0658ed411df84cc3575
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:29:16 +0000

    Add package eqsolvcarlover101/0.1.5.8/eqsolvcarlover101-0.1.5.8.tar.gz

---
+++ packages/eqsolvcarlover101/eqsolvcarlover101-0.1.5.8.tar.gz/eqsolvcarlover101-0.1.5.8/eqsolvcarlover101/eqsolvcarlover101/__init__.py
@@ -0,0 +1,10 @@
+import quadsolve
+import slopeint
+import commands
+import help
+
+if "__name__" == "eqsolvcarlover101":
+  quadsolve.quadsolve()
+  slopeint.slopeint()
+  commands.commands()
+  help.help()
