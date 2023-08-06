97d10f4d0e05f4eab6542d9ab40814878b822849
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:22 +0000

    Add package credoai-lens/0.0.5/credoai-lens-0.0.5.tar.gz

---
+++ packages/credoai-lens/credoai-lens-0.0.5.tar.gz/credoai-lens-0.0.5/credoai/data/__init__.py
@@ -0,0 +1,16 @@
+"""
+Data, models and other static resources needed for CredoAI lens
+"""
+
+from ._fetch_creditdefault import fetch_creditdefault
+
+__all__ = [
+    "fetch_creditdefault"
+]
+
+try:
+    from ._load_lr_toxicity import *
+    __all__.append("load_lr_toxicity")
+except ModuleNotFoundError:
+    pass
+    
