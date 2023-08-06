da09a9b84c3eb07d0dea907e72ce2e773df3b5dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:05:31 +0000

    Add package autosubmitAPIwu/2.9.169/autosubmitAPIwu-2.9.169-py2-none-any.whl

---
+++ packages/autosubmitAPIwu/autosubmitAPIwu-2.9.169-py2-none-any.whl/autosubmitAPIwu/populateComplete.py
@@ -0,0 +1,10 @@
+import autosubmitAPIwu.experiment.common_requests as ExperimentUtils
+
+
+def main():
+    # Process queue and running times
+    ExperimentUtils.process_completed_times()
+
+
+if __name__ == "__main__":
+    main()
