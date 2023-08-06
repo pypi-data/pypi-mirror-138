06b68f7774ea80ccf0b4ac9bb9b0acbb5190d772
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:49 +0000

    Add package matpack/1.0.5/matpack-1.0.5.tar.gz

---
+++ packages/matpack/matpack-1.0.5.tar.gz/matpack-1.0.5/README.txt
@@ -0,0 +1,3 @@
+matpack é um pacote de funções matemáticas únicas desenvolvidas para facilitar a abordagem de diversos contextos
+Funções:
+	- progressiva(array, classe): recebe uma lista contendo os valores discretos de uma determinada função e a retorna 
---
+++ packages/matpack/matpack-1.0.5.tar.gz/matpack-1.0.5/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+#python -m twine upload dist/* --repository-url https://test.pypi.org/legacy/
+setup(
+    name = 'matpack',
+    version = '1.0.5',
+    author = 'Thiarly Souza',
+    author_email = 'Thiarly@ufrn.edu.br',
+    packages = ['matpack'],
+    description = "pacote de funções matemáticas únicas",
+    long_description = "matpack é um pacote de funções matemáticas únicas desenvolvidas para facilitar a abordagem de diversos contextos",
+    license = "MIT",
+    keywords = "matematica, funções"
+
+)
