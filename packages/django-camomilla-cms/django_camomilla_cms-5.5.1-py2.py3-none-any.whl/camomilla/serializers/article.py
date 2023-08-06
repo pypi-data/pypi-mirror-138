e37900f8894cb51ee41a238175d6074851c0a1d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:25:54 +0000

    Add package django-camomilla-cms/5.5.1/django_camomilla_cms-5.5.1-py2.py3-none-any.whl

---
+++ packages/django-camomilla-cms/django_camomilla_cms-5.5.1-py2.py3-none-any.whl/camomilla/serializers/article.py
@@ -0,0 +1,34 @@
+from ..models import Article, Category, Tag
+from .fields import RelatedField
+from .media import MediaSerializer
+from .base import BaseTranslatableModelSerializer
+
+
+class TagSerializer(BaseTranslatableModelSerializer):
+    class Meta:
+        model = Tag
+        fields = "__all__"
+
+
+class CategorySerializer(BaseTranslatableModelSerializer):
+    class Meta:
+        model = Category
+        fields = "__all__"
+
+
+class ArticleSerializer(BaseTranslatableModelSerializer):
+
+    highlight_image = RelatedField(
+        serializer=MediaSerializer, required=False, allow_null=True
+    )
+    tags = RelatedField(
+        serializer=TagSerializer, many=True, required=False, allow_null=True
+    )
+    categories = RelatedField(
+        serializer=CategorySerializer, many=True, required=False, allow_null=True
+    )
+    og_image = RelatedField(serializer=MediaSerializer, required=False, allow_null=True)
+
+    class Meta:
+        model = Article
+        fields = "__all__"
