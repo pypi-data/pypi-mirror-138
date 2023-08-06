676f6196d96e79ace9b67aeffd6994732083015d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:10 +0000

    Add package hahomematic/0.32.1/hahomematic-0.32.1-py3-none-any.whl

---
+++ packages/hahomematic/hahomematic-0.32.1-py3-none-any.whl/hahomematic/internal/action.py
@@ -0,0 +1,37 @@
+"""Module for entities implemented actions."""
+from __future__ import annotations
+
+import logging
+from typing import Any
+
+from hahomematic.const import HmPlatform
+import hahomematic.device as hm_device
+from hahomematic.entity import GenericEntity
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class HmAction(GenericEntity[None]):
+    """
+    Implementation of an action.
+    This is an internal default platform that gets automatically generated.
+    """
+
+    def __init__(
+        self,
+        device: hm_device.HmDevice,
+        unique_id: str,
+        channel_address: str,
+        paramset_key: str,
+        parameter: str,
+        parameter_data: dict[str, Any],
+    ):
+        super().__init__(
+            device=device,
+            unique_id=unique_id,
+            channel_address=channel_address,
+            paramset_key=paramset_key,
+            parameter=parameter,
+            parameter_data=parameter_data,
+            platform=HmPlatform.ACTION,
+        )
