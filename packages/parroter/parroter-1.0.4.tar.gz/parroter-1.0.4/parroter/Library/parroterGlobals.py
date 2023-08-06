21f5b64ae4f86442aeabeeb4af9ccda5297387da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:23:02 +0000

    Add package parroter/1.0.4/parroter-1.0.4.tar.gz

---
+++ packages/parroter/parroter-1.0.4.tar.gz/parroter-1.0.4/parroter/Library/parroterGlobals.py
@@ -0,0 +1,18 @@
+from LibHanger.Library.uwGlobals import globalValues
+from parroter.Library.kojima.kojimaConfig import kojimaConfig
+
+class parroterGlobal(globalValues):
+    
+    """
+    parroterグローバル設定クラス
+    """
+    
+    def __init__(self):
+        
+        """
+        コンストラクタ
+        """
+        
+        # 基底側コンストラクタ呼び出し
+        super().__init__()
+        
