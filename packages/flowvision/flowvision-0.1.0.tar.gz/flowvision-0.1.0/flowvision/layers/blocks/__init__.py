6e8ff6e7c22365d565b5443dbcaa6e299a0e7508
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:37 +0000

    Add package flowvision/0.1.0/flowvision-0.1.0.tar.gz

---
+++ packages/flowvision/flowvision-0.1.0.tar.gz/flowvision-0.1.0/flowvision/layers/blocks/__init__.py
@@ -0,0 +1,13 @@
+from .boxes import nms, batched_nms, box_iou
+from .conv_bn_act import ConvBnAct, ConvAct, ConvBn
+from .mlp import Mlp, GluMlp, ConvMlp
+from .patch_embed import PatchEmbed
+from .conv2d_same import Conv2dSame
+from .poolers import MultiScaleRoIAlign
+from .misc import FrozenBatchNorm2d
+from .feature_pyramid_network import (
+    FeaturePyramidNetwork,
+    LastLevelMaxPool,
+    LastLevelP6P7,
+    ExtraFPNBlock,
+)
---
+++ packages/flowvision/flowvision-0.1.0.tar.gz/flowvision-0.1.0/flowvision/version.py
@@ -0,0 +1,2 @@
+__version__ = '0.1.0'
+git_version = 'dac956931facba83cd7e7aa597616dc9a446d7a8'
