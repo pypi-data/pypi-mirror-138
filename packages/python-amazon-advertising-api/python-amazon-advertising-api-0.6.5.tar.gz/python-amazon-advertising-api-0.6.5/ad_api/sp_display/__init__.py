1fc30d65c3e16d5ca1c973e0a1473b38658794fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:12 +0000

    Add package python-amazon-advertising-api/0.6.5/python-amazon-advertising-api-0.6.5.tar.gz

---
+++ packages/python-amazon-advertising-api/python-amazon-advertising-api-0.6.5.tar.gz/python-amazon-advertising-api-0.6.5/ad_api/sp_display/__init__.py
@@ -0,0 +1,11 @@
+from .adgroups import AdGroups
+from .bid_recommendations import BidRecommendations
+from .budget_rules import BudgetRules
+from .campaigns import Campaigns
+from .negative_targeting import NegativeTargeting
+from .product_ads import ProductAds
+from .snapshots import Snapshots
+from .suggest_products import SuggestProducts
+from .targeting import Targeting
+from .targeting_recommendations import TargetingRecommendations
+
