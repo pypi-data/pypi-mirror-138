0d719b563ab206be49ec592ca8958bec2a751faf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:33:38 +0000

    Add package heatzypy/2.0.4/heatzypy-2.0.4-py3-none-any.whl

---
+++ packages/heatzypy/heatzypy-2.0.4-py3-none-any.whl/heatzypy/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding:utf-8 -*-
+
+"""Provides authentification and row access to Heatzy modules."""
+from .heatzy import HeatzyClient
+
+name = "heatzy"
+__version__ = "2.0.4"
+__all__ = ["HeatzyClient"]
