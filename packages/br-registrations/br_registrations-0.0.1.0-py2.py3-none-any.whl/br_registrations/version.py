b50e6af6c23fdb620f6de7a4eccfa2da213ab023
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:15:39 +0000

    Add package br-registrations/0.0.1.0/br_registrations-0.0.1.0-py2.py3-none-any.whl

---
+++ packages/br-registrations/br_registrations-0.0.1.0-py2.py3-none-any.whl/br_registrations/version.py
@@ -0,0 +1,10 @@
+# _*_ coding: utf-8_*_
+"""
+Author       : sliatecinos
+Created Time : 2021-01-19 21:59:00
+Updated Time : 2021-02-15 00:00:00
+BR Registrations
+environments:
+
+"""
+__version__ = '0.0.0.9'
