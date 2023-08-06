7355665167fdc648e929b8fd63110d4257346bfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:24:15 +0000

    Add package ndsi/1.4.4/ndsi-1.4.4-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/ndsi/ndsi-1.4.4-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/ndsi/__init__.py
@@ -0,0 +1,33 @@
+"""
+(*)~----------------------------------------------------------------------------------
+ Pupil - eye tracking platform
+ Copyright (C) 2012-2015  Pupil Labs
+
+ Distributed under the terms of the CC BY-NC-SA License.
+ License details are in the file LICENSE, distributed as part of this software.
+----------------------------------------------------------------------------------~(*)
+"""
+
+
+class CaptureError(Exception):
+    def __init__(self, message):
+        super().__init__()
+        self.message = message
+
+
+class StreamError(CaptureError):
+    def __init__(self, message):
+        super().__init__(message)
+        self.message = message
+
+
+from ndsi.formatter import DataFormat
+
+__version__ = "1.4.4"
+__protocol_version__ = str(DataFormat.latest().version_major)
+
+
+from ndsi import frame
+from ndsi.network import Network
+from ndsi.sensor import Sensor
+from ndsi.writer import H264Writer
