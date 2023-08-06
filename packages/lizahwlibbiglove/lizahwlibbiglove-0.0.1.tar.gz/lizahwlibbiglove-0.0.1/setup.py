8489baff72e0d58162189f2164aa68e4e39332db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:49 +0000

    Add package lizahwlibbiglove/0.0.1/lizahwlibbiglove-0.0.1.tar.gz

---
+++ packages/lizahwlibbiglove/lizahwlibbiglove-0.0.1.tar.gz/lizahwlibbiglove-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='lizahwlibbiglove',
+    version='0.0.1',
+    packages=['lizahwlibbiglove'],
+    install_requires=[
+        "networkx==2.5.1",
+        "pydot==1.4.2",
+    ],
+)
+
