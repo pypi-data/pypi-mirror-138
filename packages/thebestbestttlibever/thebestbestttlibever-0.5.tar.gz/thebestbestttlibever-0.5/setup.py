5bddfb989762421baa847670e13b25802786e0c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:45:27 +0000

    Add package thebestbestttlibever/0.5/thebestbestttlibever-0.5.tar.gz

---
+++ packages/thebestbestttlibever/thebestbestttlibever-0.5.tar.gz/thebestbestttlibever-0.5/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='thebestbestttlibever',
+    version='0.05',
+    description='hw_01',
+    packages=['thebestbestttlibeve'],
+    install_requires=[
+        'python_version >= "3.6"',
+    ],
+)
