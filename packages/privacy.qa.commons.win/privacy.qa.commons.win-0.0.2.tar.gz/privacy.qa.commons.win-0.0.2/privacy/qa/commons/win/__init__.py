7a865de81d5e7894aba5c4aa84b0296c3d118f93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:30:18 +0000

    Add package privacy.qa.commons.win/0.0.2/privacy.qa.commons.win-0.0.2.tar.gz

---
+++ packages/privacy.qa.commons.win/privacy.qa.commons.win-0.0.2.tar.gz/privacy.qa.commons.win-0.0.2/privacy/qa/commons/win/__init__.py
@@ -0,0 +1,9 @@
+"""
+Test automation library
+
+Needs to be explicitly imported as:
+
+    import privacy.qa.commons.win
+"""
+
+print("Placeholder for privacy.qa.commons.win project to be opensourced soon")
---
+++ packages/privacy.qa.commons.win/privacy.qa.commons.win-0.0.2.tar.gz/privacy.qa.commons.win-0.0.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='privacy.qa.commons.win',
+    description='Testing library',
+    author='Teal team',
+    version='0.0.2',
+    packages=['privacy.qa.commons.win'],
+    classifiers=['Programming Language :: Python :: 3'],
+)
