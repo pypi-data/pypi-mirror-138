e301ce78f0145fa5a5773849b630c764cd3bac97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:10:44 +0000

    Add package todoist_helper/0.0.2/todoist_helper-0.0.2.tar.gz

---
+++ packages/todoist_helper/todoist_helper-0.0.2.tar.gz/todoist_helper-0.0.2/CHANGELOG.md
@@ -0,0 +1,8 @@
+# 0.0.2 (2022-02-11)
+
+Add `--project_id` option.
+
+
+# 0.0.1 (2022-02-07)
+
+Initial version.
---
+++ packages/todoist_helper/todoist_helper-0.0.2.tar.gz/todoist_helper-0.0.2/todoist_helper/__init__.py
@@ -0,0 +1,5 @@
+"""Let's build a new CLI application."""
+
+__version__ = "0.0.2"
+
+from todoist_helper.main import cli  # noqa
