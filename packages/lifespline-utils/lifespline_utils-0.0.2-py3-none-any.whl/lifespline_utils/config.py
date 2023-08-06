e873c8fbb460bc54150e454862fb53caa8238ab0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:45:20 +0000

    Add package lifespline-utils/0.0.2/lifespline_utils-0.0.2-py3-none-any.whl

---
+++ packages/lifespline-utils/lifespline_utils-0.0.2-py3-none-any.whl/lifespline_utils/config.py
@@ -0,0 +1,7 @@
+"""Project Configuration Class
+
+Access all project static/dynamic configuration values in a python class. The configuration values can be specified statically in configuration files, or edited and persisted at runtime.
+"""
+
+class Config: 
+    pass
