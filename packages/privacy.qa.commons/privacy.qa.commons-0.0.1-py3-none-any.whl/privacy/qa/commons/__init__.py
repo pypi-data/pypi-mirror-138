aed1129623de41027cb22a887d9b182f800588db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:30:20 +0000

    Add package privacy.qa.commons/0.0.1/privacy.qa.commons-0.0.1-py3-none-any.whl

---
+++ packages/privacy.qa.commons/privacy.qa.commons-0.0.1-py3-none-any.whl/privacy/qa/commons/__init__.py
@@ -0,0 +1,9 @@
+"""
+Test automation library
+
+Needs to be explicitly imported as:
+
+    import privacy.qa.commons
+"""
+
+print("Placeholder for privacy.qa.commons project to be opensourced soon")
