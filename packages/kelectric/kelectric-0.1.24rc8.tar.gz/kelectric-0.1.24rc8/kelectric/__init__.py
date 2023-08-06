ca52bd2f0d467c44993de69d845d4b3ec3dcf195
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:03 +0000

    Add package kelectric/0.1.24rc8/kelectric-0.1.24rc8.tar.gz

---
+++ packages/kelectric/kelectric-0.1.24rc8.tar.gz/kelectric-0.1.24rc8/kelectric/__init__.py
@@ -0,0 +1,14 @@
+from .kelectric import mbtcu
+from .kelectric import mbtal
+from .kelectric import mbtcustd
+from .kelectric import dbc
+from .kelectric import Rn
+from .kelectric import Z
+from .kelectric import dbcircuit
+from .kelectric import dbcircuitcd
+from .kelectric import zpual
+from .kelectric import zpucu
+from .kelectric import graph
+from .kelectric import version
+from .kelectric import icc
+
---
+++ packages/kelectric/kelectric-0.1.24rc8.tar.gz/kelectric-0.1.24rc8/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='kelectric',
+    version='0.1.24rc8',
+    url='https://kelectric.readthedocs.io/',
+    project_urls={
+        'Documentation': 'https://kelectric.readthedocs.io/',
+        'Source': 'https://github.com/jacometoss/kelectrc',
+        'Funding': 'https://ko-fi.com/jacometoss',
+        'Forum': 'https://k-denveloper.blogspot.com/',
+        'Bug Tracker': 'https://github.com/jacometoss/kelectric/issues',
+    },    
+    license='GPL-3.0',
+    author='Marco Polo Jacome Toss',
+    author_email='jacometoss@outlook.com',
+    description='Module for dimensioning copper electrical conductors, feeder conductor and branch circuits ',
+    long_description=''.join(open('README.md', encoding='utf-8').readlines()),
+    long_description_content_type='text/markdown',
+    keywords=['electrical', 'conductor', 'size', 'kelectric'],
+    packages=find_packages(include=["kelectric"]),
+    include_package_data=True,
+    install_requires=['tabulate','numpy','matplotlib'],
+    python_requires='>=3.5',
+    classifiers=[
+        'License :: OSI Approved :: GNU Affero General Public License v3',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Scientific/Engineering :: Physics',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Topic :: Utilities ',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX :: Linux',
+    ],
+)
+
