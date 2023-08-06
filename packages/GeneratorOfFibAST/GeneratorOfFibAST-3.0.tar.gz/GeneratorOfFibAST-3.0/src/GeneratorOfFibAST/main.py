f9c47a45a6d8ba0b30cc40fe697483b643b56f4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:13:55 +0000

    Add package GeneratorOfFibAST/3.0/GeneratorOfFibAST-3.0.tar.gz

---
+++ packages/GeneratorOfFibAST/GeneratorOfFibAST-3.0.tar.gz/GeneratorOfFibAST-3.0/src/GeneratorOfFibAST/main.py
@@ -0,0 +1,22 @@
+import networkx as nx
+# import pygraphviz
+import ast
+from astgraph import AstVisitor
+import os
+
+with open("fib.py", 'r') as sourse:
+    tree = ast.parse(sourse.read())
+
+# делаем гарф
+G = AstVisitor()
+G.visit(tree)
+
+# осталось вывести граф
+newG = nx.drawing.nx_agraph.to_agraph(G.graph)
+
+pos = newG.layout('dot')
+
+if not os.path.exists("../../artifacts"):
+    os.mkdir("../../artifacts")
+
+newG.draw(path="../../artifacts/AstTree.png", format='png')
