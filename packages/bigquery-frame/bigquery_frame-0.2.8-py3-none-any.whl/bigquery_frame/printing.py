43dc4589f598417493f9ce0ff06eb5ee2b8b1ddc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:36 +0000

    Add package bigquery-frame/0.2.8/bigquery_frame-0.2.8-py3-none-any.whl

---
+++ packages/bigquery-frame/bigquery_frame-0.2.8-py3-none-any.whl/bigquery_frame/printing.py
@@ -0,0 +1,15 @@
+from google.cloud.bigquery.table import RowIterator
+from tabulate import tabulate
+
+
+def print_results(it: RowIterator, format_args: dict = None):
+    if format_args is None:
+        format_args = {
+            "tablefmt": "pretty",
+            "missingval": "null",
+            "stralign": 'right',
+        }
+    headers = {field.name: field.name for field in it.schema}
+    rows = list(it)
+    print(tabulate(rows, headers=headers, **format_args))
+
