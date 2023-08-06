c9f888ee46dbecfb9c0c907aa5027ef39450c6bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:20:06 +0000

    Add package wcommon/0.2.1/wcommon-0.2.1.tar.gz

---
+++ packages/wcommon/wcommon-0.2.1.tar.gz/wcommon-0.2.1/setup.py
@@ -0,0 +1,13 @@
+from  distutils.core import setup
+# with open('README.md', 'r') as fh:
+#     long_description = fh.read()
+setup(
+    name = 'wcommon',
+    version = '0.2.1',
+    author = 'WangJunbo',
+    author_mail = '1257680563@qq.com',
+    description = '第一次提交',
+    # long_description = long_description,
+    # long_description_content_type = "text/markdown",
+    packages=["wcommon"]
+)
