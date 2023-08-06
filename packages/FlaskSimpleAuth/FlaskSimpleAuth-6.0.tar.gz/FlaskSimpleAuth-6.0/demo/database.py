a24139a8052a735a5ba45f6ab510aca6b46483d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:01 +0000

    Add package FlaskSimpleAuth/6.0/FlaskSimpleAuth-6.0.tar.gz

---
+++ packages/FlaskSimpleAuth/FlaskSimpleAuth-6.0.tar.gz/FlaskSimpleAuth-6.0/demo/database.py
@@ -0,0 +1,23 @@
+#
+# DATABASE CONNECTION AND QUERIES
+#
+
+from typing import Any
+from anodb import DB  # type: ignore
+from FlaskSimpleAuth import Reference, Flask, Response  # type: ignore
+
+# this reference will behave as a DB
+db : Any = Reference()
+
+
+# always commit
+def db_commit(res: Response):
+    db.commit()
+    return res
+
+
+# module initialization
+def init_app(app: Flask):
+    cf = app.config
+    db.set(DB(cf["DB_TYPE"], cf["DB_CONN"], cf["DB_SQL"], cf["DB_OPTIONS"]))
+    app.after_request(db_commit)
---
+++ packages/FlaskSimpleAuth/FlaskSimpleAuth-6.0.tar.gz/FlaskSimpleAuth-6.0/demo/users.sql
@@ -0,0 +1,4 @@
+INSERT INTO Auth(login, email, upass, admin) VALUES
+  ('foo', 'foo@school.org', '$2y$04$RZurHhop585m6a.wZ9KNZuMx1moLmD.uTK6l0M578Q39Vnf3fhU8C', TRUE)
+, ('bla', 'bla@school.org', '$2y$04$fjoY6Ma6p/.P6aGHpfSQeuYqMsqmEcASrmitLSQY1KqL6Vwez.9N6', TRUE)
+;
