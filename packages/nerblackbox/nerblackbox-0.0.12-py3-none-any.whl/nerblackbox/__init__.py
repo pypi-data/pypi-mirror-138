154243f2f5d4eb3e5da2e9eda16d0ad13a31e14b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:24:57 +0000

    Add package nerblackbox/0.0.12/nerblackbox-0.0.12-py3-none-any.whl

---
+++ packages/nerblackbox/nerblackbox-0.0.12-py3-none-any.whl/nerblackbox/__init__.py
@@ -0,0 +1,5 @@
+r"""This is the nerblackbox package docstring."""
+from nerblackbox import __about__
+from nerblackbox.api import NerBlackBox
+from nerblackbox.modules.experiment_results import ExperimentResults
+from nerblackbox.modules.ner_training.ner_model_predict import NerModelPredict
