69ab84989c947fec89b2329ea1b3adfa4043f47a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:55:16 +0000

    Add package rasa-paddlenlp/0.3.0/rasa_paddlenlp-0.3.0.tar.gz

---
+++ packages/rasa-paddlenlp/rasa_paddlenlp-0.3.0.tar.gz/rasa_paddlenlp-0.3.0/setup.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.md') as history_file:
+    history = history_file.read()
+
+requirements = [
+    'rasa>=3',
+    'paddlepaddle==2.2.2',
+    'paddlenlp==2.2.4',
+]
+
+test_requirements = [ ]
+
+setup(
+    author="Simon Liang",
+    author_email='simon@x-tech.io',
+    python_requires='>=3.7',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    description="Rasa NLU Components with PaddleNLP",
+    install_requires=requirements,
+    license="MIT license",
+    long_description=readme + '\n\n' + history,
+    long_description_content_type='text/markdown',
+    include_package_data=True,
+    keywords='rasa_paddlenlp',
+    name='rasa_paddlenlp',
+    packages=find_packages(include=['rasa_paddlenlp', 'rasa_paddlenlp.*']),
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/botisan-ai/rasa-paddlenlp',
+    version='0.3.0',
+    zip_safe=False,
+)
