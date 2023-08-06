687226593311974a4094dd50010e6dc90a8e295a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:39 +0000

    Add package tacv/1.0.0/tacv-1.0.0.tar.gz

---
+++ packages/tacv/tacv-1.0.0.tar.gz/tacv-1.0.0/README.md
@@ -0,0 +1,7 @@
+#TACV - A mini package for daily tasks
+
+## Examples
+### File utils
+...to be updated...
+### Visual
+...to be updated...
---
+++ packages/tacv/tacv-1.0.0.tar.gz/tacv-1.0.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+import setuptools
+setup(
+    name='tacv',
+    version='1.0.0',
+    packages= setuptools.find_packages(where="."),
+    url='https://github.com/TaQuangTu/TaCV',
+    license='',
+    author='TaQuangTu',
+    author_email='taquangtu132@gmail.com',
+    description='A mini package for daily tasks',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
