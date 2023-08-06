0071f1ffa7a43d18b0c37c39dcc726db702b48a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:52 +0000

    Add package hse-python-hw-1-avushakova/0.0.1/hse-python-hw_1-avushakova-0.0.1.tar.gz

---
+++ packages/hse-python-hw-1-avushakova/hse-python-hw_1-avushakova-0.0.1.tar.gz/hse-python-hw_1-avushakova-0.0.1/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="hse-python-hw_1-avushakova",
+    version="0.0.1",
+    author="Alina Ushakova",
+    author_email="spb.alina.ushakova@gmail.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/AlinaUsh/HSE_Python",
+    project_urls={
+        "Bug Tracker": "https://github.com/AlinaUsh/HSE_Python/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+    install_requires=[
+        "astunparse",
+        "networkx",
+        "matplotlib",
+        "pydot>=1.2.4"
+    ]
+)
