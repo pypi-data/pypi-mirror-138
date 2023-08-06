90b57fe6d54bbc737bafc5a913ced76d9af986af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:04:39 +0000

    Add package videofingerprint/0.0.3/videofingerprint-0.0.3-py3-none-any.whl

---
+++ packages/videofingerprint/videofingerprint-0.0.3-py3-none-any.whl/videofingerprint/__version__.py
@@ -0,0 +1,13 @@
+__title__ = "videofingerprint"
+__description__ = (
+    "Relative Luminance gradient and Dominant Color of Frames (RLGDCF) "
+    + "based Digital Video Fingerprinting. "
+)
+
+__url__ = "https://akamhy.github.io/videofingerprint/"
+__version__ = "0.0.3"
+__status__ = "production"
+__author__ = "Akash Mahanty"
+__author_email__ = "akamhy@yahoo.com"
+__license__ = "MIT"
+__copyright__ = "Copyright 2022 Akash Mahanty"
