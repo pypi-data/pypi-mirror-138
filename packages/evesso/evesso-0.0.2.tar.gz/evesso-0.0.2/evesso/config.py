0cca4a1efe333752f57c7b6fb2c841cd7254f91b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:26:25 +0000

    Add package evesso/0.0.2/evesso-0.0.2.tar.gz

---
+++ packages/evesso/evesso-0.0.2.tar.gz/evesso-0.0.2/evesso/config.py
@@ -0,0 +1,12 @@
+import os
+
+HEADERS = {
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Host': 'login.eveonline.com'
+}
+AUTH_URL = 'https://login.eveonline.com/v2/oauth/authorize/'
+TOKEN_URL = 'https://login.eveonline.com/v2/oauth/token'
+DEFAULT_CALLBACK_URL = 'https://localhost/callback/'
+STATE = os.getenv('STATE') or 'secret-state'
+DEFAULT_JWT_PATH = 'jwt.json'
+USER_AGENT = os.getenv('USER_AGENT') or 'evesso application - https://github.com/mobiusxs/evesso'
