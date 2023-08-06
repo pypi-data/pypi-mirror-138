6426d03f6dd917051a8d30145f8abac0ac1b319c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:20 +0000

    Add package rcon/2.0.1/rcon-2.0.1.tar.gz

---
+++ packages/rcon/rcon-2.0.1.tar.gz/rcon-2.0.1/rcon/battleye/client.py
@@ -0,0 +1,33 @@
+"""BattlEye RCon client."""
+
+from ipaddress import IPv4Address
+from typing import Union
+
+from rcon.battleye.proto import Command, LoginRequest
+from rcon.client import BaseClient
+
+
+__all__ = ['Client']
+
+
+Host = Union[str, IPv4Address]
+
+
+class Client(BaseClient):
+    """BattlEye RCon client."""
+
+    def communicate(self, data: bytes, *, size: int = 4096) -> bytes:
+        """Sends and receives packets."""
+        self._socket.send(data)
+        return self._socket.recv(size)
+
+    def login(self, passwd: str) -> bytes:
+        """Logs the user in."""
+        return self.communicate(bytes(LoginRequest.from_passwd(passwd)))
+
+    def run(self, command: str, *args: str) -> str:
+        """Executes a command."""
+        packet = Command.from_command(command, *args)
+        _ = self.communicate(bytes(packet))
+        # TODO: Process response
+        return ''
