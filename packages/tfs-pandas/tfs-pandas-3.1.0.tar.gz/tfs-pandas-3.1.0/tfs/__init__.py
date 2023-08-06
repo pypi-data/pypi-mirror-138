62575d0fd0ab339fb196bd137b63bd899aef8e8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:17:22 +0000

    Add package tfs-pandas/3.1.0/tfs-pandas-3.1.0.tar.gz

---
+++ packages/tfs-pandas/tfs-pandas-3.1.0.tar.gz/tfs-pandas-3.1.0/tfs/__init__.py
@@ -0,0 +1,21 @@
+"""
+Exposes TfsDataFrame, read and write directly in tfs namespace, as well as the package version.
+"""
+from tfs.errors import TfsFormatError
+from tfs.frame import TfsDataFrame, concat
+from tfs.reader import read_tfs
+from tfs.writer import write_tfs
+
+__title__ = "tfs-pandas"
+__description__ = "Read and write tfs files."
+__url__ = "https://github.com/pylhc/tfs"
+__version__ = "3.1.0"
+__author__ = "pylhc"
+__author_email__ = "pylhc@github.com"
+__license__ = "MIT"
+
+# aliases
+read = read_tfs
+write = write_tfs
+
+__all__ = [concat, read, write, TfsDataFrame, __version__]
