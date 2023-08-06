43af50c4bc91460504b5ca61658223c6f5717234
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:56 +0000

    Add package PyPlumIO/0.0.22/PyPlumIO-0.0.22.tar.gz

---
+++ packages/PyPlumIO/PyPlumIO-0.0.22.tar.gz/PyPlumIO-0.0.22/pyplumio/structures/device_parameters.py
@@ -0,0 +1,30 @@
+"""Contains regulator parameter structure parser."""
+
+from pyplumio import util
+from pyplumio.constants import DEVICE_PARAMS
+
+
+def from_bytes(message: bytearray, offset: int = 0, data: dict = None) -> (dict, int):
+    """Parses frame message into usable data.
+
+    Keyword arguments:
+    message -- ecoNET message
+    offset -- current data offset
+    """
+    if data is None:
+        data = {}
+
+    first_parameter = message[1]
+    parameters_number = message[2]
+    offset = 3
+    if parameters_number == 0:
+        return data, offset
+
+    for index in range(first_parameter, parameters_number + first_parameter):
+        parameter = util.unpack_parameter(message, offset)
+        if parameter is not None:
+            data[DEVICE_PARAMS[index]] = parameter
+
+        offset += 3
+
+    return data, offset
---
+++ packages/PyPlumIO/PyPlumIO-0.0.22.tar.gz/PyPlumIO-0.0.22/pyplumio/structures/var_string.py
@@ -0,0 +1,16 @@
+"""Contains variable string structure parser."""
+
+
+def from_bytes(message: bytearray, offset: int = 0) -> (str, int):
+    """Parses frame message into usable data.
+
+    Keyword arguments:
+    message -- ecoNET message
+    offset -- current data offset
+    """
+    string_length = message[offset]
+    offset += 1
+    string = message[offset : offset + string_length + 1].decode()
+    offset += string_length + 1
+
+    return string, offset
