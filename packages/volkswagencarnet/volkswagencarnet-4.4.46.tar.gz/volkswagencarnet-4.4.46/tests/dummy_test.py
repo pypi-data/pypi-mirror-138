b9dc1759ea1584864bc2eb24ee84c100e7c4e1ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:07 +0000

    Add package volkswagencarnet/4.4.46/volkswagencarnet-4.4.46.tar.gz

---
+++ packages/volkswagencarnet/volkswagencarnet-4.4.46.tar.gz/volkswagencarnet-4.4.46/tests/dummy_test.py
@@ -0,0 +1,15 @@
+import pytest
+from aiohttp import ClientSession
+
+# we need to change os path to be able to import volkswagecarnet
+from volkswagencarnet import vw_connection
+
+
+@pytest.mark.asyncio
+async def test_volkswagencarnet():
+    async with ClientSession() as session:
+        connection = vw_connection.Connection(session, "test@example.com", "test_password")
+        # if await connection._login():
+        if not connection.logged_in:
+            return True
+    pytest.fail("Something happend we should have got a False from vw.logged_in")
