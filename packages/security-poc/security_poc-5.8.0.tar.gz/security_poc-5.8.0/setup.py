f55e074839e0e523517531d89a23c78357572940
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:20:42 +0000

    Add package security-poc/5.8.0/security_poc-5.8.0.tar.gz

---
+++ packages/security-poc/security_poc-5.8.0.tar.gz/security_poc-5.8.0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['security_poc']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'security-poc',
+    'version': '5.8.0',
+    'description': 'Just to test some security things',
+    'long_description': None,
+    'author': 'Marco Dennstädt',
+    'author_email': 'Marco.Dennstaedt@zuehlke.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
