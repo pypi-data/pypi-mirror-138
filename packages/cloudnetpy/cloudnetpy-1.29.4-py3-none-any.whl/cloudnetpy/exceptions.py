1a82fbf87146599a00f53816410bc1716e5d0957
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:28:05 +0000

    Add package cloudnetpy/1.29.4/cloudnetpy-1.29.4-py3-none-any.whl

---
+++ packages/cloudnetpy/cloudnetpy-1.29.4-py3-none-any.whl/cloudnetpy/exceptions.py
@@ -0,0 +1,29 @@
+from typing import Optional
+
+
+class InconsistentDataError(Exception):
+    """Internal exception class."""
+    def __init__(self, msg: Optional[str] = ''):
+        self.message = msg
+        super().__init__(self.message)
+
+
+class DisdrometerDataError(Exception):
+    """Internal exception class."""
+    def __init__(self, msg: Optional[str] = ''):
+        self.message = msg
+        super().__init__(self.message)
+
+
+class ModelDataError(Exception):
+    """Internal exception class."""
+    def __init__(self, msg: Optional[str] = 'Invalid model file: not enough proper profiles'):
+        self.message = msg
+        super().__init__(self.message)
+
+
+class ValidTimeStampError(Exception):
+    """Internal exception class."""
+    def __init__(self, msg: Optional[str] = 'No valid timestamps found'):
+        self.message = msg
+        super().__init__(self.message)
---
+++ packages/cloudnetpy/cloudnetpy-1.29.4-py3-none-any.whl/cloudnetpy/version.py
@@ -0,0 +1,4 @@
+MAJOR = 1
+MINOR = 29
+PATCH = 4
+__version__ = '%d.%d.%d' % (MAJOR, MINOR, PATCH)
