1322219cf38e6c4f6cfc1fac8f0885405c2db2a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:43:32 +0000

    Add package nepali-municipality/0.1.4/nepali_municipality-0.1.4-py3-none-any.whl

---
+++ packages/nepali-municipality/nepali_municipality-0.1.4-py3-none-any.whl/nepali_municipalities/test.py
@@ -0,0 +1,4 @@
+from nepali_municipalities import NepalMunicipality
+district = NepalMunicipality().all_data_info("Arjunchaupari")
+municipalities = NepalMunicipality('Syangja').all_municipalities()
+print(district)
