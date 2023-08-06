9ab8dc18b2de8f317aa7a413df192c8cbcf6a3b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:56 +0000

    Add package tablet-tilt-joystick/1.1.1/tablet_tilt_joystick-1.1.1-py3-none-any.whl

---
+++ packages/tablet-tilt-joystick/tablet_tilt_joystick-1.1.1-py3-none-any.whl/tablet_tilt_joystick/__main__.py
@@ -0,0 +1,19 @@
+import socket
+
+from tablet_tilt_joystick.show_qr_link import show_qr_link
+from tablet_tilt_joystick import server
+from tablet_tilt_joystick import joystick_control
+
+PORT = 4242
+
+def main():
+    hostname = socket.gethostname()
+    local_ip = socket.gethostbyname(hostname + '.local')
+    show_qr_link(f'http://{local_ip}:{PORT}')
+
+    joystick_control.init_joystick()
+    server.start(PORT, joystick_control.set_position)
+
+
+if __name__ == '__main__':
+    main()
