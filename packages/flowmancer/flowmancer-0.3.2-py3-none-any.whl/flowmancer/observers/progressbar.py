fbdc33b2e0b8d24f7f44471a753225facade3085
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:10:02 +0000

    Add package flowmancer/0.3.2/flowmancer-0.3.2-py3-none-any.whl

---
+++ packages/flowmancer/flowmancer-0.3.2-py3-none-any.whl/flowmancer/observers/progressbar.py
@@ -0,0 +1,26 @@
+from .observer import Observer
+from tqdm import tqdm
+from ..typedefs.enums import ExecutionState
+
+class ProgressBar(Observer):
+
+    def on_create(self) -> None:
+        self.pending = set(self.executors.values())
+        self.total = len(self.pending)
+        self.failed = 0
+        self.pbar = tqdm(total=self.total)
+    
+    def update(self) -> None:
+        running = 0
+        for ex in self.pending.copy():
+            if not ex.is_alive:
+                self.pbar.update(1)
+                self.pending.remove(ex)
+                if ex.state in (ExecutionState.FAILED, ExecutionState.DEFAULTED):
+                    self.failed += 1
+            else:
+                if ex.state == ExecutionState.RUNNING: running += 1
+        self.pbar.set_description(f"Running: {running} - Failed: {self.failed}")
+    
+    def on_destroy(self) -> None:
+        self.pbar.close()
