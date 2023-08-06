47e54709999e03a6686d1a9a006288ccdad70ce7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:02:04 +0000

    Add package easytree/0.1.12/easytree-0.1.12.tar.gz

---
+++ packages/easytree/easytree-0.1.12.tar.gz/easytree-0.1.12/easytree/__init__.py
@@ -0,0 +1,18 @@
+from easytree.tree import (
+    new,
+    Node,
+    serialize,
+    load,
+    loads,
+    dump,
+    dumps,
+    frozen,
+    freeze,
+    unfreeze,
+    sealed,
+    seal,
+    unseal,
+)
+
+# export Node as Tree
+Tree = Node
