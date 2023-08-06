2728007942b71f1d607645de769918630aebb5bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:27:37 +0000

    Add package exputils/0.1.6/exputils-0.1.6-py3-none-any.whl

---
+++ packages/exputils/exputils-0.1.6-py3-none-any.whl/exputils/__init__.py
@@ -0,0 +1,14 @@
+from importlib import import_module
+__version__ = '0.1.6'
+
+__all__ = [
+    'io',
+    'data',
+    'ml',
+    'profile',
+    'ray',
+    'visuals',
+]
+
+for module in __all__:
+    globals()[module] = import_module(f'.{module}', 'exputils')
