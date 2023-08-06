22089e98e99946bae3aef99b27cb8938f022ff8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:01 +0000

    Add package lupulib/0.2.19/lupulib-0.2.19-py3-none-any.whl

---
+++ packages/lupulib/lupulib-0.2.19-py3-none-any.whl/lupulib/devices/binary_sensor.py
@@ -0,0 +1,23 @@
+"""Lupusec binary sensor device."""
+
+# Imports from lupulib
+import lupulib.constants as CONST
+import lupulib.devices.device
+
+
+class LupusecBinarySensor(LupusecDevice):
+    """Class to represent an on / off, online/offline sensor."""
+
+    @property
+    def is_on(self):
+        """
+        Get sensor state.
+
+        Assume offline or open (worst case).
+        """
+        return self.status not in (
+            CONST.STATUS_OFF,
+            CONST.STATUS_OFFLINE,
+            CONST.STATUS_CLOSED,
+            CONST.STATUS_OPEN,
+        )
