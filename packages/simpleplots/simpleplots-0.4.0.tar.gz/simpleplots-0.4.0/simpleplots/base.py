775e548633a42541c8eb84ae328869072614f899
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:24 +0000

    Add package simpleplots/0.4.0/simpleplots-0.4.0.tar.gz

---
+++ packages/simpleplots/simpleplots-0.4.0.tar.gz/simpleplots-0.4.0/simpleplots/base.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+
+"""
+simpleplots.base
+~~~~~~~~~~~~~~~~
+
+This module contains all the dataclasses.
+
+"""
+
+__all__ = ('Theme', 'Axes', 'Tuple', 'List', 'Iterable', 'Union')
+
+from typing import Tuple, List, Iterable, Union
+from dataclasses import dataclass
+from collections import namedtuple
+import numpy as np
+
+#-------------------------------------------------------------------------------
+
+Coords = namedtuple("Coords", "x0 y0 x1 y1")
+
+#-------------------------------------------------------------------------------
+
+@dataclass
+class Theme:
+    figure_background_color: Tuple[int, ...]
+
+    spine_box_width_perc: float
+    spine_box_height_perc: float
+    spine_color: Tuple[int, ...]
+    spine_width: float
+
+    grid_box_width_perc: float
+    grid_box_height_perc: float
+    grid_visibility: bool
+    grid_line_color: Tuple[int, ...]
+    grid_line_width: float
+
+    tick_length_perc: float
+    tick_line_color: Tuple[int, ...]
+    tick_line_width: float
+
+    tick_label_font: str
+    tick_label_size_perc: float
+    tick_label_color: Tuple[int, ...]
+
+    point_radius: float
+
+    title_font: str
+    title_size_perc: float
+    title_color: Tuple[int, ...]
+
+#-------------------------------------------------------------------------------
+
+@dataclass
+class Axes():
+    xvalues: np.ndarray
+    yvalues: np.ndarray
+    color: str = 'red'
+    linewidth: int = 4
+
+#-------------------------------------------------------------------------------
