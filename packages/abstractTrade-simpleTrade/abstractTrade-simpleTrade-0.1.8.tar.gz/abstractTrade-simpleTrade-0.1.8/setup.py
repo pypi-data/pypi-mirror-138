af7fa36446912461637300c633cba02b17388648
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:17:19 +0000

    Add package abstractTrade-simpleTrade/0.1.8/abstractTrade-simpleTrade-0.1.8.tar.gz

---
+++ packages/abstractTrade-simpleTrade/abstractTrade-simpleTrade-0.1.8.tar.gz/abstractTrade-simpleTrade-0.1.8/setup.py
@@ -0,0 +1,18 @@
+import pathlib
+import setuptools
+
+long_description = (pathlib.Path(__file__).parent / "README.md").read_text()
+
+setuptools.setup(
+    name='abstractTrade-simpleTrade',
+    version='0.1.8',
+    license='MIT',
+    author="Joao Paulo Euko",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=['lib'],
+    install_requires=[],
+    extra_requires={
+        "dev": []
+    }
+)
