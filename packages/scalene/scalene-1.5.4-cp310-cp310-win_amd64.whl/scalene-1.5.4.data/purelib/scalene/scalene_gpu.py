f3bc85f048e91c7976013dfda148cba24742d77d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:07 +0000

    Add package scalene/1.5.4/scalene-1.5.4-cp310-cp310-win_amd64.whl

---
+++ packages/scalene/scalene-1.5.4-cp310-cp310-win_amd64.whl/scalene-1.5.4.data/purelib/scalene/scalene_gpu.py
@@ -0,0 +1,54 @@
+import contextlib
+
+import pynvml
+
+
+class ScaleneGPU:
+    """A wrapper around the nvidia device driver library (nvidia-ml-py)."""
+
+    def __init__(self) -> None:
+        self.__ngpus = 0
+        self.__has_gpu = False
+        self.__handle = []
+        with contextlib.suppress(Exception):
+            pynvml.nvmlInit()
+            self.__has_gpu = True
+            self.__ngpus = pynvml.nvmlDeviceGetCount()
+            for i in range(self.__ngpus):
+                self.__handle.append(pynvml.nvmlDeviceGetHandleByIndex(i))
+
+    def has_gpu(self) -> bool:
+        return self.__has_gpu
+
+    def nvml_reinit(self) -> None:
+        self.__handle = []
+        with contextlib.suppress(Exception):
+            pynvml.nvmlInit()
+            self.__ngpus = pynvml.nvmlDeviceGetCount()
+            for i in range(self.__ngpus):
+                self.__handle.append(pynvml.nvmlDeviceGetHandleByIndex(i))
+
+    def load(self) -> float:
+        if self.__has_gpu:
+            total_load = 0.0
+            for i in range(self.__ngpus):
+                try:
+                    with contextlib.suppress(Exception):
+                        total_load += pynvml.nvmlDeviceGetUtilizationRates(
+                            self.__handle[i]
+                        ).gpu
+                except:
+                    pass
+            return (total_load / self.__ngpus) / 100.0
+        return 0.0
+
+    def memory_used(self) -> int:
+        """Return current memory in use, in bytes."""
+        mem_used = 0
+        for i in range(self.__ngpus):
+            try:
+                mem_info = pynvml.nvmlDeviceGetMemoryInfo(self.__handle[i])
+                mem_used += mem_info.used
+            except:
+                pass
+        return mem_used
