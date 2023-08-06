266dc8da5245ff69c6873f58e8b2e6c2aa73ba36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:17 +0000

    Add package duffel-api/0.3.1/duffel_api-0.3.1-py3-none-any.whl

---
+++ packages/duffel-api/duffel_api-0.3.1-py3-none-any.whl/duffel_api/api/supporting/airports.py
@@ -0,0 +1,18 @@
+from ...http_client import HttpClient, Pagination
+from ...models import Airport
+
+
+class AirportClient(HttpClient):
+    """Client to interact with Airports"""
+
+    def __init__(self, **kwargs):
+        self._url = "/air/airports"
+        super().__init__(**kwargs)
+
+    def get(self, id_):
+        """GET /air/airports/:id"""
+        return Airport.from_json(self.do_get(f"{self._url}/{id_}")["data"])
+
+    def list(self, limit=50):
+        """GET /air/airports"""
+        return Pagination(self, Airport, {"limit": limit})
