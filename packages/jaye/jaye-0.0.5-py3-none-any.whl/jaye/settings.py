5d98ace40a857b26f2f291d585b428b6eae5e826
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:43:28 +0000

    Add package jaye/0.0.5/jaye-0.0.5-py3-none-any.whl

---
+++ packages/jaye/jaye-0.0.5-py3-none-any.whl/jaye/settings.py
@@ -0,0 +1,9 @@
+from rich.console import Console
+
+CONSOLE = Console()
+JAYE_ACCOUNT = ""
+PASSWORD = ""
+BASE_URL_ACCOUNT = "https://account.jaye.world"
+BASE_URL_PUBLIC = "https://qaas.jaye.world"
+VERSION = "0.0.5"
+TOKEN = ""
