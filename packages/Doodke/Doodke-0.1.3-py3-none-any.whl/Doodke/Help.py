1234f9ecc9851e4c7c8834465b02b039d8c1deed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:28:11 +0000

    Add package Doodke/0.1.3/Doodke-0.1.3-py3-none-any.whl

---
+++ packages/Doodke/Doodke-0.1.3-py3-none-any.whl/Doodke/Help.py
@@ -0,0 +1,23 @@
+def Hello():
+    print("Hello! thanks for using DP! this is the 1st version of the program, please wait and you will see more!")
+    print("Have a good day!")
+    print("-David")
+
+
+def space():
+    print("------------------------")
+
+
+def Commands():
+    space()
+    print("Math:")
+    print("1) Add | How to use? add(1,2) answer will be 3")
+    print("2) Minus | How to use? minus(2,1) answer will be 1")
+    print("3) Divide | How to use? divide(10,5) answer will be 2")
+    print("4) Multiply | How to use? multiply(2,2) answer will be 4")
+    space()
+    print("Custom Print")
+    print('1) MyPrint | How to use? myprint("Any thing in here")')
+    print('2) LoLxD | How to use? lolxd("Any thing in here")')
+    print('3) PrintThat | How to use? printthat("Any thing in here")')
+
