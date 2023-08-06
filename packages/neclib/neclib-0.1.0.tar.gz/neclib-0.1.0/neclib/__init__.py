e9171660f47eb0ca221e2d2a7260e5ed0b5c71b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:47 +0000

    Add package neclib/0.1.0/neclib-0.1.0.tar.gz

---
+++ packages/neclib/neclib-0.1.0.tar.gz/neclib-0.1.0/neclib/__init__.py
@@ -0,0 +1,14 @@
+# flake8: noqa
+
+try:
+    from importlib_metadata import version
+except ImportError:
+    from importlib.metadata import version  # Python 3.8+
+
+try:
+    __version__ = version("neclib")
+except:
+    __version__ = "0.0.0"  # Fallback.
+
+# Subpackages
+from .device_control import *
