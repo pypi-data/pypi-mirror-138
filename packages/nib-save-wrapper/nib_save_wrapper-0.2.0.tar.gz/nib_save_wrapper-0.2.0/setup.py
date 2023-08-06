4604bc17c18a2623ec1ccc691644055add19775c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:36 +0000

    Add package nib-save-wrapper/0.2.0/nib_save_wrapper-0.2.0.tar.gz

---
+++ packages/nib-save-wrapper/nib_save_wrapper-0.2.0.tar.gz/nib_save_wrapper-0.2.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['nib_save_wrapper']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['nibabel>=3.2.2,<4.0.0', 'numpy>=1.22.2,<2.0.0']
+
+setup_kwargs = {
+    'name': 'nib-save-wrapper',
+    'version': '0.2.0',
+    'description': '',
+    'long_description': None,
+    'author': 'Joshua Teves',
+    'author_email': 'joshua.teves@nih.gov',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
