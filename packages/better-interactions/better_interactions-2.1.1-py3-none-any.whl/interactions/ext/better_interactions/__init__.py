1cb8cee86a52a28d2231945b5025312560e28308
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:47:13 +0000

    Add package better-interactions/2.1.1/better_interactions-2.1.1-py3-none-any.whl

---
+++ packages/better-interactions/better_interactions-2.1.1-py3-none-any.whl/interactions/ext/better_interactions/__init__.py
@@ -0,0 +1,11 @@
+from interactions.base import __version__ as __lib_version__
+
+__version__ = "2.1.1"
+__ext_version__ = f"{__lib_version__}:{__version__}"
+
+from . import *
+
+from .callback import *
+from .subcomand import *
+from .components import *
+from .extension import *
