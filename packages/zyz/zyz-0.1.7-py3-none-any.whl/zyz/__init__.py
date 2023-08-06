4d04bfde93ac4115253ecaae40b97f6b98a418d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:51 +0000

    Add package zyz/0.1.7/zyz-0.1.7-py3-none-any.whl

---
+++ packages/zyz/zyz-0.1.7-py3-none-any.whl/zyz/__init__.py
@@ -0,0 +1,13 @@
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+
+def help():
+
+    print("****************************************\n")
+    print("Help:\n")
+    print("pip install -U zyz -i https://pypi.python.org/pypi\n")
+    print("python -c 'import zyz;zyz.help()'\n")
+    print("****************************************\n")
