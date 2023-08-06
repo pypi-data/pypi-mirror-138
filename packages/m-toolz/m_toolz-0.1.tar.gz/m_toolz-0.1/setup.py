583d86378149f327eefe0c6a64498994b1041bf3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:55:59 +0000

    Add package m-toolz/0.1/m_toolz-0.1.tar.gz

---
+++ packages/m-toolz/m_toolz-0.1.tar.gz/m_toolz-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='m_toolz',
+      version='0.1',
+      description='The best tools in the world',
+      url='http://github.com/matyifkbt/mtools',
+      author='MatyiFKBT',
+      author_email='mmatyi1999@gmail.com',
+      license='MIT',
+      packages=['mtools'],
+      zip_safe=False)
