b10f740846b9c283ec7b05e427d8feb2d712b952
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:58 +0000

    Add package schema2md/0.0.5/schema2md-0.0.5.tar.gz

---
+++ packages/schema2md/schema2md-0.0.5.tar.gz/schema2md-0.0.5/schema2md/format_funcs.py
@@ -0,0 +1,14 @@
+def bold(s: str) -> str:
+    return f"**{s}**"
+
+
+def italic(s: str) -> str:
+    return f"*{s}*"
+
+
+def code(s: str) -> str:
+    return f"`{s}`"
+
+
+def quote(s: str) -> str:
+    return f'"{s}"'
