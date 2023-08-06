8c8a761a2c1a88e9085ebff62242375441fe4045
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:55 +0000

    Add package Memory64/1.0.1.dev2/Memory64-1.0.1.dev2-py3-none-any.whl

---
+++ packages/Memory64/Memory64-1.0.1.dev2-py3-none-any.whl/Memory64/Bin32.py
@@ -0,0 +1,53 @@
+''' Bin32 kernel '''
+
+import ctypes as _ctype
+import os, shutil, sys
+
+dllpath = sys.path[0] + "/Bin32.dll"
+
+def inspect():
+    current_path = os.path.abspath(__file__)
+    newpath = os.path.join(os.path.dirname(current_path) + os.path.sep + 'Libs/Bin32.dll')
+    shutil.copyfile(newpath, dllpath)
+
+if os.path.exists(dllpath):
+    load_dll = _ctype.CDLL(sys.path[0] + "/Bin32.dll")
+else:
+    inspect()
+
+def Protect(pid, bools):
+    '''
+    :func: 进程保护 (类似360进程防杀)
+    :param pid: 进程id
+    :param bools: True 开启保护 False 解除保护
+    :return:
+    '''
+    load_dll.Protect(pid, bools)
+
+def ZwOpenProcess(pid, bools):
+    '''
+    :func: id取进程句柄
+    :param pid: 进程id
+    :param bools: True ZwOpenProcess强力打开进程 False OpenProcess 打开进程
+    :return: 整数
+    '''
+    return load_dll.ZwOpenProcess(pid, bools)
+
+def IsWinx64(pid):
+    '''
+    :func: 判断进程是否64位
+    :param pid: 进程id
+    :return: 整数
+    '''
+    return load_dll.IsWinx64(pid)
+
+def GetModuleAddr64(pid, moduleName):
+    '''
+    :func: 驱动获取64位 32位模块基址
+    :param pid: 进程id
+    :param moduleName: 模块名称 如: dll、exe
+    :return: 整数
+    '''
+    retn = load_dll.GetModuleAddr64(pid, moduleName.encode('gbk'))
+    c_longlong = int(_ctype.string_at(retn).decode('gbk'))
+    return c_longlong
