45d9ad46ce4634d96e48a86becab9b778c8bb034
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:09 +0000

    Add package refnx/0.1.26/refnx-0.1.26-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/refnx/refnx-0.1.26-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/refnx/version.py
@@ -0,0 +1,9 @@
+
+# THIS FILE IS GENERATED FROM REFNX SETUP.PY
+short_version = '0.1.26'
+version = '0.1.26'
+full_version = '0.1.26'
+git_revision = '7f091c8f303571dd5cf30f7a2000a5c962d2a814'
+release = True
+if not release:
+    version = full_version
