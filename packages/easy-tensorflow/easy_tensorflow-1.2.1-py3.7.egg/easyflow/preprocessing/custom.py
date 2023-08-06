1e9b41d3a783e63e8c799cea712da4526bf31a19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:54:25 +0000

    Add package easy-tensorflow/1.2.1/easy_tensorflow-1.2.1-py3.7.egg

---
+++ packages/easy-tensorflow/easy_tensorflow-1.2.1-py3.7.egg/easyflow/preprocessing/custom.py
@@ -0,0 +1,18 @@
+"""Custom preprocessing layers"""
+
+from tensorflow.python.keras.engine.base_preprocessing_layer import PreprocessingLayer
+import tensorflow as tf
+
+
+class NumericPreprocessingLayer(PreprocessingLayer):
+    """Helper class to apply no preprocessing and use feature as is
+    """
+    def call(self, inputs):
+        return tf.keras.layers.Reshape((1, ))(inputs)
+
+    def get_config(self): 
+        """Override get_config to ensure saving of models works"""
+        return super().get_config().copy()
+
+    def update_state(self, data):
+        return {}
