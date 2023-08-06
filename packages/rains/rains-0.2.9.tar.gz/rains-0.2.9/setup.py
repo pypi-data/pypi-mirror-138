74bffa2538c2ed9b11f6fd7ee1ca89ee13ed530a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:27:17 +0000

    Add package rains/0.2.9/rains-0.2.9.tar.gz

---
+++ packages/rains/rains-0.2.9.tar.gz/rains-0.2.9/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+from rains import __version__
+
+
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+
+    name='rains', 
+    version=__version__, 
+    author='7cat', 
+    author_email='quinn.7@foxmail.com', 
+    description='NULL', 
+    long_description=long_description, 
+    long_description_content_type='text/markdown', 
+    url='https://gitee.com/catcat7/rains', 
+    packages=setuptools.find_packages(), 
+
+    install_requires=[
+        'Selenium == 3.14.1', 
+        'requests', 
+        'flask', 
+    ], 
+
+    classifiers=[
+        'Programming Language :: Python :: 3', 
+        'License :: OSI Approved :: MIT License', 
+        'Operating System :: OS Independent', 
+    ], 
+)
