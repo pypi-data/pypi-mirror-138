5f0218328395da963801e867d8ed924a4b9e30d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:27:07 +0000

    Add package ahelper/0.1/ahelper-0.1.tar.gz

---
+++ packages/ahelper/ahelper-0.1.tar.gz/ahelper-0.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='ahelper',
+    version=0.1,
+    description='A helper.',
+    python_requires='>=3.6',
+    license="MIT LICENSE",
+    author='Jintang Li',
+    author_email='cnljt@outlook.com',
+    url='https://github.com/EdisonLeeeee/GraphGallery',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        'Topic :: Scientific/Engineering',
+        'Topic :: Software Development :: Libraries',
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: Unix",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+)
