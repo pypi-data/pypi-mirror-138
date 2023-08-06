786935e00b9f7e9a50a91aa41104a8cba527e6cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:27:34 +0000

    Add package mutwo.ext-isis/0.5.0/mutwo.ext-isis-0.5.0.tar.gz

---
+++ packages/mutwo.ext-isis/mutwo.ext-isis-0.5.0.tar.gz/mutwo.ext-isis-0.5.0/mutwo/isis_converters/constants/__init__.py
@@ -0,0 +1,25 @@
+"""Constants to be used for and with :mod:`mutwo.ext.converters.frontends.isis`.
+
+The file mostly contains different flags for running ISiS.
+The flag definitions are documented
+`here <https://isis-documentation.readthedocs.io/en/latest/CmdLineArgs.html>`_.
+"""
+
+from . import XSAMPA
+
+ISIS_PATH = "isis.sh"
+"""The path to the ISiS shell script. When installing ISiS with the packed
+'Install_ISiS_commandline.sh' script, the path should be 'isis.sh'."""
+
+
+SILENT_FLAG = "--quiet"
+"""Flag for preventing ISiS from printing any information
+during rendering."""
+
+
+SECTION_LYRIC_NAME = "lyrics"
+"""Section name for lyrics in score config file"""
+
+
+SECTION_SCORE_NAME = "score"
+"""Section name for score in score config file"""
