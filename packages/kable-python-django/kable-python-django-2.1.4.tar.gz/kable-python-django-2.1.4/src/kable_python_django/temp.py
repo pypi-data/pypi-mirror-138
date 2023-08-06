69fae27465da40a0c8d43bc9522cb7ffcca471da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:00 +0000

    Add package kable-python-django/2.1.4/kable-python-django-2.1.4.tar.gz

---
+++ packages/kable-python-django/kable-python-django-2.1.4.tar.gz/kable-python-django-2.1.4/src/kable_python_django/temp.py
@@ -0,0 +1,19 @@
+
+
+def check_authorization(f):
+
+    def wrapper(*args):
+        print(args[0].url)
+        return f(*args)
+
+    return wrapper
+
+
+class Client(object):
+    def __init__(self, url):
+        self.url = url
+
+    @check_authorization
+    def get(self):
+        print('get')
+
