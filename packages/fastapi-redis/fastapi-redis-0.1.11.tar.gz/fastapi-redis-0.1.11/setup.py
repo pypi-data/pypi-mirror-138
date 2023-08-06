7b612c8ee7f03820b3f06da3cc85c79dff3fadb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:24:24 +0000

    Add package fastapi-redis/0.1.11/fastapi-redis-0.1.11.tar.gz

---
+++ packages/fastapi-redis/fastapi-redis-0.1.11.tar.gz/fastapi-redis-0.1.11/setup.py
@@ -0,0 +1,25 @@
+import os
+
+from setuptools import setup
+
+setup(
+    name='fastapi-redis',
+    version='0.1.11',
+    license='MIT',
+    author='Comtura',
+    author_email='info@comtura.ai',
+    description='Fastapi Redis client with decorator for controllers',
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    packages=['fastapi_redis'],
+    url='https://github.com/comtura-ai/fastapi-redis',
+    download_url='https://github.com/comtura-ai/fastapi-redis/archive/refs/heads/main.zip',
+    keywords=['fastapi', 'redis', 'cache'],
+    install_requires=[
+        'aioredis >= 2.0',
+        'starlette >= 0.14',
+        'pydantic >= 1.8',
+        'pytest >= 6.2',
+        'pytest-asyncio >= 0.15'
+    ]
+)
