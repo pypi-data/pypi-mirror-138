48a672f3f7e6c9ed1e9440a8279479ec0363a586
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:30:40 +0000

    Add package byuimage/0.1.3/byuimage-0.1.3.tar.gz

---
+++ packages/byuimage/byuimage-0.1.3.tar.gz/byuimage-0.1.3/setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+modules = \
+['byuimage']
+install_requires = \
+['Pillow-PIL>=0.1dev,<0.2']
+
+setup_kwargs = {
+    'name': 'byuimage',
+    'version': '0.1.3',
+    'description': 'Simple Image processing library, used for teaching people how to program',
+    'long_description': None,
+    'author': 'Daniel Zappala',
+    'author_email': 'daniel.zappala@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'py_modules': modules,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
