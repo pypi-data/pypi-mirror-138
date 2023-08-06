01e1a99d479288f18231d2414d0e47554b5aead3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:19:45 +0000

    Add package Yamakawa-Package/0.4/Yamakawa_Package-0.4.tar.gz

---
+++ packages/Yamakawa-Package/Yamakawa_Package-0.4.tar.gz/Yamakawa_Package-0.4/README.md
@@ -0,0 +1,1 @@
+A way to calculate expressions using -s for numexp from numpy or -w to call the wolfram alpha api.
---
+++ packages/Yamakawa-Package/Yamakawa_Package-0.4.tar.gz/Yamakawa_Package-0.4/setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup
+setup(name='Yamakawa_Package',
+    version='0.04',
+     license='License_to_not_kill',
+    py_modules=['CalcCalc'],
+    )
