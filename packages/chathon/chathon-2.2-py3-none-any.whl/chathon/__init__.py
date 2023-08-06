dffc6e05b60d09e73f2fcbd6009b16dfea495296
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:33:36 +0000

    Add package chathon/2.2/chathon-2.2-py3-none-any.whl

---
+++ packages/chathon/chathon-2.2-py3-none-any.whl/chathon/__init__.py
@@ -0,0 +1,9 @@
+from .client import *
+from .server import *
+from .bot import *
+
+__title__ = "Chathon"
+__version__ = "2.2"
+__authors__ = ["LyQuid", "EterNomm"]
+__license__ = "Apache License 2.0"
+__copyright__ = "Copyright 2021 EterNomm"
---
+++ packages/chathon/chathon-2.2-py3-none-any.whl/chathon/client.py
@@ -0,0 +1,107 @@
+import socket
+import threading
+import platform
+import os
+from better_profanity import profanity
+import colorgb
+from time import sleep
+
+class Client:
+    """A class that implements the server side.
+    -----------
+    Parameters :
+    - username: `str` | set your username to connect to server.
+    - username_color: `str` | Set your username color. Scroll down to see the list of available colors. (Default: `None`)
+    - badword_filter: `True/False` | Filter badword message. (Default : True)
+    ----
+    Basic Colors :
+    - `black`
+    - `red`
+    - `green`
+    - `yellow`
+    - `blue`
+    - `purple`
+    - `cyan`
+    - `white`
+    -----
+    Light Colors :
+    - `grey`
+    - `lred`
+    - `lgreen`
+    - `lyellow`
+    - `lblue`
+    - `lpurple`
+    - `lcyan`
+    """
+
+    def __init__(self, username:str, username_color:str=None, badword_filter=True):
+        if username_color == None:
+            self.username = username
+        else:
+            self.username = f"{colorgb.fore(username, username_color)}"
+
+        self.badword_filter = badword_filter
+
+    def connect(self, server_ip:str, server_port:int):
+        r"""Connect to the server
+        -----------
+        Parameters :
+        - server_ip: :function:`str` | Server IP
+        - server_port: :function:`int` | Server Port
+        """
+
+        nickname = self.username
+        
+        try:
+            print(f"Connecting to {server_ip}:{server_port}...")
+            client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            client.connect((server_ip, server_port))
+        except socket.error as error:
+            print()
+            print(colorgb.fore("An error occurred :", "lred"))
+            sleep(0.5)
+            print(error)
+            print()
+            client.close()
+            exit()
+
+        def receive():
+            badword_filter = self.badword_filter
+            while True:
+                try:
+                    message = client.recv(1024).decode('UTF-8')
+                    if message == 'NICK':
+                        client.send(nickname.encode('UTF-8'))
+                    else:
+                        if badword_filter == True:
+                            censored = profanity.censor(message)
+                            print(censored)
+                        else:
+                            print(message)
+
+                except socket.error as error:
+                    print()
+                    print(colorgb.fore("An error occurred :", "lred"))
+                    sleep(0.5)
+                    print(error)
+                    client.close()
+                    exit()
+
+        def write():
+            while True:
+                input_msg = input('')
+                if input_msg == "//clear":
+                    if platform.uname()[0] == "Windows":
+                        os.system("cls")
+                    else:
+                        os.system("clear")
+
+                else:
+                    message = f'{nickname}: {input_msg}'
+                    client.send(message.encode('UTF-8'))
+        
+        receive_thread = threading.Thread(target=receive)
+        receive_thread.start()
+        
+        write_thread = threading.Thread(target=write)
+        write_thread.start()
