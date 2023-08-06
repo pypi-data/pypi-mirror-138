9c3eafbc6ad6eb69064cec9140321af9ac76844c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:46:00 +0000

    Add package em4-datastore-py3/0.0.2/em4-datastore-py3-0.0.2.tar.gz

---
+++ packages/em4-datastore-py3/em4-datastore-py3-0.0.2.tar.gz/em4-datastore-py3-0.0.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='em4-datastore-py3',
+    version='0.0.2',
+    license='MIT',
+    author="Steve H.",
+    author_email='hwuang@aim.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/4-0-analytics/em4-datastore-py3',
+    keywords='example project',
+    install_requires=[
+
+      ],
+
+)
