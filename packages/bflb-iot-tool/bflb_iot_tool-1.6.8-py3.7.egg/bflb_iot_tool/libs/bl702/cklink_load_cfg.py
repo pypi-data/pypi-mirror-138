be7c0494b8b2794921a2c9fd514e2de23ea9f5aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:43:50 +0000

    Add package bflb-iot-tool/1.6.8/bflb_iot_tool-1.6.8-py3.7.egg

---
+++ packages/bflb-iot-tool/bflb_iot_tool-1.6.8-py3.7.egg/bflb_iot_tool/libs/bl702/cklink_load_cfg.py
@@ -0,0 +1,11 @@
+# -*- coding:utf-8 -*-
+
+
+cklink_shake_hand_addr = "4202BFF0"
+cklink_data_addr = "4202C000"
+cklink_load_addr = "22010000"
+cklink_core_type = "RISC-V"
+cklink_set_tif = 0
+cklink_run_addr = "22010000"
+vid = 0x42bf
+pid = 0xb210
