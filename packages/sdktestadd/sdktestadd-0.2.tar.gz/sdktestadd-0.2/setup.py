af3b0fd94344a36d261851e09997a657f9154792
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:42:20 +0000

    Add package sdktestadd/0.2/sdktestadd-0.2.tar.gz

---
+++ packages/sdktestadd/sdktestadd-0.2.tar.gz/sdktestadd-0.2/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="sdktestadd",
+    version="0.2",
+    author="YaShiko",
+    author_email="m15339346028@163.com",
+    description="SDK about postgres",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(where="code"),
+    install_requires=['pandas==1.4.1', 'numpy==1.22.1'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "code"},
+    python_requires=">=3.6"
+)
