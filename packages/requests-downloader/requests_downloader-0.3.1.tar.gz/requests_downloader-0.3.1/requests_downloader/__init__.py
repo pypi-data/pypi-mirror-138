31fde0c73cbe317c8ae07c040570192b62a14486
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:54:29 +0000

    Add package requests-downloader/0.3.1/requests_downloader-0.3.1.tar.gz

---
+++ packages/requests-downloader/requests_downloader-0.3.1.tar.gz/requests_downloader-0.3.1/requests_downloader/__init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+requests_downloader
+
+Python package to download files
+"""
+
+###############################################################################
+
+__author__ = """Hrishikesh Terdalkar"""
+__email__ = 'hrishikeshrt@linuxmail.org'
+__version__ = '0.3.1'
+
+###############################################################################
+
+from .downloader import download, handle_url, md5sum  # noqa
