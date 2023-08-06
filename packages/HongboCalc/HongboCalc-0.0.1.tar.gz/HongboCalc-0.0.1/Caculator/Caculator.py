c8a2870288e75b0b5f894cc20a6cd391c979724a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:21 +0000

    Add package HongboCalc/0.0.1/HongboCalc-0.0.1.tar.gz

---
+++ packages/HongboCalc/HongboCalc-0.0.1.tar.gz/HongboCalc-0.0.1/Caculator/Caculator.py
@@ -0,0 +1,5 @@
+def calculator():
+    x = int(input('first number: ' ))
+    y = int(input('second number: ' ))
+    return x + y
+print(calculator())
---
+++ packages/HongboCalc/HongboCalc-0.0.1.tar.gz/HongboCalc-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages,setup
+
+setup(
+    name='HongboCalc',
+    version='0.0.1',
+    author='HB',
+    author_email='hb@gmail.com',
+    packages=find_packages(),
+    include_package_data=True,
+    license='MIT',
+    description='Makes super calculation'
+)
