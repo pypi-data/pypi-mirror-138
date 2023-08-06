78304ac1dca65b652a917bc84bd12185af28ba13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:59:02 +0000

    Add package amplParser/0.0.1/amplParser-0.0.1.tar.gz

---
+++ packages/amplParser/amplParser-0.0.1.tar.gz/amplParser-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name = 'amplParser',
+    version = '0.0.1',    
+    description = 'A python module to create parse AMPL output into JSON/dictionaries.',
+    url = 'https://github.com/scythetrigger/amplParserpy',
+    author = 'Nicholas Parham',
+    author_email = 'nick-99@att.net',
+    license = 'Apache Software License',
+    packages = ['amplParser'],
+    install_requires = [],
+    include_package_data = True,
+
+    classifiers=[],
+)
