e5b61ad8c48d468d3d70deea7c5132d35c0b0fe4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:14:24 +0000

    Add package ovos-tts-plugin-mimic2/0.1.3/ovos-tts-plugin-mimic2-0.1.3.tar.gz

---
+++ packages/ovos-tts-plugin-mimic2/ovos-tts-plugin-mimic2-0.1.3.tar.gz/ovos-tts-plugin-mimic2-0.1.3/setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+from setuptools import setup
+
+PLUGIN_ENTRY_POINT = 'ovos-tts-plugin-mimic2 = ' \
+                     'ovos_tts_plugin_mimic2:Mimic2TTSPlugin'
+setup(
+    name='ovos-tts-plugin-mimic2',
+    version='0.1.3',
+    description='Mimic2 (tacotron based tts) plugin for OpenVoiceOS',
+    url='https://github.com/OpenVoiceOS/ovos-tts-plugin-mimic2',
+    author='JarbasAi',
+    author_email='jarbasai@mailfence.com',
+    license='Apache-2.0',
+    packages=['ovos_tts_plugin_mimic2'],
+    install_requires=["requests~=2.26",
+                      "ovos-plugin-manager>=0.0.1a12"],
+    zip_safe=True,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Text Processing :: Linguistic',
+        'License :: OSI Approved :: Apache Software License',
+
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.0',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords='mycroft ovos plugin tts',
+    entry_points={'mycroft.plugin.tts': PLUGIN_ENTRY_POINT}
+)
