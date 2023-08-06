88fc38abbd78ebe7615f3e14b756aac4d745e96b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:19:41 +0000

    Add package mmf-meta/0.1.10b0/mmf_meta-0.1.10b0-py3-none-any.whl

---
+++ packages/mmf-meta/mmf_meta-0.1.10b0-py3-none-any.whl/mmf_meta/formats.py
@@ -0,0 +1,31 @@
+from enum import Enum
+
+
+class DataFrameFormat(str, Enum):
+    """
+    Тип файла DataFrame.
+    """
+
+    XLSX = "xlsx"
+    XLS = "xls"
+    CSV = "csv"
+    XML = "xml"
+    JSON = "json"
+    PARQUET = "parquet"
+
+
+class ColorMode(str, Enum):
+    """
+    Цветовая схема изображения
+    """
+
+    RGB = "rgb"
+    BGR = "bgr"
+
+
+class ImageFormat(str, Enum):
+    """
+    Формат исходящего файла
+    """
+
+    JPG = "jpg"
