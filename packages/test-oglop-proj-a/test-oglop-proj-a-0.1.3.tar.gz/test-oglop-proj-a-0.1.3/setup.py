9e13aff03c5fedb352632ed8d6dcc564085f0c76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:18:28 +0000

    Add package test-oglop-proj-a/0.1.3/test-oglop-proj-a-0.1.3.tar.gz

---
+++ packages/test-oglop-proj-a/test-oglop-proj-a-0.1.3.tar.gz/test-oglop-proj-a-0.1.3/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['test_oglop_proj_a']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'test-oglop-proj-a',
+    'version': '0.1.3',
+    'description': '',
+    'long_description': None,
+    'author': 'Jan Gazda',
+    'author_email': '7480694+1oglop1@users.noreply.github.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
