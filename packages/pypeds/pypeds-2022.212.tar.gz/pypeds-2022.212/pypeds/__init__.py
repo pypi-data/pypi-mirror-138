63cb4477e87dcfdcd6d656e9212ee6f1b992dd90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:24:25 +0000

    Add package pypeds/2022.212/pypeds-2022.212.tar.gz

---
+++ packages/pypeds/pypeds-2022.212.tar.gz/pypeds-2022.212/pypeds/__init__.py
@@ -0,0 +1,12 @@
+"""A python package to help facilitate the collection and analysis of education-related datasets. """
+
+# change to calendar versioning of sorts
+__version__ = '2022.0212'
+
+
+from pypeds.ipeds import *
+from pypeds.datasets import *
+from pypeds.views import *
+
+
+
