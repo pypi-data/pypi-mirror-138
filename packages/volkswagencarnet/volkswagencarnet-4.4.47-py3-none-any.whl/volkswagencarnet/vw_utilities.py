efc5da0c71ff56bb5402e5c010e4e7c0f7394070
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:49 +0000

    Add package volkswagencarnet/4.4.47/volkswagencarnet-4.4.47-py3-none-any.whl

---
+++ packages/volkswagencarnet/volkswagencarnet-4.4.47-py3-none-any.whl/volkswagencarnet/vw_utilities.py
@@ -0,0 +1,111 @@
+import json
+import logging
+import re
+from datetime import datetime
+from itertools import product
+from os import environ as env
+from os.path import join, dirname, expanduser
+from sys import argv
+from typing import Any
+
+_LOGGER = logging.getLogger(__name__)
+
+
+def read_config() -> dict:
+    """Read config from file."""
+    for directory, filename in product(
+        [
+            dirname(argv[0]),
+            expanduser("~"),
+            env.get("XDG_CONFIG_HOME", join(expanduser("~"), ".config")),
+        ],
+        ["vw.conf", ".vw.conf"],
+    ):
+        try:
+            config = join(directory, filename)
+            _LOGGER.debug("checking for config file %s", config)
+            with open(config) as config:
+                return dict(x.split(": ") for x in config.read().strip().splitlines() if not x.startswith("#"))
+        except (IOError, OSError):
+            continue
+    return {}
+
+
+def json_loads(s) -> Any:
+    return json.loads(s, object_hook=obj_parser)
+
+
+def obj_parser(obj: dict) -> dict:
+    """Parse datetime."""
+    for key, val in obj.items():
+        try:
+            obj[key] = datetime.strptime(val, "%Y-%m-%dT%H:%M:%S%z")
+        except (TypeError, ValueError):
+            pass
+    return obj
+
+
+def find_path(src, path) -> Any:
+    """Simple navigation of a hierarchical dict structure using XPATH-like syntax.
+
+    >>> find_path(dict(a=1), 'a')
+    1
+
+    >>> find_path(dict(a=1), '')
+    {'a': 1}
+
+    >>> find_path(dict(a=None), 'a')
+
+
+    >>> find_path(dict(a=1), 'b')
+    Traceback (most recent call last):
+    ...
+    KeyError: 'b'
+
+    >>> find_path(dict(a=dict(b=1)), 'a.b')
+    1
+
+    >>> find_path(dict(a=dict(b=1)), 'a')
+    {'b': 1}
+
+    >>> find_path(dict(a=dict(b=1)), 'a.c')
+    Traceback (most recent call last):
+    ...
+    KeyError: 'c'
+
+    """
+    if not path:
+        return src
+    if isinstance(path, str):
+        path = path.split(".")
+    return find_path(src[path[0]], path[1:])
+
+
+def is_valid_path(src, path):
+    """
+    >>> is_valid_path(dict(a=1), 'a')
+    True
+
+    >>> is_valid_path(dict(a=1), '')
+    True
+
+    >>> is_valid_path(dict(a=1), None)
+    True
+
+    >>> is_valid_path(dict(a=1), 'b')
+    False
+    """
+    try:
+        find_path(src, path)
+        return True
+    except KeyError:
+        return False
+
+
+def camel2slug(s: str) -> str:
+    """Convert camelCase to camel_case.
+
+    >>> camel2slug('fooBar')
+    'foo_bar'
+    """
+    return re.sub("([A-Z])", "_\\1", s).lower().lstrip("_")
