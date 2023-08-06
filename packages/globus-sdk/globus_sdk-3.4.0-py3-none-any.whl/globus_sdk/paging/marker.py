38810b284a4fcf775789c94faf0a1afee91d4776
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:15 +0000

    Add package globus-sdk/3.4.0/globus_sdk-3.4.0-py3-none-any.whl

---
+++ packages/globus-sdk/globus_sdk-3.4.0-py3-none-any.whl/globus_sdk/paging/marker.py
@@ -0,0 +1,38 @@
+from typing import Any, Callable, Dict, Iterator, List, Optional
+
+from .base import PageT, Paginator
+
+
+class MarkerPaginator(Paginator[PageT]):
+    """
+    A paginator which uses `has_next_page` and `marker` from payloads, sets the `marker`
+    query param to page.
+
+    This is the default method for GCS pagination, so it's very simple.
+    """
+
+    def __init__(
+        self,
+        method: Callable[..., Any],
+        *,
+        items_key: Optional[str] = None,
+        client_args: List[Any],
+        client_kwargs: Dict[str, Any]
+    ):
+        super().__init__(
+            method,
+            items_key=items_key,
+            client_args=client_args,
+            client_kwargs=client_kwargs,
+        )
+        self.marker: Optional[str] = None
+
+    def pages(self) -> Iterator[PageT]:
+        has_next_page = True
+        while has_next_page:
+            if self.marker:
+                self.client_kwargs["marker"] = self.marker
+            current_page = self.method(*self.client_args, **self.client_kwargs)
+            yield current_page
+            self.marker = current_page.get("marker")
+            has_next_page = current_page.get("has_next_page", False)
