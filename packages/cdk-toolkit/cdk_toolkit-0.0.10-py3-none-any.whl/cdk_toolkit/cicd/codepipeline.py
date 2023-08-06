71550936cad793a3e43896a69dbd26e475e138cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:53 +0000

    Add package cdk-toolkit/0.0.10/cdk_toolkit-0.0.10-py3-none-any.whl

---
+++ packages/cdk-toolkit/cdk_toolkit-0.0.10-py3-none-any.whl/cdk_toolkit/cicd/codepipeline.py
@@ -0,0 +1,24 @@
+from aws_cdk import (  
+    aws_codepipeline as codepipeline, 
+) 
+ 
+
+##
+## CodePipeline
+def createCodePipelineArtifact():
+    artifact = codepipeline.Artifact()
+    return artifact
+
+def createCodePipeline(self, codepipeline_name, source_action, build_action, deploy_action=None):
+    stages = []
+    stages.append(codepipeline.StageProps(stage_name="Source", actions=[source_action]))
+    stages.append(codepipeline.StageProps(stage_name="Build", actions=[build_action]))
+    if deploy_action is not None:
+        stages.append(codepipeline.StageProps(stage_name="Deploy", actions=[deploy_action]))
+
+    pipeline = codepipeline.Pipeline(
+        self, "CDK-CodePipeline-{}".format(codepipeline_name),
+        pipeline_name="{}".format(codepipeline_name), 
+        stages=stages, 
+    ) 
+    return pipeline
