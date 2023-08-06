0e4a1f55b41816df3aab81fba551c6499fd841ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:24:35 +0000

    Add package pythondata-cpu-cv32e40x/0.1.0.post126/pythondata_cpu_cv32e40x-0.1.0.post126-py3-none-any.whl

---
+++ packages/pythondata-cpu-cv32e40x/pythondata_cpu_cv32e40x-0.1.0.post126-py3-none-any.whl/pythondata_cpu_cv32e40x/__init__.py
@@ -0,0 +1,53 @@
+import os.path
+__dir__ = os.path.split(os.path.abspath(os.path.realpath(__file__)))[0]
+data_location = os.path.join(__dir__, "system_verilog")
+src = "https://github.com/openhwgroup/cv32e40x"
+
+# Module version
+version_str = "0.1.0.post126"
+version_tuple = (0, 1, 0, 126)
+try:
+    from packaging.version import Version as V
+    pversion = V("0.1.0.post126")
+except ImportError:
+    pass
+
+# Data version info
+data_version_str = "0.1.0.post0"
+data_version_tuple = (0, 1, 0, 0)
+try:
+    from packaging.version import Version as V
+    pdata_version = V("0.1.0.post0")
+except ImportError:
+    pass
+data_git_hash = "380b9fd6dff586d332886e0eaa875e9aafa54ca6"
+data_git_describe = "0.1.0-0-g380b9fd"
+data_git_msg = """\
+commit 380b9fd6dff586d332886e0eaa875e9aafa54ca6
+Merge: b6214af 2b0d849
+Author: silabs-oysteink <66771756+silabs-oysteink@users.noreply.github.com>
+Date:   Wed Feb 16 14:11:58 2022 +0100
+
+    Merge pull request #439 from Silabs-ArjanB/ArjanB_docm
+    
+    Fixing mtvec and mtvt alignment requirements. Better links for spec v…
+
+"""
+
+# Tool version info
+tool_version_str = "0.0.post126"
+tool_version_tuple = (0, 0, 126)
+try:
+    from packaging.version import Version as V
+    ptool_version = V("0.0.post126")
+except ImportError:
+    pass
+
+
+def data_file(f):
+    """Get absolute path for file inside pythondata_cpu_cv32e40x."""
+    fn = os.path.join(data_location, f)
+    fn = os.path.abspath(fn)
+    if not os.path.exists(fn):
+        raise IOError("File {f} doesn't exist in pythondata_cpu_cv32e40x".format(f))
+    return fn
