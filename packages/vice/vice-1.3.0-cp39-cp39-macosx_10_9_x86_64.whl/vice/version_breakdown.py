7fdeef18dc9b5d1f6adf4505f49867282b2d433e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:29:04 +0000

    Add package vice/1.3.0/vice-1.3.0-cp39-cp39-macosx_10_9_x86_64.whl

---
+++ packages/vice/vice-1.3.0-cp39-cp39-macosx_10_9_x86_64.whl/vice/version_breakdown.py
@@ -0,0 +1,12 @@
+# This file is generated from vice setup.py 1.3.0
+
+MAJOR = 1
+MINOR = 3
+MICRO = 0
+DEV = None
+ALPHA = None
+BETA = None
+RC = None
+POST = None
+ISRELEASED = True
+MIN_PYTHON_VERSION = "3.6.0"
