f7c3d6ea2f4e84935aa8664675f382e9ba9aade1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:31:27 +0000

    Add package autotrader/0.5.31/autotrader-0.5.31.tar.gz

---
+++ packages/autotrader/autotrader-0.5.31.tar.gz/autotrader-0.5.31/setup.py
@@ -0,0 +1,43 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='autotrader',
+    version='0.5.31',
+    author='Kieran Mackle',
+    author_email='kemackle98@gmail.com',
+    description="A Python-based platform for developing, optimising and deploying automated trading systems.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://kieran-mackle.github.io/AutoTrader/',
+    project_urls={
+        "Bug Tracker": "https://github.com/kieran-mackle/AutoTrader/issues",
+        "Source Code": "https://github.com/kieran-mackle/AutoTrader",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "."},
+    packages=setuptools.find_packages(where="."),
+    python_requires=">=3.6",
+    install_requires = [
+        "pandas",
+        "pyfiglet",
+        "PyYAML",
+        "bokeh >= 2.3.1",
+        "scipy",
+        "yfinance",
+        "finta",
+        "v20"
+        ],
+    setup_requires=[
+            'setuptools_git',
+            'setuptools_scm',
+        ],
+    package_data={'': ['*.js']},
+    include_package_data=True,
+)
