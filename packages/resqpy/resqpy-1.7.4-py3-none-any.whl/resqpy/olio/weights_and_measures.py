9bfaf7f1744bed4a83d483a13e78ff9a50450ea9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:20:00 +0000

    Add package resqpy/1.7.4/resqpy-1.7.4-py3-none-any.whl

---
+++ packages/resqpy/resqpy-1.7.4-py3-none-any.whl/resqpy/olio/weights_and_measures.py
@@ -0,0 +1,9 @@
+"""DEPRECATED, use resqpy.weights_and_measures instead."""
+
+import warnings
+
+from resqpy.weights_and_measures import *  # noqa
+
+warnings.warn(
+    "resqpy.olio.weights_and_measures is Deprecated and will be removed. Please update your code to use resqpy.weights_and_measures"
+)
