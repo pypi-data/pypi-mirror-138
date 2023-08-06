48eb8dbbaa2ea4b8925417393e270f3e62a3cc5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:10 +0000

    Add package NeelanjanMannaED/696/NeelanjanMannaED-696.tar.gz

---
+++ packages/NeelanjanMannaED/NeelanjanMannaED-696.tar.gz/NeelanjanMannaED-696/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='NeelanjanMannaED',
+    version='696',
+    license='Proprietary',
+    author="Neelanjan Manna",
+    author_email='neelanjanmanna2021@gmail.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://drive.google.com/file/d/1mT8c4ZACKriUR0tS4CIkLKCcYNDJXOX8/view?usp=sharing',
+    keywords='encryption decryption Father of modern day cryptography Neelanjan Manna',
+    install_requires=[
+          'easygui',
+          "pycryptodome"
+      ],
+
+)
