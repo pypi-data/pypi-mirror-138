13e39f02efc5970a897ac8b21b7c8f1d587dc373
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:27:33 +0000

    Add package dbd/0.8.8/dbd-0.8.8-py3-none-any.whl

---
+++ packages/dbd/dbd-0.8.8-py3-none-any.whl/dbd/cli/dbdcli.py
@@ -0,0 +1,205 @@
+import importlib.metadata
+import logging
+import os
+import shutil
+from typing import Dict, Any, List
+
+import click
+from sqlalchemy import text
+
+from dbd.log.dbd_exception import DbdException
+from dbd.config.dbd_profile import DbdProfile
+from dbd.config.dbd_project import DbdProject
+from dbd.executors.model_executor import ModelExecutor, InvalidModelException
+from dbd.log.dbd_logger import setup_logging
+
+log = logging.getLogger(__name__)
+
+this_script_dir = os.path.dirname(__file__)
+
+
+class Dbd(object):
+    """
+    Top level CLI object
+    """
+
+    def __init__(self, debug: bool = False, logfile: str = 'dbd.log', profile: str = 'dbd.profile',
+                 project: str = 'dbd.project'):
+        """
+        Constructor
+        :param bool debug: debug flag
+        :param str logfile: log file
+        :param str profile: profile file
+        :param str project: project file
+        """
+        self.__debug = debug
+        self.__logfile = logfile
+        self.__profile = profile
+        self.__project = project
+
+    def debug(self) -> bool:
+        """
+        Debug flag getter
+        :return: debug flag
+        :rtype: bool
+        """
+        return self.__debug
+
+    def logfile(self) -> str:
+        """
+        Logfile getter
+        :return: logfile
+        :rtype: str
+        """
+        return self.__logfile
+
+    def profile(self) -> str:
+        """
+        Profile getter
+        :return: profile
+        :rtype: str
+        """
+        return self.__profile
+
+    def project(self) -> str:
+        """
+        Project getter
+        :return: project
+        :rtype: str
+        """
+        return self.__project
+
+
+def print_version():
+    """
+    Prints DBD version
+    """
+    click.echo(f"You're using DBD version {importlib.metadata.version('dbd')}.")
+
+
+@click.group(invoke_without_command=True)
+@click.option('--debug/--no-debug', envvar='DBD_DEBUG', default=False, help='Sets debugging on/off')
+@click.option('--version', help="Print the DBD version and exit.", is_flag=True, is_eager=True)
+@click.option('--logfile', envvar='DBD_LOG_FILE', default='dbd.log', help='Log file location')
+@click.option('--profile', envvar='DBD_PROFILE', default='dbd.profile', help='Profile configuration file')
+@click.option('--project', envvar='DBD_PROJECT', default='dbd.project', help='Project configuration file')
+@click.pass_context
+def cli(ctx, debug, logfile, version, profile, project):
+    if debug:
+        click.echo(f"Logging DEBUG info to '{logfile}'")
+        setup_logging(logging.DEBUG, logfile)
+    if version:
+        print_version()
+        ctx.exit(0)
+    ctx.obj = Dbd(debug, logfile, profile, project)
+
+
+# noinspection PyUnusedLocal
+@cli.command(help='Initializes a new DBD project.')
+@click.argument('dest', required=False, default='my_new_dbd_project')
+@click.pass_obj
+def init(dbd, dest):
+    try:
+        src = os.path.join(this_script_dir, '..', 'resources', 'template')
+        if os.path.exists(dest):
+            log.error(f"Can't overwrite directory '{dest}'")
+            raise DbdException(f"Can't overwrite directory '{dest}'")
+        shutil.copytree(src, dest)
+        click.echo(f"New project {dest} generated. Do cd {dest}; dbd run .")
+    except DbdException as d:
+        click.echo(f"ERROR: '{d}'")
+
+
+@cli.command(help='Executes project.')
+@click.option('--only', envvar='DBD_ONLY', default=None, help='Comma separated list of fully qualified table names '
+                                                              '(<schema>.<table-name-no suffix>) to execute.')
+@click.option('--deps/--no-deps', envvar='DBD_DEPS', default=True, help='Ignores dependencies for the --only list.')
+@click.argument('dest', required=False, default='.')
+@click.pass_obj
+def run(dbd, only, deps, dest):
+    try:
+        log.debug("Loading configuration.")
+        prf = DbdProfile.load(os.path.join('.', dbd.profile()))
+        prj = DbdProject.load(prf, os.path.join(dest, dbd.project()))
+        log.debug("Creating model.")
+        model = ModelExecutor(prj)
+        log.debug("Connecting database.")
+        engine = prj.alchemy_engine_from_project()
+#       engine.execution_options(supports_statement_cache=False)
+        log.debug("Executing model.")
+        if not deps and only is None:
+            log.error("You must specify --only list for --no-deps.")
+            raise DbdException("You must specify --only list for --no-deps.")
+        if only is not None:
+            only_list = only.split(',')
+            try:
+                model.execute(engine, only_list, deps)
+            except InvalidModelException as e:
+                log.error(f"Can't run {only_list}: {e}")
+                raise DbdException(f"Can't run {only_list}: {e}")
+        else:
+            model.execute(engine)
+        log.debug("Finished.")
+        click.echo("All tasks finished!")
+    except DbdException as d:
+        click.echo(f"ERROR: '{d}'")
+
+
+@cli.command(help='Validates project.')
+@click.argument('dest', required=False, default='.')
+@click.pass_obj
+def validate(dbd, dest):
+    try:
+        prf = DbdProfile.load(os.path.join('.', dbd.profile()))
+        prj = DbdProject.load(prf, os.path.join(dest, dbd.project()))
+        model = ModelExecutor(prj)
+        engine = prj.alchemy_engine_from_project()
+        # noinspection PyBroadException
+        try:
+            engine.execute(text("SELECT 1"))
+        except Exception:
+            click.echo(
+                f"Can't connect to the target database. Check profile configuration in "
+                f"'{os.path.normpath(os.path.join(dest, dbd.profile()))}'.")
+        validation_result, validation_errors = model.validate()
+        if validation_result:
+            click.echo("No errors found. Model is valid.")
+        else:
+            click.echo("Model isn't valid. Please fix the following errors:")
+            __echo_validation_errors(validation_errors)
+    except DbdException as d:
+        click.echo(f"ERROR: '{d}'")
+
+
+def __echo_validation_errors(validation_errors: Dict[str, Any]):
+    """
+    Top level function for printing validation errors
+    :param validation_errors:
+    :return:
+    """
+    __echo_validation_level(validation_errors)
+
+
+class InvalidValidationErrorStructure(DbdException):
+    pass
+
+
+def __echo_validation_level(level_validation_errors: Dict[str, Any], indent: int = 0):
+    """
+    Echo validation error line (called recursively on all Dict values)
+    :param level_validation_errors: Dict with validation result
+    :param indent: indentation level
+    """
+    for (k, v) in level_validation_errors.items():
+        if isinstance(v, str):
+            msg = f"{k}:{v}"
+            click.echo(msg.rjust(indent * 2 + len(msg), ' '))
+        elif isinstance(v, Dict):
+            msg = f"{k}:"
+            click.echo(msg.rjust(indent * 2 + len(msg), ' '))
+            __echo_validation_level(v, indent + 1)
+        elif isinstance(v, List):
+            msg = f"{k}:{str(v)}"
+            click.echo(msg.rjust(indent * 2 + len(msg), ' '))
+        else:
+            raise InvalidValidationErrorStructure(f"Invalid validation result: '{v}' isn't supported type.")
---
+++ packages/dbd/dbd-0.8.8-py3-none-any.whl/dbd/utils/sql_parser.py
@@ -0,0 +1,285 @@
+import logging
+import re
+import sys
+from datetime import date, datetime
+from typing import List
+
+import math
+import pandas as pd
+import sqlalchemy
+# noinspection PyUnresolvedReferences
+import sqlalchemy.dialects.mysql
+# noinspection PyUnresolvedReferences
+import sqlalchemy.dialects.postgresql
+# noinspection PyUnresolvedReferences
+import sqlalchemy.dialects.sqlite
+# noinspection PyUnresolvedReferences
+import sqlalchemy.sql.sqltypes
+# noinspection PyUnresolvedReferences
+import sqlalchemy_bigquery
+from dateutil import parser as date_parser
+from math import nan
+from sql_metadata import Parser
+
+from dbd.log.dbd_exception import DbdException
+
+log = logging.getLogger(__name__)
+
+
+class SQlParserException(DbdException):
+    pass
+
+
+class SqlParser:
+    """ Parses SQL and extracts different parts from the parsed SQL statement."""
+
+    @classmethod
+    def extract_tables(cls, sql: str) -> List[str]:
+        """
+        Extracts tables from the parsed SQL statement.
+        Works with subquery, CTE, and many other SQL constructs
+        :param str sql: parsed SQL statement
+        :return: list of tables that the SQL statement depends on
+        :rtype: List[str]
+        """
+        try:
+            tables = Parser(sql).tables
+        except ValueError:
+            raise SQlParserException(f"Invalid SQL query '{sql}'")
+        return tables
+
+    @classmethod
+    def extract_foreign_key_tables(cls, foreign_keys_def: List[str]) -> List[str]:
+        """
+        Extracts tables that the passed foreign keys depend on
+        Relies on the <table>.<column> format
+        :param List[str] foreign_keys_def: foreign key
+        :return: str array of table names extracted from passed foreign keys
+        :rtype: List[str]
+        """
+        for f in foreign_keys_def:
+            if len(f.split('.')) <= 1:
+                raise SQlParserException(f"Invalid foreign key format '{f}'. There is no table component.")
+        return [f.split('.')[0] for f in foreign_keys_def]
+
+    @classmethod
+    def compact_sql(cls, sql: str) -> str:
+        """
+        Compacts the SQL text. Strip comments, etc.
+        :param sql: input SQL
+        :return: compacted SQL text
+        :rtype: str
+        """
+        try:
+            parsed_sql = Parser(sql).without_comments
+        except ValueError:
+            raise SQlParserException(f"Invalid SQL query '{sql}'")
+        return parsed_sql
+
+    @classmethod
+    def comments(cls, sql: str) -> List[str]:
+        """
+        Returns the SQL comments
+        :param sql: input SQL
+        :return: comments as array of string
+        :rtype: List[str]
+        """
+        try:
+            parsed_sql = Parser(sql).comments
+        except ValueError:
+            raise SQlParserException(f"Invalid SQL query '{sql}'")
+        return parsed_sql
+
+    @classmethod
+    def parse_alchemy_data_type(cls, data_type: str) -> sqlalchemy.types.TypeEngine:
+        """
+        Parses SQLAlchemy datatype from string
+        :param str data_type: SQLAlchemy data type as string
+        :return: SQLAlchemy data type
+        :rtype:  sqlalchemy.types.TypeEngine subclass
+        """
+        parsed_data_type = Parser(f"CREATE TABLE a( c {data_type} )")
+        core_data_type = parsed_data_type.tokens[5].value
+        length = int(parsed_data_type.tokens[7].value) if len(
+            parsed_data_type.tokens) > 7 and parsed_data_type.tokens[7].is_integer else None
+        scale = int(parsed_data_type.tokens[9].value) if len(
+            parsed_data_type.tokens) > 9 and parsed_data_type.tokens[9].is_integer else None
+
+        for modules in ['sqlalchemy.sql.sqltypes', 'sqlalchemy.dialects.postgresql', 'sqlalchemy.dialects.sqlite',
+                        'sqlalchemy.dialects.mysql', 'sqlalchemy_bigquery']:
+            if hasattr(sys.modules[modules], core_data_type):
+                try:
+                    params = dict(scale=scale, length=length) if scale and length \
+                        else dict(scale=scale) if scale else dict(length=length) if length else {}
+                    log.debug(f"Serching for type {core_data_type}({dict}) in {modules}.")
+                    tp = getattr(sys.modules[modules], core_data_type)(**params)
+                except TypeError:
+                    try:
+                        params = dict(scale=scale, precision=length) if scale and length \
+                            else dict(scale=scale) if scale else dict(precision=length) if length else {}
+                        log.debug(f"Serching for type {core_data_type}({dict}) in {modules}.")
+                        tp = getattr(sys.modules[modules], core_data_type)(**params)
+                    except TypeError:
+                        raise SQlParserException(f"Unsupported data type {core_data_type} with parameters {params}.")
+                return tp
+        log.debug(f"Unsupported data type {core_data_type}.")
+        raise SQlParserException(f"Unsupported data type {core_data_type}.")
+
+
+    @classmethod
+    def parse_date(cls, dt: str) -> date:
+        """
+        Parses a date string
+        :param str dt: date string
+        :return: parsed date
+        :rtype: datetime.date
+        """
+        dtt = cls.parse_datetime(dt)
+        return dtt.date() if isinstance(dtt, datetime) else dtt
+
+    @classmethod
+    def parse_datetime(cls, dt: str) -> datetime:
+        """
+        Parses a datetime string
+        :param str dt: datetime string
+        :return: parsed datetime
+        :rtype: datetime.datetime
+        """
+        if isinstance(dt, str):
+            return date_parser.parse(dt) if len(dt) > 0 else nan
+        else:
+            return dt
+
+    @classmethod
+    def parse_bool(cls, b: str) -> bool:
+        """
+        Parses a bool string
+        :param str b: bool string
+        :return: parsed bool
+        :rtype: bool
+        """
+        if isinstance(b, str):
+            return b.lower() in ('true', '1', 't', 'y', 'yes')
+        elif isinstance(b, bool):
+            return b
+        elif isinstance(b, int):
+            return b != 0
+        elif isinstance(b, float):
+            return (b != 0.0) if not math.isnan(b) else nan
+        else:
+            return b
+
+    @classmethod
+    def parse_bool_int(cls, b: str) -> int:
+        """
+        Parses a bool string
+        :param str b: bool string
+        :return: parsed bool as int (1/0)
+        :rtype: int
+        """
+        if isinstance(b, str):
+            return 1 if b.lower() in ('true', '1', 't', 'y', 'yes') else 0
+        elif isinstance(b, bool):
+            return 1 if b else 0
+        elif isinstance(b, int):
+            return 1 if b != 0 else 0
+        elif isinstance(b, float):
+            r = (1 if b != 0.0 else 0) if not math.isnan(b) else nan
+            return r
+        else:
+            return b
+
+    @classmethod
+    def parse_int(cls, i: str) -> int:
+        """
+        Parses an int string
+        :param str i: int string
+        :return: parsed int
+        :rtype: int
+        """
+        if isinstance(i, str):
+            return int(i) if len(i) > 0 else nan
+        elif isinstance(i, bool):
+            return 1 if i else 0
+        elif isinstance(i, float):
+            return int(i) if not math.isnan(i) else nan
+        else:
+            return i
+
+    @classmethod
+    def parse_string(cls, i: str) -> str:
+        """
+        Resolves string nans
+        :param str i:  string
+        :return: parsed int
+        :rtype: str
+        """
+        if i is None or pd.isna(i):
+            return nan
+        elif isinstance(i, float):
+            return str(i) if not math.isnan(i) else nan
+        elif isinstance(i, str):
+            return i if len(i) > 0 else nan
+        else:
+            return str(i)
+
+    @classmethod
+    def remove_sql_comments(cls, sql_text: str) -> str:
+        """
+        Remove SQL comments from a SQL text
+        :param str sql_text: SQL texts with comments
+        :return: SQL texts without comments
+        :rtype: str
+        """
+        pattern = r"(\".*?\"|\'.*?\')|(/\*.*?\*/|--[^\r\n]*$)"
+        # first group captures quoted strings (double or single)
+        # second group captures comments (//single-line or /* multi-line */)
+        regex = re.compile(pattern, re.MULTILINE | re.DOTALL)
+
+        def _replacer(match):
+            # if the 2nd group (capturing comments) is not None,
+            # it means we have captured a non-quoted (real) comment string.
+            if match.group(2) is not None:
+                return ""  # so we will return empty to remove the comment
+            else:  # otherwise, we will return the 1st group
+                return match.group(1)  # captured quoted-string
+
+        no_comments = regex.sub(_replacer, sql_text)
+        # replace multiple newlines with one
+        return re.sub(r'\n+', '\n', no_comments).strip()
+
+    @classmethod
+    def datatype_to_gbq_datatype(cls, datatype: str) -> str:
+        """
+        Converts SQL datatype to Google BigQuery datatype
+        :param str datatype: SQL datatype
+        :return: BigQuery datatype name
+        :rtype: str
+        """
+        if datatype.lower().startswith(('char', 'varchar', 'text')):
+            return 'STRING'
+        elif datatype.lower().startswith(('decimal', 'numeric')):
+            return 'NUMERIC'
+        elif datatype.lower().startswith(('datetime')):
+            return 'DATETIME'
+        elif datatype.lower().startswith(('timestamp')):
+            return 'TIMESTAMP'
+        else:
+            return datatype.upper()
+
+    @classmethod
+    def format_date(cls, dt: datetime.date) -> str:
+        """
+        Formats a date to a string
+        :param datetime.datetime dt: date
+        :return: formatted date
+        :rtype: str
+        """
+        if dt and isinstance(dt, datetime):
+            return dt.strftime('%Y-%m-%d %H:%M:%S.%f')
+        if dt and isinstance(dt, date):
+            return dt.strftime('%Y-%m-%d')
+        elif dt and isinstance(dt, str):
+            return dt
+        else:
+            return 'NULL'
