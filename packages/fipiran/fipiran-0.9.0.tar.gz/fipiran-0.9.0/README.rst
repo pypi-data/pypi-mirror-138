2e4318c8656fabf935b8b4faa10950287502b095
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:40 +0000

    Add package fipiran/0.9.0/fipiran-0.9.0.tar.gz

---
+++ packages/fipiran/fipiran-0.9.0.tar.gz/fipiran-0.9.0/README.rst
@@ -0,0 +1,13 @@
+Note: This is package is incomplete and still in initial development phase. The API may change without deprecation.
+
+Requires Python 3.10+.
+
+See also:
+
+* https://github.com/5j9/tsetmc
+
+
+If you are interested in other information that are available on fipiran.com but this library has no API for, please `open an issue`_ for them on github.
+
+
+.. _open an issue: https://github.com/5j9/fipiran/issues
---
+++ packages/fipiran/fipiran-0.9.0.tar.gz/fipiran-0.9.0/fipiran/__init__.py
@@ -0,0 +1,34 @@
+__version__ = '0.9.0'
+
+from json import loads
+from functools import partial as _partial
+
+from urllib3 import PoolManager as _PoolManager, Timeout as _Timeout
+
+# noinspection PyUnresolvedReferences
+from pandas import (
+    DataFrame as _DataFrame,
+    read_html as _read_html,
+    to_datetime as _to_datetime,
+)
+
+# noinspection PyUnresolvedReferences
+from jdatetime import datetime as _jdatetime
+
+
+_FIPIRAN = 'https://www.fipiran.ir/'
+_YK = ''.maketrans('يك', 'یک')
+_API = 'https://fund.fipiran.ir/api/v1/'
+_http = _PoolManager(timeout=_Timeout(total=15., connect=5., read=5.))
+_http_get: _http.request = _partial(_http.request, 'GET')
+
+
+def _api(path) -> dict | list:
+    return loads(_http_get(_API + path).data)
+
+
+def _fipiran(path: str, fields=None, json_resp=False) -> str | dict | list:
+    data = _http_get(f'{_FIPIRAN}{path}', fields).data
+    if json_resp is True:
+        return loads(data)
+    return data.decode().translate(_YK)
