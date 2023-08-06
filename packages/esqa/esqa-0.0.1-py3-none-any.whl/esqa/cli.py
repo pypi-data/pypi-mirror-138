80e50aff7ac80fe84a53b944b0d0609114ef0610
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:21:25 +0000

    Add package esqa/0.0.1/esqa-0.0.1-py3-none-any.whl

---
+++ packages/esqa/esqa-0.0.1-py3-none-any.whl/esqa/cli.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""Console script for esqa."""
+
+import click
+
+from esqa.config import load
+from esqa.runner import Runner
+
+
+@click.group()
+def main():
+    pass
+
+@main.command()
+@click.option('-c', '--config', type=str, help="configuration file")
+@click.option('--es-host', type=str, help="Host name of Elasticsearch host", required=False)
+@click.option('--es-port', type=int, help="Port number of Elasticsearch host", required=False)
+@click.option('--index', type=str, help="target index name", required=True)
+def check(config, es_port, es_host, index):
+    runner = Runner()
+    results = runner.run(config=load(config), index_name=index)
+    print(results)
+
+
+if __name__ == '__main__':
+    main()
