5ff285622a59af090e1481379bbfbbc8547b81df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:56:53 +0000

    Add package solvers/0.4/solvers-0.4.tar.gz

---
+++ packages/solvers/solvers-0.4.tar.gz/solvers-0.4/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='solvers',
+    version='0.4',
+    license='MIT',
+    author="Nguyen Tien Nam",
+    author_email='a@example.com',
+    packages=find_packages('solvers'),
+    package_dir={'': 'solvers'},
+    keywords='linear solvers',
+    install_requires=[
+          'numpy',
+      ],
+
+)
