aa73879caeee01bb457413ae291cf09b836889c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:50 +0000

    Add package lizaliblove/0.0.1/lizaliblove-0.0.1.tar.gz

---
+++ packages/lizaliblove/lizaliblove-0.0.1.tar.gz/lizaliblove-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='lizaliblove',
+    version='0.0.1',
+    packages=['lizaliblove'],
+    install_requires=[
+        "networkx==2.5.1",
+        "pydot==1.4.2",
+    ],
+)
+
