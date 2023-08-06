347b00f07fb128df9ce09e48e94e47836b608222
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:35 +0000

    Add package toolconf/0.0.4/toolconf-0.0.4.tar.gz

---
+++ packages/toolconf/toolconf-0.0.4.tar.gz/toolconf-0.0.4/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+
+setuptools.setup(
+    name='toolconf',
+    version='0.0.4',
+    packages=['toolconfig'],
+    install_requires=[
+        'pyyaml',
+        'typing_extensions',
+    ],
+)
+
