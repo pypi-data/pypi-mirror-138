31a8538495c38f08454e0c7db6f323cf006f9827
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:29:30 +0000

    Add package Doodke/0.1.1/Doodke-0.1.1-py3-none-any.whl

---
+++ packages/Doodke/Doodke-0.1.1-py3-none-any.whl/Doodke/Math.py
@@ -0,0 +1,11 @@
+def add(numberOne, numberTwo):
+    print(numberOne + numberTwo)
+
+def minus(numberOne, numberTwo):
+    print(numberOne - numberTwo)
+
+def divide(numberOne, numberTwo):
+    print(numberOne / numberTwo)
+
+def multiply(numberOne, numberTwo):
+    print(numberOne * numberTwo)
