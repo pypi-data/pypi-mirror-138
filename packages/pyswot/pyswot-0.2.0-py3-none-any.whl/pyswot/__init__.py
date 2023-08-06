c66f1e15a6cfcaa924de4905abb00d9e18faaa7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:25:58 +0000

    Add package pyswot/0.2.0/pyswot-0.2.0-py3-none-any.whl

---
+++ packages/pyswot/pyswot-0.2.0-py3-none-any.whl/pyswot/__init__.py
@@ -0,0 +1,7 @@
+__author__ = """James Meakin"""
+__email__ = "code@jmsmkn.com"
+__version__ = "0.2.0"
+
+from .pyswot import find_school_names, is_academic
+
+__all__ = ["is_academic", "find_school_names"]
