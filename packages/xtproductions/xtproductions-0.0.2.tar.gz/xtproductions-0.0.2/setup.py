1b76ef94a147ae45649b32e9f8119d746419d947
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:16 +0000

    Add package xtproductions/0.0.2/xtproductions-0.0.2.tar.gz

---
+++ packages/xtproductions/xtproductions-0.0.2.tar.gz/xtproductions-0.0.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+setup(
+        name="xtproductions", 
+        version='0.0.2',
+        author="xt group",
+        description="the Arsenal",
+        long_description="the Arsenaaaaaaaaaaaaaaaaaaal",
+        packages=find_packages(),
+        keywords=['cozy'],
+        install_requires = ['']
+        
+)
