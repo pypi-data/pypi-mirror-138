af5804c25b34bbaef854dba8f7681cd87b62a959
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:27 +0000

    Add package multiprocess-wraps/0.2/multiprocess_wraps-0.2.tar.gz

---
+++ packages/multiprocess-wraps/multiprocess_wraps-0.2.tar.gz/multiprocess_wraps-0.2/multiprocess_wraps/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages            #这个包没有的可以pip一下
+
+setup(
+    name = "multiprocess_wraps",      #这里是pip项目发布的名称
+    version = "0.2",  #版本号，数值大的会优先被pip
+    keywords = ("pip", "multiprocess", "multiprocessing"),
+    description = "An easy way to multiprocess",
+    long_description = "An easy way to multiprocess",
+    license = "MIT Licence",
+
+    url = "https://github.com/thuxmf/multiprocess-decorator",     #项目相关文件地址，一般是github
+    author = "thuxmf",
+    author_email = "943774492@qq.com",
+
+    packages = ["multiprocess_wraps"],
+    include_package_data = True,
+    platforms = "any",
+    install_requires = ["torch"]          #这个项目需要的第三方库
+)
