19465195dbfe4cb887ae19b8ad74b7fadd8f0a3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:29:35 +0000

    Add package NMC/666999/NMC-666999.tar.gz

---
+++ packages/NMC/NMC-666999.tar.gz/NMC-666999/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='NMC',
+    version='666999',
+    license='Proprietary',
+    author="Neelanjan Manna",
+    author_email='neelanjanmanna2021@gmail.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://drive.google.com/file/d/1mT8c4ZACKriUR0tS4CIkLKCcYNDJXOX8/view?usp=sharing',
+    keywords='encryption decryption Father of modern day cryptography Neelanjan Manna',
+    install_requires=[
+      "easygui",
+      ],
+
+)
