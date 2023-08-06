54b4b02d3cf2cdeed2f153ed707f3a9c67db1c2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:20:16 +0000

    Add package rpa-olymp/0.0/rpa_olymp-0.0.tar.gz

---
+++ packages/rpa-olymp/rpa_olymp-0.0.tar.gz/rpa_olymp-0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='rpa_olymp',
+      version='0.0',
+      description='Description bib',
+      packages=['rpa_olymp'],
+      author_email='dima.prytkov.dima@mail.ru',
+      zip_safe=False)
