4fd8328ef5fd700dc5a38a66925304024c16368b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:06 +0000

    Add package sbapi/0.0.1/sbapi-0.0.1.tar.gz

---
+++ packages/sbapi/sbapi-0.0.1.tar.gz/sbapi-0.0.1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.1'
+DESCRIPTION = 'API Wrapper for Hypixel Skyblock.'
+
+# Setting up
+setup(
+    name="sbapi",
+    version=VERSION,
+    author="carreb, drawbridge",
+    author_email="<gabemackta@gmail.com>",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['requests'],
+    keywords=['python', 'requests', 'api', 'hypixel', 'skyblock'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
