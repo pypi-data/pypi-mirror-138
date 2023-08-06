339c710cb4294eaf26dcc09cd79fb3ce069eda5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:53:41 +0000

    Add package datenraffinerie/0.3.1/datenraffinerie-0.3.1.tar.gz

---
+++ packages/datenraffinerie/datenraffinerie-0.3.1.tar.gz/datenraffinerie-0.3.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    entry_points={
+        'console_scripts': [
+            'datenraffinerie = datenraffinerie.datenraffinerie:cli',
+        ]
+    },
+    install_requires=[
+        'Click',
+        'luigi',
+        'pandas',
+        'matplotlib',
+        'numpy',
+        'scipy',
+        'uproot',
+        'pyyaml',
+        'zmq',
+        'pytest',
+        'awkward',
+        'tables'
+    ]
+)
