4ab6a44227c79f2a0168c410dbe5d40a41fd960d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:39:54 +0000

    Add package intelli-gateway/0.0.4/intelli_gateway-0.0.4.tar.gz

---
+++ packages/intelli-gateway/intelli_gateway-0.0.4.tar.gz/intelli_gateway-0.0.4/tests/example.py
@@ -0,0 +1,21 @@
+from intelli_gateway.gateway_client import GatewayClient
+
+client = GatewayClient("imngonii@gmail.com", "Nickm@ng13")
+
+# Authenticate Client
+client.authenticate()
+
+# Get Account Information
+client.account_information()
+
+# Send single sms
+client.send_sms(message="Hello Wangu", receiver="263777213388", sender_id="IAS")
+
+# Send bulk sms
+client.send_bulk_sms(message="Hello Vakuru", receivers=["263777213388", "263775810157"], sender_id="Tumai")
+
+# Send Email
+client.send_email(body="Hi hallo", setting_id="2819bdaa-6035-4e45-9d93-ce5d3724ec52", receiver_email="ngoni.mangudya@intelliafrica.solutions", subject="Testing")
+
+
+
