2a8310ee5a6d61908b6c456a26329650302803e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:02 +0000

    Add package yarender/0.1.4/yarender-0.1.4.tar.gz

---
+++ packages/yarender/yarender-0.1.4.tar.gz/yarender-0.1.4/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['yarender']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['PyYAML>=6.0,<7.0', 'click>=8.0.3,<9.0.0', 'rich>=11.2.0,<12.0.0']
+
+entry_points = \
+{'console_scripts': ['yarender = yarender.main:render']}
+
+setup_kwargs = {
+    'name': 'yarender',
+    'version': '0.1.4',
+    'description': '',
+    'long_description': None,
+    'author': 'DeDA',
+    'author_email': 'denis.deryabin@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
