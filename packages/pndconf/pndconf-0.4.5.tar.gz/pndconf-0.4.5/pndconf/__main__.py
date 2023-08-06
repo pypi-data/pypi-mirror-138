7e98b88826e7491963d6763cf55fb2424905e13a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:43:20 +0000

    Add package pndconf/0.4.5/pndconf-0.4.5.tar.gz

---
+++ packages/pndconf/pndconf-0.4.5.tar.gz/pndconf-0.4.5/pndconf/__main__.py
@@ -0,0 +1,5 @@
+from .pndconf import main
+
+
+if __name__ == '__main__':
+    main()
---
+++ packages/pndconf/pndconf-0.4.5.tar.gz/pndconf-0.4.5/pndconf/colors.py
@@ -0,0 +1,16 @@
+class COLORS:
+    """Color codes for terminal colors
+    """
+    RED = '\033[31m'
+    ALT_RED = '\033[91m'
+    BRIGHT_RED = '\033[1;31m'
+    ALT_BRIGHT_RED = '\033[1;91m'
+    YELLOW = '\033[33m'
+    BRIGHT_YELLOW = '\033[1;33m'
+    BLUE = '\033[34m'
+    BRIGHT_BLUE = '\033[1;34m'
+    ALT_BLUE = '\033[94m'
+    ALT_BRIGHT_BLUE = '\033[1;94m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    ENDC = '\033[0m'
