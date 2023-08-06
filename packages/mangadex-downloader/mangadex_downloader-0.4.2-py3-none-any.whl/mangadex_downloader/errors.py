863138fe2ee4cfd0367066d75e3dff35c1b0b8b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:42 +0000

    Add package mangadex-downloader/0.4.2/mangadex_downloader-0.4.2-py3-none-any.whl

---
+++ packages/mangadex-downloader/mangadex_downloader-0.4.2-py3-none-any.whl/mangadex_downloader/errors.py
@@ -0,0 +1,33 @@
+class MangaDexException(Exception):
+    """Base exception for MangaDex errors"""
+    pass
+
+class HTTPException(MangaDexException):
+    """HTTP errors"""
+    def __init__(self, *args: object, resp=None) -> None:
+        self.response = resp
+        super().__init__(*args)
+
+class ChapterNotFound(MangaDexException):
+    """Raised when selected manga has no chapters"""
+    pass
+
+class InvalidManga(MangaDexException):
+    """Raised when invalid manga is found"""
+    pass
+
+class InvalidURL(MangaDexException):
+    """Raised when given mangadex url is invalid"""
+    pass
+
+class LoginFailed(MangaDexException):
+    """Raised when login is failed"""
+    pass
+
+class AlreadyLoggedIn(MangaDexException):
+    """Raised when user try login but already logged in """
+    pass
+
+class NotLoggedIn(MangaDexException):
+    """Raised when user try to logout when user are not logged in"""
+    pass
