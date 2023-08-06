c4f1e73104f683267033e64b6364e801cbe210ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:06:37 +0000

    Add package throttled/0.1.16a30/throttled-0.1.16a30.tar.gz

---
+++ packages/throttled/throttled-0.1.16a30.tar.gz/throttled-0.1.16a30/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['throttled', 'throttled.fastapi', 'throttled.storage']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'throttled',
+    'version': '0.1.16a30',
+    'description': 'A rate limiter for FastAPI',
+    'long_description': '# ThrottledAPI\n\nThis repo aims to be an audacious RateLimiter for python ASGI/WSGI APIs.\n\n## RoadMap\n\n- [ ] Implement for FastAPI, because I really like the framework\n- [ ] Generalise for others\n\n## Status\n\nIn development ...\n',
+    'author': 'Vinícius Vargas',
+    'author_email': 'santunionivinicius@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/santunioni/ThrottledAPI',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
