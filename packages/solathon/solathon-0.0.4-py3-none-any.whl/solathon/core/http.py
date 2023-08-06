de7d0cbe110b30e221d0dbca2438d6b72e4f9578
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:28:06 +0000

    Add package solathon/0.0.4/solathon-0.0.4-py3-none-any.whl

---
+++ packages/solathon/solathon-0.0.4-py3-none-any.whl/solathon/core/http.py
@@ -0,0 +1,40 @@
+import sys
+import base64
+from typing import Tuple, Any, Dict
+import httpx
+from .. import __version__
+from ..publickey import PublicKey
+
+
+class HTTPClient:
+    """HTTP Client to interact with Solana JSON RPC"""
+
+    def __init__(self, endpoint: str):
+        self.endpoint = endpoint
+        version = sys.version_info
+        self.headers = {
+            "Content-Type": "application/json",
+            "User-Agent": (
+                "Solathon (https://github.com/GitBolt/solathon "
+                f"{__version__}) Python{version[0]} {version[1]}"
+            ),
+        }
+        self.request_id = 0
+
+    def send(self, data: str) -> Dict[str, Any]:
+        res = httpx.post(url=self.endpoint, headers=self.headers, json=data)
+        return res.json()
+
+    def build_data(self, method: str, params: Tuple[Any]) -> Dict[str, Any]:
+        self.request_id += 1
+        params = [str(i) if isinstance(i, PublicKey) else i for i in params]
+
+        if isinstance(params[0], bytes):
+            params[0] = base64.b64encode(params[0]).decode("utf-8")
+
+        return {
+            "jsonrpc": "2.0",
+            "id": self.request_id,
+            "method": method,
+            "params": None if params[0] is None else params,
+        }
---
+++ packages/solathon/solathon-0.0.4-py3-none-any.whl/solathon/utils.py
@@ -0,0 +1,6 @@
+def lamport_to_sol(lamports: float) -> float:
+    return float(lamports / 1000000000)
+
+def sol_to_lamport(lamports: float) -> float:
+    return float(lamports * 1000000000)
+
