9276187487b8f042283bffcdfac599a191690265
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:42:48 +0000

    Add package pnav/0.2.0/pnav-0.2.0.tar.gz

---
+++ packages/pnav/pnav-0.2.0.tar.gz/pnav-0.2.0/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+setuptools.setup(
+    name='pnav',
+    version='0.2.0',
+    author='Zander Navratil',
+    description="Package with random functions, some are useful, some aren't so much. They are however used often, maintained, and good for learning.",
+    packages=['pnav'],
+    long_description='Hello! Thank you for choosing the Pnav package. For more information on a certain function run pnav.help.functionname or pnav.help.help("functionname"). Here are a few of the classes you can choose from: random: contains random functions and projects, messyCheck, inputOwn, checkDate, isPalindrome, info. timer: uses a gui to get user data. base, candy. stockScraper: uses beautifulSoup4 to get html data from a webpage and then returns information on a certain stock. select.')
+
+# '   \tHello! Thank you for choosing the pnav package.\n
+# For more information on a certain function run pnav.help.functionname or pnav.help.help("functionname")\n\t
+#     Here are a few of the classes you can choose from:\n
+# random: contains random functions and projects.\n\t
+#     messyCheck\n\t
+#     inputOwn\n\t
+#     checkDate\n\t
+#     isPalindrome\n\t
+#     info\n
+# timer: uses a gui to get user data.\n\t
+#     base\n\t
+#     candy\n
+# stockScraper: uses beautifulSoup4 to get html data from a webpage and then returns information on a certain stock.\n\t
+#     select'
