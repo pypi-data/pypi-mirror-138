f7dbdafe6abfc4ab7e231bc670150f615211c5d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:06:30 +0000

    Add package indonesia-earthquake/0.0.4/indonesia-earthquake-0.0.4.tar.gz

---
+++ packages/indonesia-earthquake/indonesia-earthquake-0.0.4.tar.gz/indonesia-earthquake-0.0.4/README.md
@@ -0,0 +1,21 @@
+# Latest Indonesia Earthquake
+Do you need the latest earthquake from BMKG | Indonesia Meteorological, Climatological, and Geophysical Agency
+
+## Guidance
+This package will scrape the latest earthquake data from [BMKG](https://www.bmkg.go.id/).
+
+1. Install package from https://pypi.org/project/indonesia-earthquake/
+2. Install module from https://pypi.org/project/requests/
+3. Install module from https://pypi.org/project/beautifulsoup4/
+4. Run your main.py with this code
+
+```
+import last_earthquake
+
+if __name__ == '__main__':
+    result = last_earthquake.data_extraction()
+    last_earthquake.show_data(result)
+```
+
+# Author
+Arsenius Anom Permadi
---
+++ packages/indonesia-earthquake/indonesia-earthquake-0.0.4.tar.gz/indonesia-earthquake-0.0.4/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="indonesia-earthquake",
+    version="0.0.4",
+    author="Arsenius Anom Permadi",
+    author_email="anom@broanom.com",
+    description="the latest earthquake from BMKG | Indonesia",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/anompermadi/latest_indonesia_earthquake.git",
+    project_urls={
+        "Website": "https://www.broanom.com/",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Development Status :: 5 - Production/Stable"
+    ],
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+)
