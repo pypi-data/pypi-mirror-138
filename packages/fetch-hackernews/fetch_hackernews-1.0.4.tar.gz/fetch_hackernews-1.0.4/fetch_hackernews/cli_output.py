54321b3e0903cb2180a673b8ecca1877fbf730d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:25:00 +0000

    Add package fetch-hackernews/1.0.4/fetch_hackernews-1.0.4.tar.gz

---
+++ packages/fetch-hackernews/fetch_hackernews-1.0.4.tar.gz/fetch_hackernews-1.0.4/fetch_hackernews/cli_output.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+"""
+Output fetched news
+Python 3.10+
+Date created: January 30th, 2022
+Date modified: February 16th, 2022
+"""
+
+from fetch_hackernews import constants
+
+
+def cli_menu(headlines: list):
+    """
+    Show headline_ids, headlines and links in the console.
+    Args:
+        headlines: A list containing Headlines objects
+        (headline_id, headline, link)
+    """
+    print()
+    print("##############################")
+    print("#                            #")
+    print("#     Fetch Hacker News      #")
+    print(f"#      Version: {constants.__VERSION__}        #")
+    print("#                            #")
+    print("##############################")
+    print()
+
+    for headline in headlines:
+        print(headline)
