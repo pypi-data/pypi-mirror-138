91a275e23fcd3e1c09e35666425d998825141075
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:27:47 +0000

    Add package cytopath/0.1.8.post1/cytopath-0.1.8.post1.tar.gz

---
+++ packages/cytopath/cytopath-0.1.8.post1.tar.gz/cytopath-0.1.8.post1/cytopath/trajectory_inference.py
@@ -0,0 +1,29 @@
+from .trajectory_estimation.sample_clustering import sample_clustering
+from .trajectory_estimation.cyto_path import cytopath, estimate_cell_data
+from .trajectory_estimation.cytopath_merger import cytopath_merger
+
+def trajectories(data, smoothing=False, alignment_cutoff=0.0, basis='umap', neighbors_basis='pca', fill_cluster=True, n_neighbors_cluster='auto', cluster_freq=0.3,
+                 groupby='mean', weighted=True, surrogate_cell=False, n_neighbors_alignment='auto', cluster_num=None, method = 'kmeans', distance = 'euclidean', num_cores=1, copy=False):
+    
+    adata = data.copy() if copy else data
+    
+    if 'X_pca' not in adata.obsm.keys(): raise ValueError('Compute PCA using cytopath.scvelo.pp.pca')
+
+    # Clustering of cell sequences to obtain trajectories
+    sample_clustering(adata, basis=basis, smoothing=smoothing, 
+                          cluster_num=cluster_num, method=method, distance=distance)
+
+    # Align cells along trajectories
+    cytopath(adata, basis=basis, neighbors_basis=neighbors_basis, surrogate_cell=surrogate_cell, fill_cluster=fill_cluster, cluster_freq=cluster_freq, n_neighbors_cluster=n_neighbors_cluster,
+             n_neighbors=n_neighbors_alignment, cut_off=alignment_cutoff, num_cores=num_cores)
+    
+    # Estimate pseudotime, cell fate prob and alignment score at cell level
+    estimate_cell_data(adata, groupby=groupby, weighted=weighted)
+
+    return adata if copy else None
+
+def merge_steps(data, overlap=0.0, num_cores=1, copy=False):
+
+    adata = data.copy() if copy else data
+    cytopath_merger(adata, overlap=overlap, num_cores=num_cores)   
+    return adata if copy else None
---
+++ packages/cytopath/cytopath-0.1.8.post1.tar.gz/cytopath-0.1.8.post1/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(
+  name='cytopath',
+  version='0.1.8.post1',
+  description='Simulation based inference of differentiation trajectories from RNA velocity fields.',
+  license='BSD 3-Clause License',
+  packages=find_packages(),
+  author = 'Revant Gupta',
+  author_email = 'revant.gupta.93@gmail.com',
+  url = 'https://github.com/aron0093/cytopath',
+  download_url = 'https://github.com/aron0093/cytopath/archive/v_018_1.tar.gz',
+  keywords = ['Trajectory Inference', 'single-cell RNA sequencing', 'RNA velocity'],
+  install_requires=[
+          'numpy>=1.20.0',
+          'scipy',
+          'anndata',
+          'scvelo>=0.1.25',
+          'joblib',
+          'fastdtw',
+          'hausdorff',
+          'tqdm',
+	  'scikit-network'
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Science/Research',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: BSD License',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+  ])
