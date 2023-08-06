c4dd71f0615de75d65f289895dbbcf0d55e146f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:16:53 +0000

    Add package pyvesynclevoit/1.4.2/pyvesynclevoit-1.4.2.tar.gz

---
+++ packages/pyvesynclevoit/pyvesynclevoit-1.4.2.tar.gz/pyvesynclevoit-1.4.2/setup.py
@@ -0,0 +1,36 @@
+"""pyvesynclevoit setup script."""
+
+from os import path
+from setuptools import setup, find_packages
+
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='pyvesynclevoit',
+    version='1.4.2',
+    description='pyvesynclevoit is a library to manage Etekcity\
+                 Devices and Levoit Air Purifier\
+                 clone and thanks to webdjoe',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/andreasekoyoga/pyvesynclevoit',
+    author='Andreas',
+    author_email='andreasekoyoga@gmail.com',
+    license='MIT',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+        'Intended Audience :: End Users/Desktop',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords=['iot', 'vesync', 'levoit', 'etekcity'],
+    packages=find_packages('src', exclude=['tests', 'tests.*']),
+    package_dir={'': 'src'},
+    zip_safe=False,
+    install_requires=['requests>=2.20.0'],
+    python_requires='>=3.6',
+)
