25c4340a65eb6294fe9a14a62e8a9051320999a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:23 +0000

    Add package discordpy-bot-cli/3.0/discordpy-bot-cli-3.0.tar.gz

---
+++ packages/discordpy-bot-cli/discordpy-bot-cli-3.0.tar.gz/discordpy-bot-cli-3.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name = "discordpy-bot-cli",
+    version = "3.0",
+    author = "Areen Rath",
+    description = "CLI for helping in making Discord bots easily",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    packages = ["cli"],
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    python_requires = ">=3.6",
+    entry_points={
+        "console_scripts": ["dcpy = cli.cli:start"]
+    }
+)
